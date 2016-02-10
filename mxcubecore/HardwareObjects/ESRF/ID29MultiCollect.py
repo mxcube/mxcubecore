085c8ca19de81e82dd337d4df2bcc92ae5640ffb
bliss administrator <blissadm@p1-id29new.esrf.fr>
2016-02-10 11:08:43 +0100

    Change from spec to bliss.

--- mxcubecore/HardwareObjects/ESRF/ID29MultiCollect.py
+++ mxcubecore/HardwareObjects/ESRF/ID29MultiCollect.py
@@ -1,8 +1,9 @@
-from ESRFMultiCollect import *
+from ESRF.ESRFMultiCollect import *
 from detectors.LimaPilatus import Pilatus
+import gevent
 import shutil
 import logging
-import time
+import os
 
 class ID29MultiCollect(ESRFMultiCollect):
     def __init__(self, name):
@@ -10,22 +11,7 @@ class ID29MultiCollect(ESRFMultiCollect):
 
     @task
     def data_collection_hook(self, data_collect_parameters):
-      oscillation_parameters = data_collect_parameters["oscillation_sequence"][0]
-     
-      data_collect_parameters["dark"] = 0
-      # are we doing shutterless ?
-      #if oscillation_parameters["overlap"] != 0:
-      #  shutterless = False
-      #else:
-      
-      shutterless = data_collect_parameters.get("shutterless")
-      self._detector.shutterless = True if shutterless else False
-      #self.getChannelObject("shutterless").setValue(1 if shutterless else 0)
-
-      self.getChannelObject("parameters").setValue(data_collect_parameters)
-      self.execute_command("build_collect_seq")
-      #self.execute_command("local_set_experiment_type")
-      self.execute_command("prepare_beamline")
+      self._detector.shutterless = data_collect_parameters["shutterless"]
 
     @task
     def data_collection_cleanup(self):
@@ -33,17 +19,51 @@ class ID29MultiCollect(ESRFMultiCollect):
         if state != "out":
             print "-----------------------> Closing fast shutter"
             self.close_fast_shutter()
+      
+    @task
+    def get_beam_size(self):
+        return self.bl_control.beam_info.get_beam_size()
+ 
+    @task
+    def get_slit_gaps(self):
+        controller = self.getObjectByRole("controller")
 
-    def open_fast_shutter(self):
-        self.getObjectByRole("fastshut").actuatorIn()
+        return (None,None)
 
-    def close_fast_shutter(self):
-        self.getObjectByRole("fastshut").actuatorOut()
+    def get_measured_intensity(self):
+        return 0
+
+    @task
+    def get_beam_shape(self):
+        return self.bl_control.beam_info.get_beam_shape()
+
+    @task
+    def move_detector(self, detector_distance):
+        det_distance = self.getObjectByRole("detector_distance")
+        det_distance.move(detector_distance)
+        while det_distance.motorIsMoving():
+          gevent.sleep(0.1)
+
+    @task
+    def set_resolution(self, new_resolution):
+        self.bl_control.resolution.move(new_resolution)
+        while self.bl_control.resolution.motorIsMoving():
+          gevent.sleep(0.1)
+
+    def get_resolution_at_corner(self):
+        return self.bl_control.resolution.get_value_at_corner()
+
+    def get_detector_distance(self):
+        det_distance = self.getObjectByRole("detector_distance")
+        return det_distance.getPosition()
+
+    def ready(*motors):
+        return not any([m.motorIsMoving() for m in motors])
 
     @task
     def move_motors(self, motors_to_move_dict):
         diffr = self.bl_control.diffractometer
-        #cover_task = self.getObjectByRole("controller").detcover.set_out()
+        cover_task = self.getObjectByRole("controller").detcover.set_out()
         try:
             motors_to_move_dict.pop('kappa')
             motors_to_move_dict.pop('kappa_phi')
@@ -52,51 +72,64 @@ class ID29MultiCollect(ESRFMultiCollect):
         diffr.moveSyncMotors(motors_to_move_dict, wait=True, timeout=200)
 
     @task
-    def move_detector(self, detector_distance):
-        self.bl_control.detector_distance.move(detector_distance)
-        while self.bl_control.resolution.motorIsMoving():
-           time.sleep(0.5)
-        #time.sleep(1)
-        #self.bl_control.detector_distance.waitMove()
-
-    def get_detector_distance(self):
-        return self.bl_control.detector_distance.getPosition()
+    def take_crystal_snapshots(self, number_of_snapshots):
+        if self.bl_control.diffractometer.in_plate_mode():
+            if number_of_snapshots > 0:
+                number_of_snapshots = 1
+        self.bl_control.diffractometer.takeSnapshots(number_of_snapshots, wait=True)
 
     @task
-    def set_resolution(self, new_resolution):
-        self.bl_control.resolution.move(new_resolution, wait=True)
-        while self.bl_control.resolution.motorIsMoving():
-           time.sleep(0.5)
-        #time.sleep(1)
-        #self.bl_control.resolution.waitMove()
+    def do_prepare_oscillation(self, *args, **kwargs):
+        diffr = self.getObjectByRole("diffractometer")
+        #move to DataCollection phase
+        if diffr.getPhase() != "DataCollection":
+            logging.getLogger("user_level_log").info("Moving MD2 to Data Collection")
+            diffr.moveToPhase("DataCollection", wait=True, timeout=200)
+        #switch on the front light
+        diffr.getObjectByRole("flight").move(2)
+        #take the back light out
+        diffr.getObjectByRole("lightInOut").actuatorOut()
 
-    def get_beam_size(self):
-        # should be moved to ESRFMultiCollect
-        # (at the moment, ESRFMultiCollect is still using spec)
-        return self.bl_control.beam_info.get_beam_size()
+    @task
+    def oscil(self, start, end, exptime, npass):
+        diffr = self.getObjectByRole("diffractometer")
+        if self.helical:
+            diffr.oscilScan4d(start, end, exptime, self.helical_pos, wait=True)
+        else:
+            diffr.oscilScan(start, end, exptime, wait=True)
 
-    def get_beam_shape(self):
-        # should be moved to ESRFMultiCollect
-        # (at the moment, ESRFMultiCollect is still using spec)
-        return self.bl_control.beam_info.get_beam_shape()
+    def open_fast_shutter(self):
+        #self.getObjectByRole("diffractometer").controller.fshut.open()
+        self.getObjectByRole("fastshut").actuatorIn()
 
-    def get_resolution_at_corner(self):
-        # should be moved to ESRFMultiCollect
-        # (at the moment, ESRFMultiCollect is still using spec)
-        return self.bl_control.resolution.get_value_at_corner()
+    def close_fast_shutter(self):
+        #self.getObjectByRole("diffractometer").controller.fshut.close()
+        self.getObjectByRole("fastshut").actuatorOut()
+
+    def set_helical(self, helical_on):
+        self.helical = helical_on
+
+    def set_helical_pos(self, helical_oscil_pos):
+        self.helical_pos = helical_oscil_pos
+
+    def set_transmission(self, transmission):
+    	self.getObjectByRole("transmission").set_value(transmission)
+
+    def get_transmission(self):
+        return self.getObjectByRole("transmission").get_value()
+
+    def get_cryo_temperature(self):
+        return 0
+
+    @task
+    def prepare_intensity_monitors(self):
+        return
 
     def get_beam_centre(self):
-        # should be moved to ESRFMultiCollect
-        # (at the moment, ESRFMultiCollect is still using spec)
         return self.bl_control.resolution.get_beam_centre()
-
-    def trigger_auto_processing(self, process_event, *args, **kwargs):       
-        if process_event in ('before', 'after'):
-            return ESRFMultiCollect.trigger_auto_processing(self, process_event, *args, **kwargs)
 
     @task
     def write_input_files(self, datacollection_id):
-        # copy *geo_corr.cbf* files to process directory
         try:
             process_dir = os.path.join(self.xds_directory, "..")
             raw_process_dir = os.path.join(self.raw_data_input_file_dir, "..") 
@@ -110,4 +143,3 @@ class ID29MultiCollect(ESRFMultiCollect):
             logging.exception("Exception happened while copying geo_corr files")
        
         return ESRFMultiCollect.write_input_files(self, datacollection_id)
-        
