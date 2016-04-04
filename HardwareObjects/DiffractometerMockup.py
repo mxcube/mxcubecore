e3214c5769bf58b4d42c62f678ff29c87391fbe0
Jie Nan <jie.nan@maxlab.lu.se>
2016-04-04 10:20:44 +0200

    add sample changer and related methods in the Diffractometer Mockup

--- HardwareObjects/DiffractometerMockup.py
+++ HardwareObjects/DiffractometerMockup.py
@@ -101,6 +101,9 @@ class DiffractometerMockup(Equipment):
         self.phase_list = None
         self.head_type = None
         self._drawing = None
+        self.use_sc = False
+        self.transfer_mode = None
+
 
         self.connect(self, 'equipmentReady', self.equipmentReady)
         self.connect(self, 'equipmentNotReady', self.equipmentNotReady)
@@ -116,15 +119,13 @@ class DiffractometerMockup(Equipment):
         self.start3ClickCentring = self.start_3Click_centring
         self.startAutoCentring = self.start_automatic_centring
      
-        self.centringStatus = self.centring_status
-        self.currentCentringProcedure = self.current_centring_procedure
-
     def init(self):
         """
         Descript. :
         """
         self.x_calib = 0.000444
         self.y_calib = 0.000446
+        self.transfer_mode = "SAMPLE_CHANGER"# available mode is DIRECT, KAPPA, SAMPLE_CHANGER, CLEARED, UNKNOWN
          
         self.pixels_per_mm_x = 1.0 / self.x_calib
         self.pixels_per_mm_y = 1.0 / self.y_calib
@@ -158,6 +159,9 @@ class DiffractometerMockup(Equipment):
         self.beam_info_hwobj = self.getObjectByRole('beam_info')
         self.camera = self.camera_hwobj
         self.beam_info = self.beam_info_hwobj
+       
+        self.sample_changer = self.getObjectByRole('samplechanger')
+
 
         if self.phiMotor is not None:
             self.connect(self.phiMotor, 'stateChanged', self.phiMotorStateChanged)
@@ -193,7 +197,12 @@ class DiffractometerMockup(Equipment):
         except:
             self.grid_direction = {"fast": (0, 1), "slow": (1, 0)}
             logging.getLogger("HWR").warning('MiniDiff: Grid direction is not defined. Using default.')
-
+        
+        if self.sample_changer is not None and self.transfer_mode == "SAMPLE_CHANGER":
+            self.use_sc = True  # by default use sample changer if it's defined
+        else:
+            logging.getLogger("HWR").warning('Sample Changer is not defined.')
+ 
         try:
             self.current_phase = "Transfer"
             self.phase_list = eval(self.getProperty("phaseList"))
@@ -201,15 +210,27 @@ class DiffractometerMockup(Equipment):
         except:
             self.phase_list = []
 
+    # to make it compatibile
+    def __getattr__(self, attr):
+        if attr.startswith("__"):
+            raise AttributeError(attr)
+        else:
+            if attr == "currentCentringProcedure":
+                return self.current_centring_procedure
+            if attr == "centringStatus" :
+                return self.centring_status
+
     def set_drawing(self, drawing):
 	self._drawing = drawing
 
     def use_sample_changer(self):
-        return False
+        return self.use_sc
+   
+    def set_use_sc(self,state):
+        self.use_sc = state
 
     def in_plate_mode(self):
-        #currently not used for mockup version
-	return False
+        return self.head_type == "Plate"
 
     def toggle_fast_shutter(self):
         self.fast_shutter_is_open = not self.fast_shutter_is_open
@@ -217,6 +238,9 @@ class DiffractometerMockup(Equipment):
 
     def get_head_type(self):
         return self.head_type
+
+    def get_transfer_mode(self):
+        return self.transfer_mode
          
     def get_grid_direction(self):
         return self.grid_direction
@@ -626,6 +650,9 @@ class DiffractometerMockup(Equipment):
         Descript. :
         """
         return self.current_phase
+
+    def set_phase(self, phase, timeout=None):
+	self.start_set_phase(phase)
 
     def start_set_phase(self, name):
         """
