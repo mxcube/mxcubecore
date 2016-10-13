cbd7fa219203bb296f402da3acda1367cac77a55
Jie Nan <jie.nan@maxlab.lu.se>
2016-10-13 15:22:28 +0200

    add BIOMAXEiger

--- mxcubecore/HardwareObjects/MAXIV/BIOMAXEiger.py
+++ mxcubecore/HardwareObjects/MAXIV/BIOMAXEiger.py
@@ -30,6 +30,9 @@ class Eiger:
                     'PixelMask','PixelMaskApplied','Compression','RoiMode','FileWriterMode',
                     'NbImagesPerFile','ImagesNbStart','NamePattern','CompressionEnabled',
                     'FileWriterState','BufferFree', 'State')
+
+        buffer_free_chan = self.getChannelObject("BufferFree") 
+
         # not all of the following commands are needed, for now all of them here for convenience
         cmd_list = ('Arm','Trigger','Abort','Cancel','ClearBuffer','DeleteFileFromBuffer',
                 'Disarm','DownloadFilesFromBuffer')
@@ -59,30 +62,52 @@ class Eiger:
 
     def get_deadtime(self):
         return float(self.config.getProperty("deadtime"))
+  
+    def get_buffer_free(self):
+        return self.buffer_free_chan.getValue()
 
     @task
-    def prepare_acquisition(self, take_dark, start, osc_range, exptime, npass, number_of_images, comment, energy, still):
+    def prepare_acquisition(self, config):
+        """
+        config is a dictionary
+        OmegaStart,OmegaIncrement,
+        BeamCenterX
+        BeamCenterY
+        OmegaStart
+        OmegaIncrement
+         
+        start, osc_range, exptime, ntrigger, number_of_images, images_per_file, energy, compression,ROI,wavelength):
+        """
         diffractometer_positions = self.collect_obj.bl_control.diffractometer.getPositions()
+       
+        
+        """This writes into the tango device"""
+        for param in header.items():
+            if hasattr(self.dev, param[0])
+                setattr(self.dev, param[0], param[1])
+
+       
         # we have already the tango channel from the init, we could also do a setValue() in all of them
-        self.header['OmegaStart'] = start
-        self.header['OmegaIncrement'] = osc_range
-        beam_x, beam_y = self.collect_obj.get_beam_centre()
-        self.header['BeamCenterX'] = beam_x/7.5000003562308848e-02
-        self.header['BeamCenterY'] = beam_y/7.5000003562308848e-02
-        self.header['Wavelength'] = self.collect_obj.get_wavelength()
-        self.header['DetectorDistance'] = self.collect_obj.get_detector_distance()/1000.0
+        self.config['OmegaStart'] = start
+        self.config['OmegaIncrement'] = osc_range
+        beam_x, beam_y = self.collect_obj.get_beam_centre() # returns length, not pixel
+        self.config['BeamCenterX'] = beam_x
+        self.config['BeamCenterY'] = beam_y
+        self.config['Wavelength'] = self.collect_obj.get_wavelength()
+        self.config['DetectorDistance'] = self.collect_obj.get_detector_distance()/1000.0
 
-        self.header['FrameTime'] = exptime + self.get_deadtime()
+        self.config['FrameTime'] = exptime + self.get_deadtime()
 
-        self.header['NbImages'] = number_of_images
-        self.header['NbTriggers'] = 1 # to check for different tasks
-        self.header['RoiMode'] = self.RoiMode.getValue() 
+        self.config['NbImages'] = number_of_images
+        self.config['NbTriggers'] = ntrigger # to check for different tasks
+        self.config['NbImagesPerFile'] = imagers_per_file
+        self.config['RoiMode'] = roi
+        self.config['PhotonEnergy'] = energy
         # This one??: self.getChannelObject("NbImagesPerFile").setValue(min(100,number_of_images))
 
         self.stop()
         self.wait_ready()
    
-        self.set_energy_threshold(energy)
 
         if still:
             self.getChannelObject("TriggerMode").setValue("ints")
@@ -90,19 +115,12 @@ class Eiger:
         else:
             self.getChannelObject("TriggerMode").setValue("exts")
             # self.header['TriggerMode'] = "exts"
+        self.change_config(self.config)
+        # check the bufferfree in DCU
+        # compression
 
-        self.set_header_data(self.header)
-
-     def set_energy_threshold(self, energy):  
-        minE = self.getChannelObject("PhotonEnergyMin").getValue()
-        if energy < minE:
-            energy = minE
-     
-        working_energy_chan = self.getChannelObject("PhotonEnergy")  # eV
-        working_energy = working_energy_chan.getValue()/1000.0
-        if math.fabs(working_energy - energy) > 0.1:
-            egy = int(energy*1000.0)
-            working_energy_chan.setValue(egy)
+    def has_shutterless(self):
+        return True
         
     @task 
     def set_detector_filenames(self, frame_number, start, filename, jpeg_full_path, jpeg_thumbnail_full_path):
@@ -134,13 +152,9 @@ class Eiger:
         time.sleep(1)
         self.getCommandObject("Disarm")()
 
-    def set_header_data(header):
+    def change_config(header):
         """This writes into the tango device"""
         for param in header.items():
             if hasattr(self.dev, param[0])
                 setattr(self.dev, param[0], param[1])
 
-    # def set_header_data_channel(header):
-    #     """This writes into the tango channel"""
-    #     for param in header.items():
-    #         self.param[0].setValue(param[1])
