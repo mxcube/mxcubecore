f57ddec66c154f3b9d20983e6726d57d89833a52
blissadm@id30_massif3 <blissadm@id30_massif3>
2016-11-15 11:05:33 +0100

    ESRF/Bliss.py: adapted to new Session

--- HardwareObjects/Bliss.py
+++ HardwareObjects/Bliss.py
@@ -1,13 +1,14 @@
 from HardwareRepository.BaseHardwareObjects import HardwareObject
 import os
 import sys
-from bliss.shell import setup
+from bliss.config import static
 
 class Bliss(HardwareObject):
   def __init__(self, *args):
     HardwareObject.__init__(self, *args)
 
-  def init(self, *args):  
-     setup_file = self.getProperty("setup_file")
+  def init(self, *args):
+     cfg = static.get_config()
+     session = cfg.get(self.getProperty("session"))
 
-     setup(setup_file, self.__dict__)
+     session.setup(self.__dict__, verbose=True)
