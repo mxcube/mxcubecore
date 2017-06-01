12e7fd5ede87ecc6e99d18ea461d0f87461d051b
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2017-06-01 17:11:00 +0200

    sdm related fixes

--- MaxIVSession.py
+++ MaxIVSession.py
@@ -4,8 +4,10 @@ MAXIV Session hardware object.
 Adapting from original Session.py to adapt the names of data directories
 """
 import os
+import sys
 import time
 import storage
+import logging
 
 from Session import Session
 import queue_model_objects_v1 as queue_model_objects
@@ -26,16 +28,7 @@ class MaxIVSession(Session):
     # Framework-2 method, inherited from HardwareObject and called
     # by the framework after the object has been initialized.
     def init(self):
-        self.synchrotron_name = self.getProperty('synchrotron_name')
-        self.endstation_name = self.getProperty('endstation_name').lower()
-        self.suffix = self["file_info"].getProperty('file_suffix')
-
-        self.base_directory = self["file_info"].getProperty('base_directory')
-        self.base_process_directory = self["file_info"].getProperty('processed_data_base_directory')
-
-        self.raw_data_folder_name = self["file_info"].getProperty('raw_data_folder_name')
-        self.processed_data_folder_name = self["file_info"].getProperty('processed_data_folder_name')
-
+        Session.init(self)
         self.default_precision = "03"
         self.login = ''
         self.in_house_users = self.getProperty('inhouse_users').split(',')
@@ -48,29 +41,36 @@ class MaxIVSession(Session):
         queue_model_objects.PathTemplate.intersection = self.run_number_intersection
 
     def prepare_directories(self, proposal_info):
-        self.login = proposal_info['Person']['login']
+        msg = "[SDM]MAX IV Session preparing directories for proposal: "
+        msg += "%s, user: %s" % (proposal_info['Proposal']['number'],
+                                 proposal_info['person']['login'])
+        logging.getLogger("HWR").info(msg)
 
-        start_time = proposal_info['Session']['startDate']
-
+        self.login = proposal_info['person']['login']
+        start_time = proposal_info['session']['session']['startDate']
         if start_time:
             start_date = start_time.split(' ')[0].replace('-', '')
         else:
             start_date = time.strftime("%Y%m%d")
-
         self.set_session_start_date(start_date)
+        try:
+            # this checks that the beamline data path has been properly created
+            # e.g. /data/visitor/biomax
+            self.storage = storage.Storage(self.get_user_category(self.login), self.endstation_name)
 
-        # this checks that the beamline data path has been properly created
-        # e.g. /data/visitor/biomax
-        self.storage = storage.Storage(self.get_user_category(self.login), self.endstation_name)
+            # this creates the path for the data and ensures proper permissions.
+            # e.g. /data/visitor/biomax/<propsal>/<visit>/{raw, process}
+            self.storage.create_path(self.proposal_number,
+                                     self.storage.get_proposal_group(self.proposal_number),
+                                     self.get_session_start_date(),
+                                     self.login)
+        except:
+            logging.getLogger("HWR").error("[SDM] Cannot create directory, %s" % sys.exc_info()[1])
 
-        # this creates the path for the data and ensures proper permissions.
-        # e.g. /data/visitor/biomax/<proposal>/<visit>/{raw, process}
-        self.storage.create_path(self, self.proposal_code,
-                                 storage.get_proposal_group(),
-                                 self.get_session_start_date(),
-                                 self.login)
+        logging.getLogger("HWR").info("[SDM] Directories created.")
 
-    def is_inhouse(self, user):
+    def is_inhouse(self, user=None, proposal_number=None):
+>>>>>>> b90c265... sdm related fixes
         """
         Determines if a given user is considered to be inhouse.
 
@@ -85,13 +85,26 @@ class MaxIVSession(Session):
         else:
             return False
 
-    def get_user_category(self):
+    def get_user_category(self, username):
         # missing industrial users
-        if self.is_inhouse():
+        if self.is_inhouse(user=username):
             user_category = 'staff'
         else:
-            user_category = 'visitor'
+            user_category = 'visitors'
         return user_category
+
+    def get_proposal(self):
+        """
+        :returns: The proposal, 'local-user' if no proposal is
+                  available
+        :rtype: str
+        """
+        proposal = 'local-user'
+
+        if self.proposal_code and self.proposal_number:
+            proposal = self.proposal_number
+
+        return proposal
 
     def get_base_data_directory(self):
         """
@@ -109,8 +122,7 @@ class MaxIVSession(Session):
 
         # /data/visitor/biomax/prop/visit/
         # /data/(user-type)/(beamline)/(proposal)/(visit)/raw
-
-        user_category = self.get_user_category()
+        user_category = self.get_user_category(self.login)
 
         # now it is a new visit everyday
         directory = os.path.join(self.base_directory,
