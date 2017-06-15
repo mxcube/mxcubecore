3304ba579bbb357c34f350fbd5df1b3f9716b0fd
bliss administrator <blissadm@sybil.esrf.fr>
2017-06-15 15:32:42 +0200

    added ESRF ID232BeamInfo hardware object

---
+++ HardwareObjects/ESRF/ID232BeamInfo.py
@@ -0,0 +1,31 @@
+import logging
+from HardwareRepository import HardwareRepository
+import BeamInfo
+
+class ID232BeamInfo(BeamInfo.BeamInfo):
+    def __init__(self, *args):
+        BeamInfo.BeamInfo.__init__(self, *args)
+
+    def init(self):
+        self.chan_beam_size_microns = None 
+        self.chan_beam_shape_ellipse = None 
+        BeamInfo.BeamInfo.init(self)
+
+        self.beam_size_slits = map(float,self.getProperty("beam_size_slits").split()) #[0.1, 0.05]
+
+        self.flux = self.getObjectByRole("flux")
+
+    def get_beam_position(self):
+        return 696, 523
+
+    def set_beam_position(self, beam_x, beam_y):
+        return
+
+    def evaluate_beam_info(self,*args):
+        BeamInfo.BeamInfo.evaluate_beam_info(self,*args)
+        self.beam_info_dict["shape"] = "ellipse"
+        return self.beam_info_dict
+
+    def get_flux(self):
+        return self.flux.getCurrentFlux()
+     
