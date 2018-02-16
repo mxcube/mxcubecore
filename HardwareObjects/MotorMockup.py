442ca594c7bba02f210487f4b5c6ea734774baea
IvarsKarpics <ivars.karpics@gmail.com>
2018-02-16 14:05:12 +0100

    [master] Redesigned AbstractMotor

--- HardwareObjects/MotorMockup.py
+++ HardwareObjects/MotorMockup.py
@@ -1,98 +1,96 @@
-from HardwareRepository.BaseHardwareObjects import Device
-import math
-import logging
+#
+#  Project: MXCuBE
+#  https://github.com/mxcube.
+#
+#  This file is part of MXCuBE software.
+#
+#  MXCuBE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MXCuBE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
+
 import time
 import gevent
-import types
-import time
 
-class MotorMockup(Device):      
-    (NOTINITIALIZED, UNUSABLE, READY, MOVESTARTED, MOVING, ONLIMIT) = (0,1,2,3,4,5)
+from AbstractMotor import AbstractMotor, MotorStates
+from HardwareRepository.BaseHardwareObjects import HardwareObject
+
+
+__credits__ = ["The MxCuBE collaboration"]
+__version__ = "2.3."
+__category__ = "Motor"
+
+
+"""
+Example of xml config file
+
+<device class="MotorMockup">
+  <start_position>500</start_position>
+  <velocity>100</velocity>
+</device>
+"""
+
+class MotorMockup(AbstractMotor, HardwareObject):
 
     def __init__(self, name):
-        Device.__init__(self, name)
+        AbstractMotor.__init__(self)
+        HardwareObject.__init__(self, name)
 
-    def init(self): 
-        self.motorState = MotorMockup.READY
-        self.username = self.name()
-        # this is ugly : I added it to make the centring procedure happy
-        self.specName = self.name()
-        self.motorPosition = 0
-        self._move_task = None
-        self.velocity = 100
+        self.__move_task = None
 
-        if self.getProperty("start_position") is not None:
-            self.motorPosition = int(self.getProperty("start_position"))
- 
-    def isReady(self):
+    def init(self):
+        try:
+            self.move(float(self.getProperty("start_position")))
+        except:
+            self.move(10.124)
+
+        try:
+            self.set_velocity(float(self.getProperty("velocity")))
+        except:
+            self.set_velocity(100)
+
+    def is_ready(self):
         return True
 
-    def getState(self):
-        return self.motorState
-    
-    def motorLimitsChanged(self):
-        self.emit('limitsChanged', (self.getLimits(), ))
-                     
-    def getLimits(self):
-        return (-1E3, 1E3)
+    def set_ready(self, task):
+        self.set_state(MotorStates.Ready)
+        self.emit('stateChanged', (self.get_state(), ))
 
-    def getPosition(self):
-        return self.motorPosition
+    def move(self, position, wait=False, timeout=None):
+        self.set_state(MotorStates.Moving)
+        self.__move_task = gevent.spawn(self.move_task, position)
+        self.__move_task.link(self.set_ready)
 
-    def getDialPosition(self):
-        return self.getPosition()
-
-    def _move(self, target_pos):
-        start_pos = self.motorPosition
-        delta = abs(target_pos - start_pos)
-        if target_pos > self.motorPosition:
-          d = 1
-        else:
-          d = -1
-        t0 = time.time()
-        self.emit('stateChanged', (self.motorState, ))           
-        while (time.time() - t0) < (delta / float(self.velocity)):
-          self.motorPosition = start_pos + d*self.velocity*(time.time() - t0)
-          self.emit('positionChanged', (self.motorPosition, ))
-          time.sleep(0.02)
-        self.motorPosition = target_pos
-        self.emit('positionChanged', (target_pos, ))
-
-    def _set_ready(self, task):
-        self.motorState = MotorMockup.READY
-        self.emit('stateChanged', (self.motorState, ))           
-
-    def move(self, position, wait=False):
-        self.motorState = MotorMockup.MOVING
-        self._move_task = gevent.spawn(self._move, position)
-        self._move_task.link(self._set_ready) 
-
-    def moveRelative(self, relativePosition):
-        self.move(self.getPosition() + relativePosition)
-
-    def syncMoveRelative(self, relative_position, timeout=None):
-        return self.syncMove(self.getPosition() + relative_position)
-
-    def waitEndOfMove(self, timeout=None):
-        if self._move_task is not None:
-          with gevent.Timeout(timeout):
-            self._move_task.join()
-
-    def syncMove(self, position, timeout=None):
-        self.move(position)
-        self.waitEndOfMove(timeout)
-
-    def motorIsMoving(self):
-        return self.motorState == 'MOVING'
- 
-    def getMotorMnemonic(self):
-        return self.name()
+    def move_task(self, position, wait=False, timeout=None):
+        start_pos = self.get_position()
+        if start_pos:
+            delta = abs(position - start_pos)
+            if position > self.get_position():
+                direction = 1
+            else:
+                direction = -1
+            start_time = time.time()
+            self.emit('stateChanged', (self.get_state(), ))
+            while (time.time() - start_time) < (delta / self.get_velocity()):
+                self.set_position(start_pos + direction * self.get_velocity() * \
+                                  (time.time() - start_time))
+                self.emit('positionChanged', (self.get_position(), ))
+                time.sleep(0.02)
+        self.set_position(position)
+        self.emit('positionChanged', (self.get_position(), ))
 
     def stop(self):
-        if self._move_task is not None:
-            self._move_task.kill()
+        if self.__move_task is not None:
+            self.__move_task.kill()
 
     def update_values(self):
-        self.emit('stateChanged', (self.motorState, ))
-        self.emit('positionChanged', (self.motorPosition, ))
-
+        self.emit('stateChanged', (self.get_state(), ))
+        self.emit('positionChanged', (self.get_position(), ))
