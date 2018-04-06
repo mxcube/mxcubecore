86016c67cf22d6ccf45ea64775f40b38363b77b8
IvarsKarpics <ivars.karpics@gmail.com>
2018-04-06 14:20:21 +0200

    [master] AbstracAperture and ApertureMockup

--- mxcubecore/HardwareObjects/ApertureMockup.py
+++ mxcubecore/HardwareObjects/ApertureMockup.py
@@ -17,87 +17,30 @@
 #  You should have received a copy of the GNU General Public License
 #  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-"""
-
-import logging
-from HardwareRepository.BaseHardwareObjects import Device
+from AbstractAperture import AbstractAperture
 
 
 __credits__ = ["MXCuBE colaboration"]
 __version__ = "2.2."
 
 
-class ApertureMockup(Device):
-    """
-    Description:	
-    """	
-    POSITIONS = ("BEAM", "OFF", "PARK")
+"""
+xml example:
+
+<device class="ApertureMockup">
+  <position_list>["BEAM", "OFF", "PARK"]</position_list>
+  <diameter_size_list>[5, 10, 20, 30, 50, 100]</diameter_size_list>
+</device>
+"""
+
+
+class ApertureMockup(AbstractAperture):
 
     def __init__(self, name):
-        """
-        Description: Active position is defined as index (int)	
-        """
-        Device.__init__(self, name)
-
-        self.position = None
-        self.current_diameter_index = None
-        self.diameter_list = []
+        AbstractAperture.__init__(self, name)
 
     def init(self):
-        """
-	Description:
-	"""
+        AbstractAperture.init(self)
 
-        self.diameter_list = [5, 10, 20, 30, 50, 100]
-        self.current_diameter_index = 2
-        self.diameter_index_changed(self.current_diameter_index)
-        self.position = "BEAM"
-        self.position_changed(self.position) 
-
-    def get_diameter_size(self):
-        return self.diameter_list[self.current_diameter_index] / 1000.0
-
-    def position_changed(self, position):
-        self.position = position
-        self.emit('positionChanged', position)
-
-    def set_diameter_index(self, diameter_index):
-        self.current_diameter_index = diameter_index
-        self.diameter_index_changed(self.current_diameter_index)
-
-    def set_diameter(self, diameter):
-        self.chan_diameter_index.setValue(self.diameter_list.index(diameter)) 
-
-    def set_position(self, position):
-        self.position = position
-        self.emit('positionChanged', position)
-
-    def set_in(self):
-        self.position = "BEAM"
-        self.emit('positionChanged', position)
-
-    def set_out(self):
-        self.position = "OFF"
-        self.emit('positionChanged', position)
-
-    def is_out(self):
-        return self.position != "BEAM"
-	   	
-    def get_diameter_list(self):
-        return self.diameter_list
-
-    def get_position_list(self):
-        return ApertureMockup.POSITIONS 
-
-    def update_values(self):
-        self.diameter_index_changed(self.current_diameter_index) 
-        self.position_changed(self.position)
-
-    def diameter_index_changed(self, diameter_index):
-        """
-        Descript. :
-        """
-        self.current_diameter_index = diameter_index
-        self.emit('diameterIndexChanged', diameter_index,
-             self.diameter_list[diameter_index] / 1000.0)
+        self.set_position_index(0)
+        self.set_diameter_index(0)
