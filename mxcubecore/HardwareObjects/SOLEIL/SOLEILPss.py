63caebaea46f2b48cf1dde7ecb2d7e7e52e9679c
Martin Savko <martin.savko@gmail.com>
2018-06-07 19:31:56 +0200

    Adding SOLEIL specific hardware objects controlling PSS and safety shutter.

---
+++ mxcubecore/HardwareObjects/SOLEIL/SOLEILPss.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import logging
+import time
+from HardwareRepository.BaseHardwareObjects import Device
+from PyTango import DeviceProxy
+
+'''Complex means we are not using SimpleDevice'''
+
+class SOLEILPss(Device):
+    states = {
+      0:   "not ready",
+      1:   "ready",
+    }
+
+    READ_CMD, READ_OUT = (0,1)
+    
+    def __init__(self, name):
+        Device.__init__(self, name)
+
+        self.wagoState = "unknown"
+        self.__oldValue = None
+        self.device = None
+        self.hutch = None
+
+    def init(self):
+        try:
+            self.device = DeviceProxy(self.getProperty("tangoname")) 
+        except:
+            logging.getLogger("HWR").error("%s: unknown pss device name", self.getProperty("tangoname"))
+
+        if self.getProperty("hutch") not in ("optical", "experimental"):
+            logging.getLogger("HWR").error("TangoPss.init Hutch property %s is not correct",self.getProperty("hutch"))
+        else :
+            self.hutch  = self.getProperty("hutch")
+            self.stateChan = self.getChannelObject("State")
+            self.stateChan.connectSignal("update", self.valueChanged)
+        if self.device:
+            self.setIsReady(True)
+    
+    def get_state(self, value):
+        return SOLEILPss.states[value]
+    
+    def getWagoState(self):
+        return self.get_state(self.stateChan.getValue())
+    
+    def valueChanged(self, value):
+        logging.getLogger("HWR").info("%s: TangoPss.valueChanged, %s", self.name(), value)
+        state = self.get_state(value)
+        self.emit('wagoStateChanged', (state, ))
+            
