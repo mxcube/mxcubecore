b75b3adce7be34b5a16ab7403425fc3026e7af65
IvarsKarpics <ivars.karpics@gmail.com>
2018-11-29 15:22:06 +0100

    fixed imports

--- mxcubecore/HardwareObjects/ALBA/ALBAISPyBClient.py
+++ mxcubecore/HardwareObjects/ALBA/ALBAISPyBClient.py
@@ -6,11 +6,10 @@ import os
 from suds.transport.http import HttpAuthenticated
 from suds.client import Client
 
-import ISPyBClient
-from ISPyBClient import _CONNECTION_ERROR_MSG
+from ISPyBClient import ISPyBClient
 
 
-class ALBAISPyBClient(ISPyBClient.ISPyBClient):
+class ALBAISPyBClient(ISPyBClient):
     def ldap_login(self, login_name, psd, ldap_connection):
         # overwrites standard ldap login  is ISPyBClient.py
         #  to query for homeDirectory
