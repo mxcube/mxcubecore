e0730068bdb9fa417a7f45f8a1a242642995aadc
IvarsKarpics <ivars.karpics@gmail.com>
2018-12-07 09:50:05 +0100

    Added pytest to check beamline setup atributes

---
+++ test/pytest/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+import sys
+import os
+
+TESTS_DIR = os.path.abspath(os.path.dirname(__file__))
+MXCUBE = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+HWR_OBJECT_DIR = os.path.join(MXCUBE, "HardwareObjects")
+HWR_XML_FILES = os.path.join(MXCUBE, "HardwareRepository/test/xml-qt")
+sys.path.insert(0, MXCUBE)
+
+print ("MXCuBE root: %s" % MXCUBE)
+print ("Config path: %s" % HWR_XML_FILES)
+
+from HardwareRepository import HardwareRepository
+
+hwobj_dirs = [HWR_OBJECT_DIR]
+for subdir in ('abstract', 'mockup', 'sample_changer'):
+    hwobj_dirs.append(os.path.join(HWR_OBJECT_DIR, subdir))
+
+HardwareRepository.addHardwareObjectsDirs(hwobj_dirs)
+
+@pytest.fixture(scope="session")
+def hwr():
+    hwr = HardwareRepository.HardwareRepository(HWR_XML_FILES)  
+    hwr.connect()
+    return hwr
+
+@pytest.fixture(scope="session")
+def blsetup(hwr):
+    return hwr.getHardwareObject("beamline-setup")
