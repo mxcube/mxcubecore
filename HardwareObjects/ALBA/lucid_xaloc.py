b560c0ce786dc3f274570f006c9a0cf3dfc50819
Sicilia Developer Controls <sicilia@bl13mxcube.cells.es>
2018-12-17 17:03:41 +0100

    Fix image orientation to fit lucid
    An additional module is used to patch the default
    method. This must be compatible for other sites.

---
+++ HardwareObjects/ALBA/lucid_xaloc.py
@@ -0,0 +1,21 @@
+
+import lucid2
+import Image
+import numpy as np
+
+def find_loop(filename,*args,**kwargs):
+    im = Image.open(filename) 
+    out = im.transpose(Image.FLIP_LEFT_RIGHT)
+    data = np.array(out)
+    coords = lucid2.find_loop(data,*args,**kwargs)
+    print coords
+    x,y = coords[1:]
+    if x > 0 and y > 0: 
+        x = 900 - x
+        coords = ("Coord", x,y)
+    return coords
+
+if __name__ == '__main__':
+   import sys
+   print find_loop(sys.argv[1])
+
