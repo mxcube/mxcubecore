05b2c9f74fe345c1aafa249c52cc2f4bf6939749
IvarsKarpics <ivars.karpics@gmail.com>
2018-12-19 16:08:45 +0100

    Fixed imports 5

--- test/pytest/test_hwr_web.py
+++ test/pytest/test_hwr_web.py
@@ -1,6 +1,6 @@
-from HardwareRepository.HardwareRepository import getHardwareRepository
+from HardwareRepository import HardwareRepository
 
 def test_hwr_web(hwr_web):
-    hwr = getHardwareRepository(hwr_web)
+    hwr = HardwareRepository.getHardwareRepository(hwr_web)
     hwr.connect()
     blsetup_hwobj = hwr.getHardwareObject("beamline-setup")
