c37f746ab3334d30f40b8fa0fb2870961b26b054
IvarsKarpics <ivars.karpics@gmail.com>
2018-12-20 09:19:37 +0100

    Fixed tests

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -4,26 +4,15 @@ import os
 
 TESTS_DIR = os.path.abspath(os.path.dirname(__file__))
 MXCUBE = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
-HWR_OBJECT_DIR = os.path.join(MXCUBE, "HardwareObjects")
-HWR_XML_FILES = os.path.join(MXCUBE, "HardwareRepository/configuration/xml-qt")
+HWR = os.path.join(MXCUBE, "HardwareRepository")
 
 sys.path.insert(0, MXCUBE)
-
-print ("MXCuBE root: %s" % MXCUBE)
-print ("Config path: %s" % HWR_XML_FILES)
-
-hwobj_dirs = [HWR_OBJECT_DIR]
-for subdir in ('abstract', 'mockup', 'sample_changer'):
-    hwobj_dirs.append(os.path.join(HWR_OBJECT_DIR, subdir))
-
-from HardwareRepository.HardwareRepository import getHardwareRepository
+#sys.path.insert(0, HWR)
 
 @pytest.fixture(scope="session")
-def hwr():
-    hwr = getHardwareRepository(HWR_XML_FILES)
-    hwr.connect()
-    return hwr
+def hwr_qt():
+    return os.path.join(HWR, "configuration/xml-qt")
 
 @pytest.fixture(scope="session")
-def blsetup(hwr):
-    return hwr.getHardwareObject("beamline-setup")
+def hwr_web():
+    return os.path.join(HWR, "configuration/xml-qt")
---
+++ test/pytest/test_hwr_qt.py
@@ -0,0 +1,23 @@
+import sys
+import HardwareRepository.QtImport as QtImport
+from HardwareRepository import HardwareRepository
+
+try:
+   from PyQt4.QtGui import QApplication
+except:
+   from PyQt5.QtGui import QApplication
+
+def test_hwr_qt(hwr_qt):
+    app = QtImport.QApplication([])
+
+    hwr = HardwareRepository.getHardwareRepository(hwr_qt)
+    hwr.connect()
+
+    blsetup_hwobj = hwr.getHardwareObject("beamline-setup")
+    role_list = ("transmission", "diffractometer", "omega_axis", "kappa_axis",
+                 "kappa_phi_axis", "sample_changer", "plate_manipulator",
+                 "resolution", "energy", "flux", "beam_info", "shape_history",
+                 "session", "lims_client", "data_analysis", "energyscan",
+                 "collect", "parallel_processing", "xrf_spectrum", "detector")
+    for role in role_list:
+        assert hasattr(blsetup_hwobj, "%s_hwobj" % role)
