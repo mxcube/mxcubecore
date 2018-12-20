dfdcfccceb67d5bd463f8f9bef0f6eae121c71d4
IvarsKarpics <ivars.karpics@gmail.com>
2018-12-20 10:10:48 +0100

    Fixed tests

--- test/pytest/test_hwr_qt.py
+++ test/pytest/test_hwr_qt.py
@@ -1,17 +1,14 @@
 import sys
-import HardwareRepository.QtImport as QtImport
 from HardwareRepository import HardwareRepository
 
 def test_hwr_qt(hwr_qt):
-    app = QtImport.QApplication([])
-
     hwr = HardwareRepository.getHardwareRepository(hwr_qt)
     hwr.connect()
 
     blsetup_hwobj = hwr.getHardwareObject("beamline-setup")
     role_list = ("transmission", "diffractometer", "omega_axis", "kappa_axis",
                  "kappa_phi_axis", "sample_changer", "plate_manipulator",
-                 "resolution", "energy", "flux", "beam_info", "shape_history",
+                 "resolution", "energy", "flux", "beam_info",
                  "session", "lims_client", "data_analysis", "energyscan",
                  "collect", "parallel_processing", "xrf_spectrum", "detector")
     for role in role_list:
