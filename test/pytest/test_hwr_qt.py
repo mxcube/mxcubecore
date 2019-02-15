94739352444abd530050fb23a922665c96b5a7a3
IvarsKarpics <ivars.karpics@gmail.com>
2019-02-15 14:38:20 +0100

    Removed qt import and test energy hwobj

--- test/pytest/test_hwr_qt.py
+++ test/pytest/test_hwr_qt.py
@@ -1,22 +1,11 @@
-import sys
 from HardwareRepository import HardwareRepository
-
-try:
-    from PyQt5.QtGui import QApplication
-except:
-    try:
-        from PyQt4.QtGui import QApplication
-    except:
-        pass
-
-
-main_application = QApplication([])
 
 def test_hwr_qt(hwr_qt):
 
     hwr = HardwareRepository.getHardwareRepository(hwr_qt)
     hwr.connect()
 
+    """
     blsetup_hwobj = hwr.getHardwareObject("beamline-setup")
     role_list = ("transmission", "diffractometer", "omega_axis", "kappa_axis",
                  "kappa_phi_axis", "sample_changer", "plate_manipulator",
@@ -25,12 +14,14 @@ def test_hwr_qt(hwr_qt):
                  "collect", "parallel_processing", "xrf_spectrum", "detector")
     for role in role_list:
         assert hasattr(blsetup_hwobj, "%s_hwobj" % role)
+    """
+    energy_hwobj = hwr.getHardwareObject("energy-mockup")
 
     # Energy hwobj
-    current_energy = blsetup_hwobj.energy_hwobj.get_current_energy()
-    current_wavelength = blsetup_hwobj.energy_hwobj.get_current_wavelength()
-    energy_limits = blsetup_hwobj.energy_hwobj.get_energy_limits()
-    wavelength_limits = blsetup_hwobj.energy_hwobj.get_wavelength_limits()
+    current_energy = energy_hwobj.get_current_energy()
+    current_wavelength = energy_hwobj.get_current_wavelength()
+    energy_limits = energy_hwobj.get_energy_limits()
+    wavelength_limits = energy_hwobj.get_wavelength_limits()
 
     assert isinstance(current_energy, float), "Energy value has to be float"
     assert isinstance(current_wavelength, float), "Energy value has to be float"
@@ -39,6 +30,6 @@ def test_hwr_qt(hwr_qt):
     assert not None in energy_limits, "One or several energy limits is None"
     assert not None in wavelength_limits, "One or several wavelength limits is None"
     assert energy_limits[0] < energy_limits[1], "First value of energy limits has to be the low limit"
-    assert hasattr(blsetup_hwobj.energy_hwobj, "can_move_energy")
-    assert hasattr(blsetup_hwobj.energy_hwobj, "move_energy")
-    assert hasattr(blsetup_hwobj.energy_hwobj, "move_wavelength")
+    assert hasattr(energy_hwobj, "can_move_energy")
+    assert hasattr(energy_hwobj, "move_energy")
+    assert hasattr(energy_hwobj, "move_wavelength")
