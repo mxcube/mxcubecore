eac26d69c897e9fed2ce9e3c4a0f05c5be50c813
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2019-02-19 13:27:07 +0100

    Moved initialization of phase to concrete implementation

--- mxcubecore/HardwareObjects/GenericDiffractometer.py
+++ mxcubecore/HardwareObjects/GenericDiffractometer.py
@@ -195,7 +195,7 @@ class GenericDiffractometer(HardwareObject):
         self.image_height = None
 
         self.current_state = None
-        self.current_phase = GenericDiffractometer.PHASE_CENTRING
+        self.current_phase = None
         self.transfer_mode = None
         self.current_centring_procedure = None
         self.current_centring_method = None
