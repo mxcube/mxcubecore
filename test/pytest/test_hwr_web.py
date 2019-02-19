6d8c0be1210c62b96b120082d998028e80727ba2
IvarsKarpics <ivars.karpics@gmail.com>
2019-02-19 10:39:02 +0100

    Created a test_energy method to test energy hwobj

--- test/pytest/test_hwr_web.py
+++
@@ -1,6 +0,0 @@
-from HardwareRepository import HardwareRepository
-
-def test_hwr_web(hwr_web):
-    hwr = HardwareRepository.getHardwareRepository(hwr_web)
-    hwr.connect()
-    #blsetup_hwobj = hwr.getHardwareObject("beamline-setup")
