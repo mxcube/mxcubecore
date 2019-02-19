dd7b69d5ab4725feff2b3a4eb28f8def64b5e7b2
IvarsKarpics <ivars.karpics@gmail.com>
2019-02-19 10:39:02 +0100

    Created a test_energy method to test energy hwobj

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -7,12 +7,23 @@ MXCUBE = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
 HWR = os.path.join(MXCUBE, "HardwareRepository")
 
 sys.path.insert(0, MXCUBE)
-#sys.path.insert(0, HWR)
+
+from HardwareRepository import HardwareRepository
+
+# There are two different mockup configurations for qt and web
+# An issue #335 to join the configuration is open
+# Meanwhile we create two hardware repositories and do tests
+# against both of them.
+# After solving the issue #355 there should be just one hardware repository
+
+hwr_qt_path = os.path.join(HWR, "configuration/xml-qt")
+hwr_web_path = os.path.join(HWR, "configuration/xml-web")
+hwr_qt = HardwareRepository.getHardwareRepository(hwr_qt_path)
+hwr_web = HardwareRepository.getHardwareRepository(hwr_web_path)
+hwr_qt.connect()
+hwr_web.connect()
 
 @pytest.fixture(scope="session")
-def hwr_qt():
-    return os.path.join(HWR, "configuration/xml-qt")
-
-@pytest.fixture(scope="session")
-def hwr_web():
-    return os.path.join(HWR, "configuration/xml-web")
+def energy_list():
+    return (hwr_qt.getHardwareObject("energy-mockup"),
+            hwr_web.getHardwareObject("energy-mockup"))
