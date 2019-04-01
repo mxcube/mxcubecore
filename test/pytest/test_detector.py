6f83b4d08ca26523c5a639e18091946d584a90f2
IvarsKarpics <ivars.karpics@gmail.com>
2019-04-01 13:54:46 +0200

    fixed typo

--- test/pytest/test_detector.py
+++ test/pytest/test_detector.py
@@ -6,7 +6,7 @@ def test_detector_atributes(detector_list):
         exp_time_limits = detector.get_exposure_time_limits()
         has_shutterless = detector.has_shutterless()
 
-        assert isinstance(current_distance, (int, float)), "Distance value has to be int or float. Now %s, %d" % (str(current_distance))
+        assert isinstance(current_distance, (int, float)), "Distance value has to be int or float. Now %s" % str(current_distance)
         assert isinstance(distance_limits, (list, tuple)), "Distance limits has to be defined as a tuple or list"
         assert not None in distance_limits, "One or several distance limits is None"
         assert distance_limits[0] < distance_limits[1], "First value of distance limits has to be the low limit"
