3d4c4eb34c1817e0528a838cfebcc708ac376184
Antonia Beteva <beteva@esrf.fr>
2019-06-11 16:39:02 +0200

    Concept of simultaneous or not moving. wait_ready in AbstractDiffarctometer. Cleanup, pylint and black.

--- HardwareObjects/Microdiff.py
+++ HardwareObjects/Microdiff.py
@@ -19,7 +19,7 @@
 
 """
 Example xml file
-<object class = "Microdiff"
+<object class = "MicroDiff"
   <username>MD2S</username>
   <exporter_address>wid30bmd2s:9001</exporter_address>
   <exporter_commands>
@@ -51,27 +51,33 @@ Example xml file
   </exporter_channels>
 </object>
 """
-
-import logging
-import gevent
-import math
-import gevent
+from __future__ import absolute_import, division
 import time
+import math
 
-# import AbstractDiffractometer
-from AbstractDiffractometer import AbstractDiffractometer, DiffrHead, DiffrPhase
+from HardwareObjects.abstract.abstract_diffractometer import (
+    AbstractDiffractometer,
+    DiffrHead,
+    DiffrPhase,
+)
 
-import sample_centring
-
-MICRODIFF = None
+__credits__ = ["MXCuBE collaboration"]
 
 
-class Microdiff(AbstractDiffractometer):
+class MicroDiff(AbstractDiffractometer):
+    """ AbstractDiffractometer implementattion for EMBL Microdiff """
+
+    def __init__(self, name):
+        self.exporter_addr = None
+        self.commands = {}
+        self.channels = {}
+        AbstractDiffractometer.__init__(self, name)
+
     def init(self):
+        """ Initisalise the commands and the channels """
         self.exporter_addr = self.getProperty("exporter_address")
 
-        self.commands = {}
-        for name, val in self["exporter_commands"].getProperties().iteritems():
+        for name, val in self["exporter_commands"].getProperties().items():
             self.commands[name] = self.addCommand(
                 {
                     "type": "exporter",
@@ -81,8 +87,7 @@ class Microdiff(AbstractDiffractometer):
                 val,
             )
 
-        self.channels = {}
-        for name, val in self["exporter_channels"].getProperties().iteritems():
+        for name, val in self["exporter_channels"].getProperties().items():
             self.channels[name] = self.addChannel(
                 {
                     "type": "exporter",
@@ -94,20 +99,11 @@ class Microdiff(AbstractDiffractometer):
 
         AbstractDiffractometer.init(self)
 
-    def wait_ready(self, timeout=None):
-        """ Waits when diffractometer status is ready
-        Args:
-            timeout (int): timeout [s]. None means infinite timeout,
-        """
-        if timeout is not None:
-            timeout = self.timeout
-        with gevent.Timeout(
-            timeout, RuntimeError("Timeout waiting for diffractometer to be ready")
-        ):
-            while not self._ready():
-                gevent.sleep(0.5)
-
     def _ready(self):
+        """ Read the state.
+        Returns:
+            (bool): True if ready, False otherwise
+        """
         try:
             hwstate = self.channels["hwstate"].get_value()
         except KeyError:
@@ -128,80 +124,107 @@ class Microdiff(AbstractDiffractometer):
                 self.head_type = val
         return self.head_type
 
-    """ motors handling """
-
-    def move_motors(self, motors_positions_dict, wait=False, timeout=20):
+    def move_motors(
+        self, motors_positions_list, wait=True, timeout=20, simultaneous=True
+    ):
         """ Move simultaneously specified motors to the requested positions
         Args:
-            motors_positions_dict (dict): dictionary with motor names or hwobj
-                            and target values [mm].
+            motors_positions_list (list): list of tuples
+                                   (motor names or hwobj, target values [mm]).
+        Kwargs:
             wait (bool): wait for the move to finish (True) or not (False)
+            timeout (float): wait time [s]
+            simultaneous (bool): Move the motors simultaneously (True) or not.
         Raises:
-            RuntimeError: Timeout
+            RuntimeError: Wrong motor name.
             KeyError: The name does not correspond to an existing motor
         """
         argin = ""
         # be sure the previous command has finished
-        if wait:
-            self.wait_ready()
+        self.wait_ready()
 
-        # prepare the command
-        for mot, pos in motors_positions_dict.iteritems():
-            try:
-                name = self.motors_hwobj[mot].name
-                argin += "%s=%0.3f;" % (name, pos)
-            except KeyError:
-                raise
+        if simultaneous:
+            # prepare the command
+            for mot in motors_positions_list:
+                try:
+                    name = self.motors_hwobj[mot[0]].name
+                    argin += "%s=%0.3f;" % (name, mot[1])
+                except KeyError:
+                    raise RuntimeError("The name does not correspond to an existing motor")
 
-        self.commands["move_motors_sync"](argin)
-        if wait:
-            self.wait_ready(timeout)
+            self.commands["move_motors_sync"](argin)
+        else:
+            AbstractDiffractometer.move_motors(self, motors_positions_list, wait, timeout, simultaneous=False)
 
-    def get_motor_positions(self, motors_positions_list=[]):
+        self.wait_ready(timeout)
+
+    def get_motor_positions(self, motors_list=None):
         """ Get the positions of diffractometer motors. If the
-            motors_positions_list is empty, return the positions of all
+            motors_list is empty, return the positions of all
             the availble motors
         Args:
-            motors_positions_list (list): List of motor names or hwobj
+            motors_list (list): List of motor names or hwobj
         Returns:
-            motors_positions_dict (dict): role:position dictionary
+            (list): list of tuples (motor role, position)
         """
-        motors_positions_dict = AbstractDiffractometer.get_motor_positions(
-            self, motors_positions_list
-        )
+        mot_pos_list = AbstractDiffractometer.get_motor_positions(self, motors_list)
         if not self.in_kappa_mode():
-            motors_positions_dict["kappa"] = None
-            motors_positions_dict["kappa_phi"] = None
-        return motors_positions_dict
-
-    """ data acquisition scans """
+            for tup in mot_pos_list:
+                if "kappa" in tup:
+                    mot_pos_list[mot_pos_list.index(tup)] = ("kappa", None)
+                if "kappa_phi" in tup:
+                    mot_pos_list[mot_pos_list.index(tup)] = ("kappa_phi", None)
+        return mot_pos_list
 
     def check_scan_limits(self, start, end, exptime):
+        """ Check the parameters of a scan
+        Args:
+            start (float): start position
+            end (float): final position
+            exptime (float): exposure time [s]
+        """
         if self.in_plate_mode():
             scan_speed = math.fabs(end - start) / exptime
             low_lim, hi_lim = map(float, self.commands["scan_limits"](scan_speed))
             if start < low_lim:
                 raise ValueError("Scan start below the allowed value %f" % low_lim)
-            elif end > hi_lim:
+            if end > hi_lim:
                 raise ValueError("Scan end abobe the allowed value %f" % hi_lim)
 
-    def set_oscillation_scan(self, start, end, exptime, wait=False):
-        # check the scan limits
-        self.check_scan_limits(start, end, exptime)
-
-        self.channelss["scan_nb_frames"].set(1)
-        scan_params = "1\t%0.3f\t%0.3f\t%0.4f\t1" % (start, (end - start), exptime)
-        self.commands["osc_scan"](scan_params)
-        print("oscil scan started at -----------> %f" % time.time())
-        if wait:
-            self.wait_ready(600)
-        print("finished at ----------> %f" % time.time())
-
-    def set_line_scan(self, start, end, exptime, motors_pos, wait=False):
+    def do_oscillation_scan(self, start, end, exptime, timeout=600):
+        """ Execute oscillation scan
+        Args:
+            start (float): start omega position
+            end (float): final omega position
+            exptime (float): exposure time/frame [s]
+            timeout (int): time wait for the scan to finish [s] (default 10 min)
+        """
         # check the scan limits
         self.check_scan_limits(start, end, exptime)
 
         self.channels["scan_nb_frames"].set(1)
+        scan_params = "1\t%0.3f\t%0.3f\t%0.4f\t1" % (start, (end - start), exptime)
+        self.commands["osc_scan"](scan_params)
+        print("oscil scan started at -----------> %f" % time.time())
+        self.wait_ready(timeout)
+        print("finished at ----------> %f" % time.time())
+
+    def do_line_scan(self, start, end, exptime, motors_pos, timeout=900):
+        """ Execute line (helical) scan
+        Args:
+            start (float): start omega position
+            end (float): final omega position
+            exptime (float): exposure time/frame [s]
+            motors_pos (list): list of two lists - motor positions of
+                               phiy, phiz, sampx, sampy for
+                               start and end point
+            timeout (int): time wait for the scan to finish [s] (default 15 min)
+        """
+        # check the scan limits
+        self.check_scan_limits(start, end, exptime)
+
+        self.channels["scan_nb_frames"].set(1)
+        scan_params = "%0.3f\t%0.3f\t%f\t" % (start, (end - start), exptime)
         scan_params += "%0.3f\t" % motors_pos["1"]["phiy"]
         scan_params += "%0.3f\t" % motors_pos["1"]["phiz"]
         scan_params += "%0.3f\t" % motors_pos["1"]["sampx"]
@@ -213,11 +236,10 @@ class Microdiff(AbstractDiffractometer):
 
         self.commands["helical_scan"](scan_params)
         print("helical scan started at -----------> %f" % time.time())
-        if wait:
-            self.wait_ready(900)  # timeout of 15 min
+        self.wait_ready(timeout)
         print("finished at ----------> %f" % time.time())
 
-    def set_mesh_scan(
+    def do_mesh_scan(
         self,
         start,
         end,
@@ -227,34 +249,49 @@ class Microdiff(AbstractDiffractometer):
         total_nb_frames,
         mesh_centre,
         mesh_range,
-        wait=False,
+            timeout=1800,
     ):
+        """ Execute mesh scan
+        Args:
+            start (float): start omega position
+            end (float): final omega position
+            exptime (float): exposure time/frame [s]
+            dead_time (float): dead time between each point [s]
+            nb_lines (int): number of lines for the scan
+            total_nb_frames (int): total number of frames
+            mesh_centre (list): centre of the mesh position
+            mesh_range (list): horizontal and vertical range values
+            timeout (int): time wait for the scan to finish [s] (default 30 min)
+        """
+        # check the scan limits
+        self.check_scan_limits(start, end, exptime)
 
         self.channels["scan_range"].set_value(end - start)
-        self.channels["scan_exposure_time"].set_value(exptime / mesh_num_lines)
+        self.channels["scan_exposure_time"].set_value(exptime / nb_lines)
         self.channels["scan_start_angle"].set_value(start)
         self.channels["scan_detector_gate_pulse_enabled"].set_value(True)
-        # adding 0.11 ms to the readout time to avoid any servo cycle jitter
+        # adding time [ms] to the readout time to avoid any servo cycle jitter
+        servo_time = 0.11
         self.channels["detector_gate_pulse_readout_time"].set_value(
-            dead_time * 1000 + 0.11
+            dead_time * 1000 + servo_time
         )
 
         # move to the centre of the grid
-        self.move_motors(mesh_center.as_dict())
-        hpos = (
-            self.grid_directions["horizontal_centring"] * mesh_range["horizontal_range"]
-        )
-        vpos = self.grid_directions["centring_y"] * mesh_range["vertical_range"]
+        self.move_motors(mesh_centre)
+        hpos = self.grid_directions["horizontal_centring"] * mesh_range["horizontal"]
+        vpos = self.grid_directions["centring_y"] * mesh_range["vertical"]
         self.motors_hwobj["horizontal_alignment"].rmove(hpos / 2)
         self.motors_hwobj["centring_y"].rmove(vpos / 2)
 
-        scan_params = "%0.3f\t" % -mesh_range["horizontal_range"]
-        scan_params += "%0.3f\t" % mesh_range["vertical_range"]
-        scan_params += "%d\t" % mesh_num_lines
-        scan_params += "%d\t" % (total_nb_frames / nb_lines)
+        scan_params = "%0.3f\t" % -mesh_range["horizontal"]
+        scan_params += "%0.3f\t" % mesh_range["vertical"]
+        scan_params += "%d\t" % nb_lines
+        scan_params += "%d\t" % (total_nb_frames // nb_lines)
         scan_params += "%r" % True
+        """ ???
         scan_params += "%r\t" % True
         scan_params += "%r" % False
+        """
 
         self.commands["mesh_scan"](scan_params)
         print("mesh scan started at -----------> %f" % time.time())
@@ -262,7 +299,14 @@ class Microdiff(AbstractDiffractometer):
             self.wait_ready(1800)  # timeout of 30 min
         print("finished at ----------> %f" % time.time())
 
-    def set_still_scan(self, duration, period, nb_pulses, wait=False):
+    def do_still_scan(self, duration, period, nb_pulses, wait=True):
+        """ Execute mesh scan
+        Args:
+            duration (float): pulse duration [s]
+            period (float): pulse periond [s]
+            nb_pulses (int): number of pulses
+            wait (bool): wait for the scan to finish
+        """
         scan_params = "%0.6f\t%0.6f\t%d" % (duration, period, nb_pulses)
 
         self.commands["still_scan"](scan_params)
@@ -295,20 +339,9 @@ class Microdiff(AbstractDiffractometer):
         """
         phase = self.channels["current_phase"].get_value()
         for i in DiffrPhase:
-                if phase == i.value:
-                    self.current_phase = i
-        return self.current_phase
-
-    def get_head_type(self):
-        """ Get the head type
-        Returns:
-            head_type(enum): Head type
-        """
-        head = self.channels["head_type"].get_value()
-        for i in DiffrHead:
             if phase == i.value:
-                self.head_type = i
-        return self.head_type
+                self.current_phase = i
+        return self.current_phase
 
     def get_pixels_per_mm(self):
         """ Pixel per mm values
@@ -317,8 +350,8 @@ class Microdiff(AbstractDiffractometer):
 
         """
         try:
-            self.pixels_per_mm_x = 1./self.channels["scale_x"].get_value()
-            self.pixels_per_mm_y = 1./self.channels["scale_y"].get_value()
+            self.pixels_per_mm_x = 1. / self.channels["scale_x"].get_value()
+            self.pixels_per_mm_y = 1. / self.channels["scale_y"].get_value()
         except (TypeError, ValueError):
             return None, None
         return self.pixels_per_mm_x, self.pixels_per_mm_y
