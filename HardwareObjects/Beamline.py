09c494498b96281082b0224a9165fca8c23aaf5a
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2019-11-04 16:02:07 +0100

    Adding undulators to beamline object

--- HardwareObjects/Beamline.py
+++ HardwareObjects/Beamline.py
@@ -112,6 +112,9 @@ class Beamline(ConfiguredObject):
         # int Starting run number for path_template
         self.run_number = 1
 
+        # List of undelators
+        self.undelators = []
+
     def init(self):
         """Object initialisation - executed *after* loading contents"""
 
