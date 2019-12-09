1022af58446af4571a38e3a20cb6936c2d553e98
Martin Savko <savko@synchrotron-soleil.fr>
2019-12-09 19:01:16 +0100

    AbstractTransmission -- base class AbstractMotor instead of Device. This allows to remove most of the code as they are already implemented in the base class.

--- HardwareObjects/abstract/AbstractTransmission.py
+++ HardwareObjects/abstract/AbstractTransmission.py
@@ -1,67 +1,61 @@
-#
+# -*- coding: utf-8 -*-
 #  Project: MXCuBE
 #  https://github.com/mxcube.
 #
 #  This file is part of MXCuBE software.
 #
 #  MXCuBE is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
+#  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  MXCuBE is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#  GNU Lesser General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
+#  You should have received a copy of the GNU General Lesser Public License
 #  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Base class for transmission control."""
 
 import abc
-from HardwareRepository.BaseHardwareObjects import Device
+from HardwareRepository.HardwareObject.abstract.AbstractMotor import AbstractMotor
+
+__copyright__ = """ Copyright © 2019 by the MXCuBE collaboration """
+__license__ = "LGPLv3+"
 
 
-__credits__ = ["MXCuBE collaboration"]
-__version__ = "2.3"
-
-
-class AbstractTransmission(Device, object):
+class AbstractTransmission(AbstractMotor):
     """
     Base class for transmission control by filters, slits, apertures or other means
     """
+
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, name):
-        Device.__init__(self, name)
+        AbstractMotor.__init__(self, name)
 
         self._value = None
         self._limits = [0, 100]
         self._state = None
 
     @abc.abstractmethod
-    def set_value(self, value, timeout=None):
+    def set_value(self, value, wait=True, timeout=None):
+        """Set transmission Move motor to absolute position.
+           Wait for the move to finish by default.
+        Args:
+            value (float): target position
+            wait (bool): optional - wait until all movements finished.
+            timeout (float): optional - timeout [s].
+        """
         self._value = value
         self.emit("valueChanged", self._value)
 
+    @abc.abstractmethod
     def get_value(self):
-        return self._value
-
-    def set_limits(self, limits):
-        self._limits = limits
-        self.emit("limitsChanged", (self._limits,))
-
-    def get_limits(self):
-        return self._limits
-
-    def set_state(self, state):
-        self._state = state
-        self.emit("stateChanged", self._state)
-
-    def get_state(self):
-        return self._state
-
-    def update_values(self):
-        self.emit("valueChanged", self._value)
-        self.emit("limitsChanged", (self._limits,))
-        self.emit("stateChanged", self._state)
+        """Get the current transmission in percents
+        Returns:
+            float: current transmission.
+        """
+        return None
