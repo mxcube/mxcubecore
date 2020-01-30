f00b9b500522f3fc02e9ba7bc2060baa803a17b5
bliss administrator <blissadm@egg.esrf.fr>
2020-01-30 15:31:13 +0100

    AbstractMotor inheriting AbstractActuator as discussed during the last MXCuBE DEV meeting

--- HardwareObjects/abstract/AbstractMotor.py
+++ HardwareObjects/abstract/AbstractMotor.py
@@ -23,8 +23,7 @@
 import abc
 from enum import IntEnum, unique
 from gevent import Timeout
-from gevent.event import Event
-from HardwareRepository.BaseHardwareObjects import HardwareObject
+from HardwareRepository.HardwareObjects.abstract.AbstractActuator import AbstractActuator
 
 __copyright__ = """ Copyright © 2019 by the MXCuBE collaboration """
 __license__ = "LGPLv3+"
@@ -33,67 +32,30 @@ __license__ = "LGPLv3+"
 @unique
 class MotorStates(IntEnum):
     """Motor states definitions."""
-
-    UNKNOWN = 0
-    WARNING = 1
-    BUSY = 2
-    READY = 3
-    FAULT = 4
     HOME = 5
-    LIMPOS = 6
-    LIMNEG = 7
+    LOWLIMIT = 6
+    HIGHLIMIT = 7
 
 
-class AbstractMotor(HardwareObject):
+class AbstractMotor(AbstractActuator):
     """Abstract motor API"""
 
     __metaclass__ = abc.ABCMeta
-    READY_STATES = (
-        MotorStates.READY,
-        MotorStates.LIMPOS,
-        MotorStates.LIMNEG,
-        MotorStates.HOME,
-    )
     unit = None
 
+    SPECIFIC_STATES = MotorStates
+
     def __init__(self, name):
-        HardwareObject.__init__(self, name)
+        AbstractActuator.__init__(self, name)
 
         self._state = MotorStates.UNKNOWN
-        self._nominal_value = None
-        self._limits = (None, None)
         self._velocity = None
         self._tolerance = None
-        self.motor_name = None
-        self.username = None
-        self.read_only = False
-        self.default_value = None
-        self._ready_event = None
 
     def init(self):
         """Initialise some parametrs."""
-        self.motor_name = self.getProperty("motor_name")
-        self.username = self.getProperty("username") or self.motor_name
         self._tolerance = self.getProperty("tolerance") or 1e-3
-        self.read_only = self.getProperty("read_only") or False
-        self.default_value = self.getProperty("default_value")
-        self._ready_event = Event()
-
-    def is_ready(self):
-        """Check if the motor state is READY.
-        Returns:
-            (bool): True if ready, otherwise False.
-        """
-        return self._ready_event.is_set()
-
-    @abc.abstractmethod
-    def get_state(self):
-        """Get the motor state
-        Returns:
-            (enum 'MotorStates'): Motor state.
-        """
-        return None
-
+        
     @abc.abstractmethod
     def get_value(self):
         """Read the motor position.
@@ -101,21 +63,6 @@ class AbstractMotor(HardwareObject):
             float: Motor position.
         """
         return None
-
-    def get_limits(self):
-        """Return motor low and high limits.
-        Returns:
-            (tuple): two floats tuple (low limit, high limit).
-        """
-        return self._limits
-
-    def set_limits(self, limits):
-        """Set motor low and high limits.
-        Args:
-            limits (tuple): two floats tuple (low limit, high limit).
-        """
-        self._limits = limits
-        self.emit("limitsChanged", (self._limits,))
 
     def get_velocity(self):
         """Read motor velocity.
@@ -140,19 +87,6 @@ class AbstractMotor(HardwareObject):
             timeout (float): optional - timeout [s].
         """
 
-    def set_value(self, value, wait=True, timeout=None):
-        """Move motor to absolute value. Wait the move to finish.
-        Args:
-            value (float): target value
-            wait (bool): optional - wait until motor movement finished.
-            timeout (float): optional - timeout [s].
-        """
-        self._set_value(value)
-        self.update_value()
-
-        if wait:
-            self.wait_ready(timeout)
-
     def set_value_relative(self, relative_value, wait=False, timeout=None):
         """Move to value relative to the current. Wait the move to finish.
         Args:
@@ -161,24 +95,6 @@ class AbstractMotor(HardwareObject):
             timeout (float): optional - timeout [s].
         """
         self.move(self.get_value() + relative_value, wait, timeout)
-
-    def wait_ready(self, timeout=None):
-        """Wait until event ready
-        Args:
-            (float): timeout [s]
-        Raises:
-            RuntimeError: Timeout waiting for status ready.
-        """
-        with Timeout(timeout, RuntimeError("Timeout waiting for status ready")):
-            self._ready_event.wait(timeout=timeout)
-
-    def abort(self):
-        """Abort the motor movement immediately."""
-        raise NotImplementedError
-
-    def stop(self):
-        """Stop the motor movement"""
-        raise NotImplementedError
 
     def home(self, timeout=None):
         """Homing procedure.
@@ -203,38 +119,3 @@ class AbstractMotor(HardwareObject):
 
         self._nominal_value = value
         self.emit("valueChanged", (self._nominal_value,))
-
-    def update_state(self, state=None):
-        """Check if the state has changed. Emist signal stateChanged.
-        Args:
-            state (enum 'MotorState'): state
-        """
-        if state != self._state:
-            if state is None:
-                state = self.get_state()
-            if state in self.READY_STATES:
-                self._ready_event.set()
-            else:
-                self._ready_event.clear()
-
-            self._state = state
-            self.emit("stateChanged", (self._state,))
-
-    def update_limits(self, limits=None):
-        """Check if the limits have changed. Emist signal limitsChanged.
-        Args:
-            limits (tuple): two floats tuple (low limit, high limit).
-        """
-        if limits is None:
-            limits = self.get_limits()
-
-        if all(limits):
-            self._limits = limits
-            self.emit("limitsChanged", (self._limits,))
-
-    def update_values(self):
-        """ Reemits all signals
-        """
-        self.update_value()
-        self.update_state()
-        self.update_limits()
