88a9b3441aa939baa58e575f36361de209bfd0ed
bliss administrator <blissadm@egg.esrf.fr>
2020-02-27 15:19:22 +0100

    Fixed typo

--- mxcubecore/HardwareObjects/abstract/AbstractDetector.py
+++ mxcubecore/HardwareObjects/abstract/AbstractDetector.py
@@ -53,8 +53,9 @@ class AbstractDetector(HardwareObject):
         """Initialise some common paramerters"""
         self.width = self.getProperty("width")
         self.height = self.getProperty("height")
+
         try:
-            self._det_metadata = self.getProperties("beam")
+            self._det_metadata = self["beam"]
         except KeyError:
             pass
 
