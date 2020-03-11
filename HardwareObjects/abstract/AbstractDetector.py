79d2d2aafa470880ea00a4babe8c38b191c7963e
rhfogh <rhfogh@globalphasing.com>
2020-03-11 14:43:17 +0000

    Fixed bugs and merging errors.

--- HardwareObjects/abstract/AbstractDetector.py
+++ HardwareObjects/abstract/AbstractDetector.py
@@ -106,6 +106,11 @@ class AbstractDetector(HardwareObject):
         Stops acquisition
         """
 
+    def wait_ready(self):
+        """
+        Blocks until detector is ready
+        """
+
     def get_roi_mode(self):
         """
         Returns:
