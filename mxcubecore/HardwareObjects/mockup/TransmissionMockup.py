6e8a936badd37aa93675044dd346da7e88fb0017
Jordi Andreu <jandreu@cells.es>
2020-04-02 12:38:52 +0200

    [WIP] Refactor TransmissionMockup class

--- mxcubecore/HardwareObjects/mockup/TransmissionMockup.py
+++ mxcubecore/HardwareObjects/mockup/TransmissionMockup.py
@@ -1,46 +1,64 @@
-from HardwareRepository.BaseHardwareObjects import Device
+#
+#  Project: MXCuBE
+#  https://github.com/mxcube.
+#
+#  This file is part of MXCuBE software.
+#
+#  MXCuBE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MXCuBE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
+
+__credits__ = ["The MxCuBE collaboration"]
+__copyright__ = """ Copyright 2019 by the MXCuBE collaboration """
+__license__ = "LGPLv3+"
+__version__ = "2.3."
+__api__ = "3"
+__category__ = "Actuator"
 
 
-class TransmissionMockup(Device):
+from HardwareRepository.TaskUtils import task
+from HardwareRepository.HardwareObjects.abstract.AbstractActuator import \
+    (AbstractActuator)
+
+
+class TransmissionMockup(AbstractActuator):
+
     def __init__(self, name):
-        Device.__init__(self, name)
-
-        self.labels = []
-        self.bits = []
-        self.attno = 0
-        self.value = 100
+        AbstractActuator.__init__(self, name)
 
     def init(self):
-        pass
-
-    # remove following
-    def getAttState(self):
-        return 0
-
-    # remove following
-    def setAttFactor(self, value):
-        self.value = value
-        self.emit("valueChanged", self.value)
+        AbstractActuator.init(self)
+        self.log.debug("Initializing")
+        self.update_limits((0, 1))
+        self.update_state(self.STATES.READY)
 
     def get_value(self):
-        return self.value
+        """Read the transmission value.
+        Returns:
+            Normalized transmission value.
+        """
+        return self._nominal_value
 
+    @task
     def _set_value(self, value):
-        self.setAttFactor(value)
+        """
+        Sets the hardware to target a given transmission.
+        Args:
+            value: target normalized transmission value
+        """
+        self._nominal_value = value
 
-    def connected(self):
-        self.setIsReady(True)
-
-    def disconnected(self):
-        self.setIsReady(False)
-
-    # remove following
-    def attStateChanged(self, channelValue):
+    def abort(self):
         pass
 
-    # remove following
-    def attFactorChanged(self, channelValue):
-        pass
-
-    def is_ready(self):
-        return True
+    def get_state(self):
+        return self.STATES.READY
