281f9b17387e76900f995ad31f38f05457cacb1f
JB <jbflorial2@gmail.com>
2020-04-02 13:39:50 +0100

    Add test method (empty for now)

--- test/pytest/test_sample_changer.py
+++ test/pytest/test_sample_changer.py
@@ -6,3 +6,25 @@ def test_sample_change_init(beamline):
 
 def test_sample_changer_load(beamline):
     pass
+
+def test_sample_change_unload(beamline):
+  pass
+
+
+def test_sample_change_select(beamline):
+  pass
+
+def test_sample_change_abort(beamline):
+  pass
+
+def test_sample_change_get_state(beamline):
+  pass
+
+def test_sample_change_get_status(beamline):
+  pass
+
+def test_sample_change_has_loaded_sample(beamline):
+  pass
+
+def test_sample_change_get_loaded_sample(beamline):
+  pass
