08d78bb5fdbae1559aae49edc5a503909c5ed714
JB <jbflorial2@gmail.com>
2020-04-03 12:24:23 +0100

    Add  test to be check

--- test/pytest/test_sample_changer.py
+++ test/pytest/test_sample_changer.py
@@ -1,8 +1,3 @@
-def test_sample_change_init(beamline):
-    assert (
-        not beamline.sample_changer is None
-    ), "Sample changer hardware objects is None (not initialized)"
-
 
 def test_sample_changer_load(beamline):
     beamline.sample_changer.load((1, 1))
@@ -15,19 +10,31 @@ def test_sample_has_loaded_sample(beamline):
     assert(not beamline.sample_changer.has_loaded_sample())
 
 def test_sample_changer_unload(beamline):
-    pass
+    assert(beamline.sample_changer.unload() is not None)
+    assert (
+       not beamline.sample_changer.has_loaded_sample() is None
+    ), "Sample changer  has_loaded_sample() must be  None (not initialized)"
+
 
 def test_sample_changer_select(beamline):
     pass
 
 def test_sample_changer_abort(beamline):
-    pass
+    beamline.sample_changer.abort()
+    assert(beamline.sample_changer.task_proc is None)
+
 
 def test_sample_changer_get_state(beamline):
-    pass
+   state = beamline.sample_changer.get_state()
+   assert(not beamline.sample_changer. get_state() is None)
+   assert(not beamline.sample_changer.SampleChangerState.STATE_DESC.get(state, "Unknown"))
 
 def test_sample_changer_get_status(beamline):
-    pass
+    assert(beamline.sample_changer.get_status is not None)
 
 def test_sample_changer_get_loaded_sample(beamline):
-    pass
+    loaded_samples = len(beamline.sample_changer.get_sample_list())
+    assert (
+        loaded_samples >= 0
+    ), "Sample changer len(get_loaded_sample) must be >= 0 but is None (not initialized)"
+    assert(beamline.sample_changer.get_loaded_sample())
