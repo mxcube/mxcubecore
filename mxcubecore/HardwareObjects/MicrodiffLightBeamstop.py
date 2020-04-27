68b55e9c8148c33b43226556c98e42e6fb47943e
Antonia Beteva <beteva@esrf.fr>
2020-04-27 16:07:53 +0200

    Inherit from ExporterNState.

--- mxcubecore/HardwareObjects/MicrodiffLightBeamstop.py
+++ mxcubecore/HardwareObjects/MicrodiffLightBeamstop.py
@@ -1,27 +1,72 @@
-from MicrodiffInOut import MicrodiffInOut
+"""
+Combine setting the back light IN with moving the beamstop to a safety
+position. This is used to set the back light in/out faster, than using
+the phase CENTRING - the beamstop motor moves only if needed.
+Example xml file:
+<device class="MicrodiffLightBeamstop">
+  <username>Back Light</username>
+  <exporter_address>wid30bmd2s:9001</exporter_address>
+  <actuator_name>BackLightIsOn</actuator_name>
+  <values>{"IN": True, "OUT": False}</values>
+  <use_hwstate>True</use_hwstate>
+  <object role="beamstop" href="/udiff_bstopx"/>
+  <safety_position>38</safety_position>
+</device>
+"""
+
+from HardwareRepository.HardwareObjects.ExporterNState import ExporterNState
+
+__copyright__ = """ Copyright © 2010-2020 by the MXCuBE collaboration """
+__license__ = "LGPLv3+"
 
 
-class MicrodiffLightBeamstop(MicrodiffInOut):
+class MicrodiffLightBeamstop(ExporterNState):
+    """Control backlight, move the beamstop to safety position"""
+
     def __init__(self, name):
-        MicrodiffInOut.__init__(self, name)
-        self.save_position = None
+        super(MicrodiffLightBeamstop, self).__init__(self, name)
+        self.safety_position = None
+        self._beamstop_obj = None
+        self._saved_value = None
 
     def init(self):
-        MicrodiffInOut.init(self)
-        self.beamstop = self.getObjectByRole("beamstop")
-        try:
-            self.safety_position = float(self.getProperty("safety_position"))
-        except TypeError:
-            self.safety_position = 38
+        """Initialize the light and the beamstop object"""
+        super(MicrodiffLightBeamstop, self).init(self)
 
-    def actuatorIn(self, wait=True, timeout=None):
-        pos = self.beamstop.get_value()
-        if pos < self.safety_position:
-            self.save_position = pos
-            self.beamstop.set_value(self.safety_position, wait=True)
-        MicrodiffInOut.actuatorIn(self, wait=True, timeout=None)
+        # for now the beamstop only moves in X directiron.
+        self._beamstop_obj = self.getObjectByRole("beamstop")
+        self.safety_position = float(self.getProperty("safety_position", 38.0))
 
-    def actuatorOut(self, wait=True, timeout=None):
-        MicrodiffInOut.actuatorOut(self, wait=True, timeout=None)
-        if self.save_position:
-            self.beamstop.set_value(self.save_position, wait=True)
+    def _set_value(self, value):
+        """Set device to value. Move the beamstop, if needed.
+        Args:
+            value (enum): Value to be set.
+        """
+        # move the beamstop backwords before setting the back light in
+        if self._beamstop_obj:
+            self.handle_beamstop(value.name)
+
+        super(MicrodiffLightBeamstop, self)._set_value(value)
+
+        # move the beamstop to the previous position after extracting
+        # the back light
+        if self._beamstop_obj:
+            self.handle_beamstop(value.name)
+
+    def handle_beamstop(self, value):
+        """ Move the beamstop as function of the value of the back light.
+        Args:
+            (str): value name
+        """
+        if value == "IN":
+            _pos = self._beamstop_obj.get_value()
+            # only move if the beamstop is closer than the sefety_position
+            if _pos < self.safety_position:
+                self._saved_value = _pos
+                self._beamstop_obj.set_value(self.safety_position, timeout=60)
+        elif value == "OUT":
+            if self._saved_value:
+                self._beamstop_obj.set_value(self._saved_value, timeout=60)
+        else:
+            # do nothing
+            pass
