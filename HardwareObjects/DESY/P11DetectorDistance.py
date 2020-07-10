4b346ec1e3db63ea24e0b712490c6d62ee58df3c
Jan Meyer <jan.meyer@desy.de>
2020-07-10 15:02:09 +0200

    DESY P11 Hardware Objects

---
+++ HardwareObjects/DESY/P11DetectorDistance.py
@@ -0,0 +1,154 @@
+
+__credits__ = ["DESY P11"]
+__license__ = "LGPLv3+"
+__category__ = "Motor"
+
+from HardwareRepository.HardwareObjects.abstract.AbstractMotor import AbstractMotor
+
+from HardwareRepository.utils.mxcube_logging import log
+
+class P11DetectorDistance(AbstractMotor):
+
+    def __init__(self,name):
+        AbstractMotor.__init__(self, name)
+
+        self.interlock_set = None
+        self.limits = None
+
+        self.chan_position = None
+        self.chan_state = None
+        self.chan_min_value = None
+        self.chan_max_value = None
+        self.chan_interlock_state = None
+        self.cmd_stop = None
+
+    def init(self):
+        self.chan_state = self.get_channel_object('axisState')
+        if self.chan_state is not None:
+           self.chan_state.connectSignal("update", self._set_state)
+        self._set_state()
+
+        self.chan_position = self.get_channel_object('axisPosition')
+        if self.chan_position is not None:
+            self.chan_position.connectSignal("update", self.update_value)
+        self.update_value()
+
+        self.chan_min_value = self.get_channel_object('axisMinValue')
+        self.chan_max_value = self.get_channel_object('axisMaxValue')
+
+        self.chan_interlock_state = self.get_channel_object('interlockState')
+        if self.chan_interlock_state is not None:
+           self.chan_interlock_state.connectSignal("update", self.interlock_state_changed)
+        self.interlock_state_changed()
+
+        self.cmd_stop = self.get_command_object("stopAxis")
+        if self.cmd_stop:
+            self.cmd_stop.connectSignal("connected", self.connected)
+            self.cmd_stop.connectSignal("disconnected", self.disconnected)
+
+    def connectNotify(self, signal):
+        """
+        :param signal: signal
+        :type signal: signal
+        """
+        if self.connected():
+            if signal == "stateChanged":
+                self.update_state()
+            elif signal == "limitsChanged":
+                self.update_limits()
+            elif signal == "valueChanged":
+                self.update_value()
+
+    def connected(self):
+        """
+        Sets ready
+        :return:
+        """
+        self._set_state()
+        #self.update_state(self.STATES.READY)
+
+    def disconnected(self):
+        """
+        Sets not ready
+        :return:
+        """
+        self.update_state(self.STATES.OFF)
+
+    def _set_state(self, state=None):
+
+        if state is None:
+            _state = self.chan_state.getValue()
+        else:
+            _state = state
+
+        _state = str(_state)
+        
+        if self.interlock_set is None:
+            self.update_interlock_state()
+
+        if not self.interlock_set:
+            state = self.STATES.FAULT
+            log.debug("P11 Detector Distance is FAULT because interlock is not set")
+        else:
+            if _state == 'ON':
+                state = self.STATES.READY
+            elif _state == 'MOVING':
+                state = self.STATES.BUSY
+            else:
+                state = self.STATES.FAULT
+        
+        self.update_state(state)
+        return state
+
+    def get_value(self):
+        return self.chan_position.getValue()
+
+    def update_value(self, value=None):
+        """Updates motor position
+        """
+        if value is None:
+            value = self.chan_position.getValue()
+
+        super(P11DetectorDistance, self).update_value(value)
+
+    def _set_value(self, value):
+        """
+        Main move method
+        :param value: float
+        :return:
+        """
+        ##if self.chan_state is not None:
+            #self.update_state(self.STATES.BUSY)
+            
+        self.chan_position.setValue(value)
+
+    def get_limits(self):  
+        min_value = self.chan_min_value.getValue()
+        max_value = self.chan_max_value.getValue()
+        return [min_value, max_value]
+
+    def abort(self):
+        """Stops motor movement
+        """
+        self.cmd_stop()
+
+
+    def get_motor_mnemonic(self):
+        """
+        Returns motor mnemonic
+        :return:
+        """
+        return "DetectorDistance"
+
+    def update_interlock_state(self, state=None):
+        if self.chan_interlock_state is None:
+            return
+
+        if state is None:
+            state = self.chan_interlock_state.getValue()
+        self.interlock_set = state
+        log.debug("P11 DetectorDistance / INTERLOCK is %s" % (self.interlock_set and "SET" or "NOT SET") )
+  
+    def interlock_state_changed(self, state=None):
+        self.update_interlock_state(state)   
+        self._set_state() 
