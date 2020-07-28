2767f33bbf17bb6c9572d52eee148a176dc3c197
IvarsKarpics <ivars.karpics@gmail.com>
2020-07-28 11:11:43 +0200

    fixed licence header

--- mxcubecore/HardwareObjects/queue_model_objects.py
+++ mxcubecore/HardwareObjects/queue_model_objects.py
@@ -1,21 +1,22 @@
-#
+# encoding: utf-8
+# 
 #  Project: MXCuBE
-#  https://github.com/mxcube.
+#  https://github.com/mxcube
 #
 #  This file is part of MXCuBE software.
 #
 #  MXCuBE is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
+#  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  MXCuBE is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#  GNU Lesser General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with MXCuBE. If not, see <http://www.gnu.org/licenses/>.
 
 """
 This module contain objects that combined make up the data model.
@@ -33,6 +34,10 @@ except ImportError:
 
 from HardwareRepository.HardwareObjects import queue_model_enumerables
 from HardwareRepository import HardwareRepository as HWR
+
+
+__copyright__ = """ Copyright © 2010 - 2020 by MXCuBE Collaboration """
+__license__ = "LGPLv3+"
 
 
 class TaskNode(object):
