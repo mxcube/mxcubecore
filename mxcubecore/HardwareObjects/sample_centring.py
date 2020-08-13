4b077e9239fd0398fa369564264aa19b861cea5b
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2020-08-13 10:34:41 +0200

    Passing chi_angle to find_loop

--- mxcubecore/HardwareObjects/sample_centring.py
+++ mxcubecore/HardwareObjects/sample_centring.py
@@ -535,7 +535,8 @@ def find_loop(camera, pixelsPerMm_Hor, chi_angle, msg_cb, new_point_cb):
     )
     camera.take_snapshot(snapshot_filename, bw=True)
 
-    info, x, y = lucid.find_loop(snapshot_filename, IterationClosing=6)
+    info, x, y = lucid.find_loop(
+        snapshot_filename, rotation=-chi_angle, debug=False, IterationClosing=6)
 
     try:
         x = float(x)
