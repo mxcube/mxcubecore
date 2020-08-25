e2d77a8ca07a66dff9492cf412e6f8c5fff6f7c4
Lais Pessine do Carmo <lais.carmo@lnls.br>
2020-08-25 10:01:12 -0300

    Fix movement check in LNLSDetDistMotor

--- HardwareObjects/LNLS/LNLSDetDistMotor.py
+++ HardwareObjects/LNLS/LNLSDetDistMotor.py
@@ -19,11 +19,16 @@ class LNLSDetDistMotor(LNLSMotor):
                              )
         self.update_specific_state(self.SPECIFIC_STATES.MOVING)
 
+        for i in range(2):
+            logging.getLogger(
+                "user_level_log").info('Setting detector distance...'
+            )
+
         # Enable air
         logging.getLogger("HWR").info('%s: Enabling air' % self.motor_name)
         command = '#5,7,8j/'
         self.set_channel_value(self.PMAC_SENDCMD, command)
-        time.sleep(1)
+        time.sleep(2)
 
         # Wait for stability
         logging.getLogger("HWR").info('%s: Wait for air stability' % self.motor_name)
@@ -38,12 +43,14 @@ class LNLSDetDistMotor(LNLSMotor):
 
         # Wait for movement
         logging.getLogger("HWR").info('%s: Wait for movement to finish' % self.motor_name)
-        while (self.get_channel_value(self.MOTOR_DMOV) == 0):
+        while (self.get_channel_value(self.MOTOR_DMOV) == 0
+        or abs(current_value - value) > 0.0005):
             time.sleep(0.2)
             current_value = self.get_value()
             self.update_value(current_value)
 
         # Disable air
+        time.sleep(1)
         logging.getLogger("HWR").info('%s: Disabling air' % self.motor_name)
         command = '#5,7,8dkill'
         self.set_channel_value(self.PMAC_SENDCMD, command)
@@ -61,6 +68,10 @@ class LNLSDetDistMotor(LNLSMotor):
         # Wait for stability
         logging.getLogger("HWR").info('%s: Movement done!' % self.motor_name)
         self.update_state(self.STATES.READY)
+        for i in range(2):
+            logging.getLogger(
+                "user_level_log").info('Detector distance is set.'
+            )
         return value
 
     def _set_value(self, value):
