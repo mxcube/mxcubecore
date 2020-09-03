f35b757860472e123ac52d972b83c2e9ed32f7e2
bliss administrator <blissadm@id30a3control.esrf.fr>
2020-09-03 14:18:18 +0200

    Improved get available gripper handling

--- HardwareObjects/FlexHCDMaintenance.py
+++ HardwareObjects/FlexHCDMaintenance.py
@@ -121,13 +121,6 @@ class FlexHCDMaintenance(Equipment):
            [ cmd_name,  display_name, category ]
         """
         """ [cmd_id, cmd_display_name, nb_args, cmd_category, description ] """
-        grippers = self._sc.get_available_grippers()
-        gripper_cmd_list = []
-
-        for gripper in grippers:
-            arg = list(self._sc.gripper_types.keys())[list(self._sc.gripper_types.values()).index(gripper)]
-            gripper_cmd_list.append(["change_gripper", gripper.title().replace("_", " "), "Gripper", arg])
-
         cmd_list = [
             [
                 "Actions",
@@ -137,11 +130,25 @@ class FlexHCDMaintenance(Equipment):
                     ["reset_sample_number", "Reset sample number", "Actions", None],
                     ["abort", "Abort", "Actions", None],
                 ],
-            ],
-            [
-                "Gripper: %s" % self._sc.get_gripper().title().replace("_", " "), gripper_cmd_list,
             ]
         ]
+
+        try:
+            grippers = self._sc.get_available_grippers()
+        except Exception:
+            pass
+        else:
+            gripper_cmd_list = []
+
+            for gripper in grippers:
+                arg = list(self._sc.gripper_types.keys())[list(self._sc.gripper_types.values()).index(gripper)]
+                gripper_cmd_list.append(["change_gripper", gripper.title().replace("_", " "), "Gripper", arg])
+
+            grippers_cmd = ["Gripper: %s" % self._sc.get_gripper().title().replace("_", " "), gripper_cmd_list,]
+
+            cmd_list.append(grippers_cmd)
+
+
         return cmd_list
 
     def send_command(self, cmdname, args=None):
