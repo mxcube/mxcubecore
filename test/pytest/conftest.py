d11baa5ba59bc98c0178d95ab56edee8735c0852
IvarsKarpics <ivars.karpics@gmail.com>
2021-03-08 20:29:13 +0100

    Fixed tests

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -8,10 +8,9 @@ from gevent import monkey
 monkey.patch_all(thread=False)
 
 TESTS_DIR = os.path.abspath(os.path.dirname(__file__))
-MXCUBE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
-HWR_DIR = os.path.join(MXCUBE_DIR, "HardwareRepository")
+MXCUBE_CORE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
-sys.path.insert(0, MXCUBE_DIR)
+sys.path.insert(0, MXCUBE_CORE_DIR)
 
 
 # hwr_path = os.path.join(HWR_DIR, "configuration/test")
@@ -27,10 +26,11 @@ sys.path.insert(0, MXCUBE_DIR)
 @pytest.fixture(scope="function")
 def beamline():
     hwr_path = "%s%s%s" % (
-        os.path.join(HWR_DIR, "configuration/mockup"),
-        os.path.pathsep,
-        os.path.join(HWR_DIR, "configuration/mockup/test")
+        os.path.join(MXCUBE_CORE_DIR, "configuration/mockup"),
+        ":",
+        os.path.join(MXCUBE_CORE_DIR, "configuration/mockup/test")
     )
+    print(hwr_path)
     HWR._instance = HWR.beamline = None
     HWR.init_hardware_repository(hwr_path)
     hwr = HWR.get_hardware_repository()
