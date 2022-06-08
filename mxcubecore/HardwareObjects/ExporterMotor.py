70a2485eb9492e6fd60c606c0ddd06fa1cc61e34
bliss administrator <blissadm@id30a1control.esrf.fr>
2022-06-08 08:43:52 +0200

    Removed name method override

--- mxcubecore/HardwareObjects/ExporterMotor.py
+++ mxcubecore/HardwareObjects/ExporterMotor.py
@@ -45,7 +45,7 @@ class ExporterMotor(AbstractMotor):
 
     def __init__(self, name):
         AbstractMotor.__init__(self, name)
-        self.username = None
+        #self.username = None
         self._motor_pos_suffix = None
         self._motor_state_suffix = None
         self._exporter = None
