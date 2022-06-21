7e14930d1d1be8ff435aa1d6b856086fe8c6e188
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2022-06-21 09:02:34 +0200

    Simple way to handle/read version from version file

--- setup.py
+++ setup.py
@@ -1,39 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
+import os
 from setuptools import setup, find_packages
+from pathlib import Path
 
 
-if sys.version_info >= (3, 0):
-    requirements_filename = 'requirements_python3.txt'
-else:
-    requirements_filename = 'requirements_python2.txt'
+def get_version(fname):
+    with open(fname) as f:
+        content = f.readlines()
 
-with open(requirements_filename) as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content]
-requirements = [x for x in requirements if not x.startswith('#')]
+    ver = content[0].split("=")[-1].replace('"', "").replace(" ", "").strip()
 
+    return ver
+
+
+def get_requirements(fname):
+    with open(fname) as f:
+        content = f.readlines()
+
+    requirements = [x.strip() for x in content]
+    requirements = [x for x in requirements if not x.startswith('#')]
+
+    return requirements
+
+
+requirements = get_requirements(
+    os.path.join(Path(__file__).parent.stem, "requirements_python3.txt")
+)
 setup_requirements = []
-
 tests_requirements = []
-
 extras_requirements = {'gphl': ['f90nml', 'py4j', 'mgen']}
-
 console_scripts = []
-
 gui_scripts = []
 
 entry_points = {
-        'console_scripts': console_scripts,
-        'gui_scripts': gui_scripts
-    }
+    'console_scripts': console_scripts,
+    'gui_scripts': gui_scripts
+}
+
+version = get_version(
+    os.path.join(Path(__file__).parent.stem, "mxcubecore/__version__.py")
+)
 
 setup(
     name='mxcubecore',
-    # version='0.1.0',
-    # author='The MXCuBE developers',
-    # author_email='mxcube@mxcube.org',
+    version=version,
+    author='The MXCuBE collaboration',
+    author_email='mxcube@esrf.fr',
     description='MXCuBE core library',
     long_description='Core libraries for the MXCuBE application',
     url='http://github.com/mxcube/mxcubecore',
@@ -59,5 +72,5 @@ setup(
     setup_requires=setup_requirements,
     tests_require=tests_requirements,
     extras_require=extras_requirements,
-    python_requires='>=2.7',
-   )
+    python_requires='>=3.6',
+)
