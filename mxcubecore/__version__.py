47b50f2a14bdca11bd63604543bed5449077a226
Jacob Oldfield <oldfielj@ansto.gov.au>
2022-09-20 16:34:19 +1000

    Version, dependency and pytest changes

--- mxcubecore/__version__.py
+++ mxcubecore/__version__.py
@@ -1,1 +1,8 @@
-__version__ = "1.0.0"
+try:
+    # Python3.8+ standard library
+    from importlib.metadata import metadata
+except ImportError:
+    # Python3.7 module backport
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__package__ or __name__)
