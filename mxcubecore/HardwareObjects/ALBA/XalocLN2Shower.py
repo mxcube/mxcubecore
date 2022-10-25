5399a4ba9d98968e710e225bf294f8e37ac44971
Operator Beamline Xaloc <opbl13@pcbl1307.cells.es>
2022-10-25 14:38:21 +0200

    Add automated crystal wash when changing sample

--- mxcubecore/HardwareObjects/ALBA/XalocLN2Shower.py
+++ mxcubecore/HardwareObjects/ALBA/XalocLN2Shower.py
@@ -66,6 +66,14 @@ class XalocLN2Shower(HardwareObject):
         self.cmd_ln2shower_on = None
         self.cmd_ln2shower_off = None
         self.cmd_ln2shower_sleep = None
+        
+        self.wash_mounted_crystals = None # wash every crystal mounted by robot
+        self.robot_path_is_safe = None
+        self.sample_changer_loading = None
+        
+        self.collecting = None
+        self.super_hwobj = None
+        
  
     def init(self):
         self.logger.debug("Initializing {0}".format(self.__class__.__name__))
@@ -79,19 +87,71 @@ class XalocLN2Shower(HardwareObject):
         self.cmd_ln2shower_on = self.get_command_object("ln2shower_on")
         self.cmd_ln2shower_off = self.get_command_object("ln2shower_off")
         self.cmd_ln2shower_sleep = self.get_command_object("ln2shower_sleep")
+        self.wash_mounted_crystals = self.get_property("wash_mounted_crystals")
+
+        if HWR.beamline.sample_changer is not None:
+            self.robot_path_is_safe = HWR.beamline.sample_changer.path_safe()
+            self.sample_changer_loading = HWR.beamline.sample_changer.cats_running
+
+        if HWR.beamline.detector is not None:
+            if HWR.beamline.detector.get_cam_state() == "RUNNING":
+                self.collecting = True
+            elif HWR.beamline.detector.get_cam_state() == "STANDBY":
+                self.collecting = False
+            else: 
+                raise Exception("The detector seems to be in a funny state")
         
+        self.super_hwobj = self.get_object_by_role('beamline-supervisor')
+
         self.connect(self.chn_operation_mode, "update", self.operation_mode_changed)
-        
+        if HWR.beamline.collect is not None:
+            self.connect(
+                HWR.beamline.collect, "collectStarted", self.collect_started
+            )
+            self.connect(
+                HWR.beamline.collect, "collectOscillationFinished", self.collect_finished
+            )
+            self.connect(
+                HWR.beamline.collect, "collectOscillationFailed", self.collect_finished
+            )
+            
+        if HWR.beamline.sample_changer is not None:
+            HWR.beamline.sample_changer.connect("path_safeChanged", self.path_safe_changed)
+            HWR.beamline.sample_changer.connect("loadedSampleChanged", self.loaded_sample_changed)
+
+    def collect_started(self, owner, num_oscillations):
+        #logging.getLogger("user_level_log").info("Collection started in sample_control_brick")
+        self.collecting = True
+
+    def collect_finished(self, owner, state, message, *args):
+        #logging.getLogger("user_level_log").info("Collection finished in sample_control_brick")
+        self.collecting = False
+
+    def path_safe_changed(self, value):
+        self.robot_path_is_safe = value
+        HWR.beamline.diffractometer.wait_device_ready()
+        if value == False:
+            self.sample_changer_loading = True
+            if self.wash_mounted_crystals and HWR.beamline.diffractometer.get_diff_phase() == 'Transfer':
+                self.run_ln2shower_wash()
+        else: self.sample_changer_loading = False
+
     def run_ln2shower_wash(self, washflow = 120):
         #TODO: move diff to transfer phase first, use XalocMiniDiff set_phase method
         
         self.logger.debug( "get_current_phase %s" % HWR.beamline.diffractometer.get_current_phase() ) 
-        if HWR.beamline.diffractometer.get_current_phase() != HWR.beamline.diffractometer.PHASE_TRANSFER:
-            HWR.beamline.diffractometer.set_phase( HWR.beamline.diffractometer.PHASE_TRANSFER, timeout = 20 )
-        if HWR.beamline.diffractometer.get_current_phase() == HWR.beamline.diffractometer.PHASE_TRANSFER:
-            self.cmd_ln2shower_wash(washflow, wait = False)
+        self.super_hwobj.wait_ready()
+        if not self.collecting:
+            if HWR.beamline.diffractometer.get_current_phase() != HWR.beamline.diffractometer.PHASE_TRANSFER:
+                HWR.beamline.diffractometer.set_phase( HWR.beamline.diffractometer.PHASE_TRANSFER, timeout = 20 )
+            if HWR.beamline.diffractometer.get_current_phase() == HWR.beamline.diffractometer.PHASE_TRANSFER:
+                self.cmd_ln2shower_wash(washflow, wait = False)
+            else:
+                return False
         else:
+            self.logger.debug( "Cannot use the shower while collecting!! Wait till the collection is finished" ) 
             return False
+            
         return True
             
     def run_ln2shower_off(self):
@@ -108,3 +168,19 @@ class XalocLN2Shower(HardwareObject):
  
     def is_pumping(self):
         return self.is_pumping_attr
+
+    def loaded_sample_changed(self, sample):
+        """
+          For each getput, the signal is emitted twice: once when the crystal is removed, 
+              once when the new crystal is mounted. 
+          For a get or a put, only one signal is sent
+          Want only the first signal and turn off the shower.
+          
+          TODO: when a put is done, the waiting time should be reduced, because the robot is in and out much faster
+        """
+        time_margin = 2 # waiting time between detecting change of sample and turning off the shower
+        if self.sample_changer_loading:
+            time.sleep( time_margin ) # give the CATS time to load the next (in case of getput)
+            self.run_ln2shower_off()
+            self.sample_changer_loading = False
+            
