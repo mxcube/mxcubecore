e089d64776a722363eb7117e758fe9297d52672f
rhfogh <rhfogh@globalphasing.com>
2022-11-02 19:06:46 +0000

    WIP - continuing on JSON-based GPhL popup UI

--- mxcubecore/HardwareObjects/QueueManager.py
+++ mxcubecore/HardwareObjects/QueueManager.py
@@ -8,6 +8,7 @@ container of the queue, note the inheritance from QueueEntryContainer. See the
 documentation for the queue_entry module for more information.
 """
 import logging
+import traceback
 import gevent
 from mxcubecore import queue_entry
 from mxcubecore.BaseHardwareObjects import HardwareObject
