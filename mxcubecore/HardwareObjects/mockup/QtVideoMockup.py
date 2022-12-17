5b9e105a28bdd4ef8d2016b17f32326bda7dbd47
Martin Savko <martin.savko@synchrotron-soleil.fr>
2022-12-17 20:24:47 +0100

    Bug fix: QtVideoMockup and AbstractVideoDevice inherit from HardwareObject which does not define "set_is_ready" method          removing specific reference to qt4

--- mxcubecore/HardwareObjects/mockup/QtVideoMockup.py
+++ mxcubecore/HardwareObjects/mockup/QtVideoMockup.py
@@ -39,7 +39,7 @@ class QtVideoMockup(AbstractVideoDevice):
             "mxcubecore", "configuration/mockup/qt/fakeimg.jpg"
         )
         image_path = self.get_property("file_name", default_image_path)
-
+        print('image_path', image_path)
         self.image = QPixmap(image_path)
         self.image_dimensions = (self.image.width(), self.image.height())
         self.painter = QPainter(self.image)
@@ -53,7 +53,6 @@ class QtVideoMockup(AbstractVideoDevice):
         custom_brush.setColor(Qt.lightGray)
         self.painter.setBrush(custom_brush)
 
-        self.set_is_ready(True)
         AbstractVideoDevice.init(self)
 
     def get_new_image(self):
