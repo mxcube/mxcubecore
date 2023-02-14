496a189cc0b386c34ddf436fbcb3be2cb6a7dedc
Marcus Oskarsson <oscarsso@esrf.fr>
2023-02-14 16:27:00 +0100

    ESRF handling of run number

--- mxcubecore/HardwareObjects/ESRF/ESRFSession.py
+++ mxcubecore/HardwareObjects/ESRF/ESRFSession.py
@@ -21,3 +21,33 @@ class ESRFSession(Session.Session):
             queue_model_objects.PathTemplate.set_archive_path(
                 archive_base_directory, archive_folder
             )
+
+
+    def get_image_directory(self, sub_dir=None):
+        """
+        Returns the full path to images, using the name of each of
+        data_nodes parents as sub directories.
+
+        :param data_node: The data node to get additional
+                          information from, (which will be added
+                          to the path).
+        :type data_node: TaskNode
+
+        :returns: The full path to images.
+        :rtype: str
+        """
+        data_path = self.get_base_image_directory()
+
+        if sub_dir:
+            run_num = 0
+            data_path = os.path.join(
+                self.get_base_image_directory(), sub_dir, f"run_{run_num}/"
+            )
+
+            while os.path.exists(data_path):
+                data_path = os.path.join(
+                    self.get_base_image_directory(), sub_dir, f"run_{run_num}/"
+                )
+                run_num += 1
+
+        return data_path
