e14ad96d94278636c29facfc37f566b2b375b6de
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-03-06 16:25:20 +0100

    Added prototype of the cross-check whether the transmission value is set

--- mxcubecore/HardwareObjects/DESY/P11Transmission.py
+++ mxcubecore/HardwareObjects/DESY/P11Transmission.py
@@ -20,6 +20,7 @@
 
 import gevent
 import logging
+import time
 
 from mxcubecore.HardwareObjects.abstract.AbstractTransmission import (
     AbstractTransmission,
@@ -104,3 +105,11 @@ class P11Transmission(AbstractTransmission):
     def _set_value(self, value):
         value = value / 100.0
         self.chan_set_value.setValue(value)
+
+        print("============== Setting transmission ==================")
+        
+        while self.get_state() == "MOVING":
+            time.sleep(0.1)
+            print("Changing transmission")
+
+
