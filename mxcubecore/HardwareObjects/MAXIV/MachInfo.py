40f99968a5250b8228c9a27f9d01e7b3ac6b712b
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2023-05-03 14:56:39 +0200

    MAXIV: new common HWO for machine information
    New MachInfo hardware object, for using at BioMAX and MicroMAX.
    
    Based on older HWO used at BioMAX, but updated to follow the
    new AbstractMachineInfo API.

--- mxcubecore/HardwareObjects/MAXIV/MachInfo.py
+++ mxcubecore/HardwareObjects/MAXIV/MachInfo.py
@@ -1,153 +1,109 @@
-"""
-[Name] MachInfoMockup
-
-[Description]
-MachInfo hardware objects are used to obtain information from the accelerator
-control system.
-
-This is a mockup hardware object, it simulates the behaviour of an accelerator
-information by :
-
-    - produces a current value that varies with time
-    - simulates a control room message that changes with some condition
-      ()
-    - simulates
-
-[Emited signals]
-machInfoChanged
-   pars:  values (dict)
-
-   mandatory fields:
-     values['current']  type: str; desc: synchrotron radiation current in milli-amps
-     values['message']  type: str; desc: message from control room
-     values['attention'] type: boolean; desc: False (if no special attention is required)
-                                            True (if attention should be raised to the user)
-
-   optional fields:
-      any number of optional fields can be sent over with this signal by adding them in the
-      values dictionary
-
-      for example:
-         values['lifetime']
-         values['topup_remaining']
-"""
-
+import re
+import math
 import logging
 import gevent
-import time
-import PyTango
-from mxcubecore import HardwareRepository as HWR
-from mxcubecore.BaseHardwareObjects import HardwareObject
+from tango import DeviceProxy
+from mxcubecore.utils.units import sec_to_hour, A_to_mA
+from mxcubecore.HardwareObjects.abstract.AbstractMachineInfo import AbstractMachineInfo
+
+# how often we refresh machine info
+REFRESH_PERIOD_SEC = 30
+CLEANR = re.compile("<.*?>")
 
 
-class MachInfo(HardwareObject):
-    default_current = 0
-    default_lifetime = 0
-    default_message = ""
-    default_topup_remaining = 0
+log = logging.getLogger("HWR")
+
+
+def cleanhtml(raw_html):
+    cleantext = re.sub(CLEANR, "", raw_html)
+    return cleantext
+
+
+def catch_errors(func):
+    """
+    run wrapped function, catching all exception
+
+    If an exception as raised, log the exception and return 'unknown'
+    """
+
+    def wrapper(*a, **kw):
+        try:
+            return func(*a, **kw)
+        except Exception:
+            log.exception("error fetching machine info")
+            return "unknown"
+
+    return wrapper
+
+
+class MachInfo(AbstractMachineInfo):
+    """
+    Machine info hardware object for MAXIV site.
+
+    Provides to the user general information about the machine,
+    such as ring status, current, operator message, etc.
+
+    This hardware objects fetches the information from attributes
+    of specified tango devices.
+
+    Hardware object properties:
+        mach_info (str): name of the machine status tango device
+        current (str): name of the ring status tango device
+        parameters (str): topics to export, see AbstractMachineInfo class for details
+    """
 
     def __init__(self, *args):
-        Equipment.__init__(self, *args)
-        default_current = 0
-        default_lifetime = 0
-        default_message = ""
-        default_topup_remaining = 0
-        self.current = self.default_current
-        self.lifetime = self.default_lifetime
-        self.message = self.default_message
-
-        self.mach_info_channel = None
-        self.mach_curr_channel = None
+        super().__init__(*args)
+        self.mach_info = None
+        self.mach_curr = None
 
     def init(self):
+        super().init()
+
+        self.mach_info = self._get_tango_device("mach_info")
+        self.mach_curr = self._get_tango_device("current")
+        gevent.spawn(self._refresh_ticker)
+
+    def _get_tango_device(self, property_name: str) -> DeviceProxy:
+        dev_name = self.get_property(property_name)
         try:
-            # self.mach_info_channel =  self.get_channel_object("mach_info")
-            channel = self.get_property("mach_info")
-            self.mach_info_channel = PyTango.DeviceProxy(channel)
-            self.message = self.mach_info_channel.OperatorMessage
-            self.message += "\n" + self.mach_info_channel.R3NextInjection
-        except Exception as ex:
-            logging.getLogger("HWR").warning("Error initializing machine info channel")
+            return DeviceProxy(dev_name)
+        except Exception:
+            log.exception(f"error connecting to machine info tango device {dev_name}")
 
-        try:
-            # self.curr_info_channel =  self.get_channel_object("curr_info")
-            channel_current = self.get_property("current")
-            self.curr_info_channel = PyTango.DeviceProxy(channel_current)
-            # why twice??
-            # why hwr channel does not work?? why??
-            if self.curr_info_channel is None:
-                self.curr_info_channel = PyTango.DeviceProxy(channel_current)
-                curr = self.curr_info_channel.Current
-            if curr < 0:
-                self.current = 0.00
-            else:
-                self.current = "{:.2f}".format(curr * 1000)
-            self.lifetime = float(
-                "{:.2f}".format(self.curr_info_channel.Lifetime / 3600)
-            )
-        except Exception as ex:
-            logging.getLogger("HWR").warning("Error initializing current info channel")
-
-        self._run()
-
-    def _run(self):
-        gevent.spawn(self._update_me)
-
-    def _update_me(self):
-        self.t0 = time.time()
-
+    def _refresh_ticker(self):
         while True:
-            gevent.sleep(2)
-            self.message = self.mach_info_channel.OperatorMessage
-            self.message += "\n" + self.mach_info_channel.R3NextInjection
-            curr = self.curr_info_channel.Current
-            if curr < 0:
-                self.current = 0.00
-            else:
-                self.current = "{:.2f}".format(curr * 1000)
+            self.update_value()
+            gevent.sleep(REFRESH_PERIOD_SEC)
 
-            self.lifetime = float(
-                "{:.2f}".format(self.curr_info_channel.Lifetime / 3600)
-            )
+    @catch_errors
+    def get_current(self) -> str:
+        current = A_to_mA(self.mach_curr.Current)
+        return f"{current:.2f} mA"
 
-            self.attention = False
-            values = dict()
-            values["current"] = self.current
-            values["message"] = self.message
-            values["lifetime"] = self.lifetime
-            values["attention"] = self.attention
-            self.emit("machInfoChanged", values)
-            self.emit("valueChanged", values)
+    @catch_errors
+    def get_fillmode(self) -> str:
+        return self.mach_info.R3Mode
 
-    def get_current(self):
-        return self.current
+    @catch_errors
+    def get_message(self) -> str:
+        return self.mach_info.OperatorMessage
 
-    def getLifeTime(self):
-        return self.lifetime
+    @catch_errors
+    def get_lifetime(self) -> str:
+        lifetime = self.mach_curr.Lifetime
+        if math.isnan(lifetime):
+            return "n/a"
 
-    def getTopUpRemaining(self):
-        return self.topup_remaining
+        return f"{sec_to_hour(lifetime):.2f} h"
 
-    def getMessage(self):
-        return self.message
+    @catch_errors
+    def get_injection(self) -> str:
+        return self.mach_info.R3NextInjection
 
+    @catch_errors
+    def get_status(self) -> str:
+        message = cleanhtml(self.mach_info.MachineMessage)
+        message = message.replace("R1", " R1").replace("Linac", " Linac")
 
-def test():
-    import sys
-
-    hwr = HWR.get_hardware_repository()
-    hwr.connect()
-
-    conn = hwr.get_hardware_object(sys.argv[1])
-
-    print("Machine current: ", conn.get_current())
-    print("Life time: ", conn.getLifeTime())
-    print("TopUp remaining: ", conn.getTopUpRemaining())
-    print("Message: ", conn.getMessage())
-
-    while True:
-        gevent.wait(timeout=0.1)
-
-
-if __name__ == "__main__":
-    test()
+        return message
