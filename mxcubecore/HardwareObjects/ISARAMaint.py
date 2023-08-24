39da3bf96cf4f81f1cd2875b01c8d2599acf497c
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-08-24 15:51:59 +0200

    ISARA maint hardware object
    The sample changer maintenance hardware object for ISARA2 robots.
    This hardware object support ISARA2 sample changes exposed via
    tango device server.

---
+++ mxcubecore/HardwareObjects/ISARAMaint.py
@@ -0,0 +1,265 @@
+from typing import Optional
+from tango import DeviceProxy, DevFailed
+from mxcubecore.BaseHardwareObjects import Equipment
+
+"""
+The sample changer maintenance hardware object for ISARA2 robot.
+
+Provide commands for powering on and off the robot. Populates the 'equipment' tab
+with a handful of other useful command, while operating the sample changer.
+
+This hardware object assumes that ISARA2 sample changer is exposed
+via tango device server implementation as implemented here:
+https://gitlab.com/MaxIV/isara/dev-maxiv-isara-ns-09
+
+Example XML configuration for this hardware object:
+
+  <object class="ISARAMaint">
+     <tangoname>b312-e/ctl/sm-01</tangoname>
+     <polling>400</polling>
+  </object>
+
+  tangoname - the sample changers tango device to use
+  polling   - optional, sets the polling frequency of device attributes, in milliseconds
+"""
+
+DEFAULT_POLLING = 1000
+
+
+def _reword_isara_error(err_message: str) -> str:
+    """
+    Reword some of the ISARA error messages to make them easier to understand for the user.
+    """
+    msg = err_message.lower()
+    if msg == "remote mode requested":
+        return "it is not in remote mode"
+
+    if msg == "doors must be closed":
+        return "hutch is not searched"
+
+    # let's use error as-is
+    return msg
+
+
+def _get_isara_command_error(ex: DevFailed) -> Optional[str]:
+    """
+    Check if specified exception encodes an ISARA command error.
+
+    Returns the human-readable error message from ISARA, or None
+    if this is not an ISARA command error exception.
+    """
+
+    # The ISARA command errors are signaled by raising DevError exception,
+    # with reason set to 'ISARACommandError' and 'desc' to the error message
+    # from ISARA. The DevError will be wrapped inside DevFailed exception by
+    # tango.
+    #
+    # Check if provided exception contains an DevError signalling ISARA command error.
+    for err in ex.args:
+        if err.reason == "ISARACommandError":
+            return _reword_isara_error(err.desc)
+
+    # this is not an ISARA command error exception
+    return None
+
+
+class ISARAMaint(Equipment):
+    def __init__(self, name):
+        super().__init__(name)
+
+        self._commands_state = dict(
+            powerOn=False,
+            powerOff=False,
+            openLid=True,
+            closeLid=True,
+            home=True,
+            dry=True,
+            soak=True,
+            clearMemory=True,
+            reset=True,
+            back=True,
+            abort=True,
+        )
+
+        self._powered = None
+        self._position_name = None
+        self._message = None
+
+    def init(self):
+        self.isara_dev = DeviceProxy(self.tangoname)
+
+        polling = self._get_polling()
+
+        self._poll_attribute("Powered", polling, self._powered_updated)
+        self._poll_attribute("PositionName", polling, self._position_name_updated)
+        self._poll_attribute("Message", polling, self._message_updated)
+
+    def _get_polling(self):
+        """
+        get polling frequency to use for device attribute poller
+        """
+        try:
+            # polling is specified in the XML file
+            return self.polling
+        except AttributeError:
+            # no polling is specified in the XML, use default polling value
+            return DEFAULT_POLLING
+
+    def _poll_attribute(self, attr_name: str, polling: int, callback):
+        channel = self.add_channel(
+            {
+                "type": "tango",
+                "name": f"_chn{attr_name}",
+                "tangoname": self.tangoname,
+                "polling": polling,
+            },
+            attr_name,
+        )
+
+        channel.connect_signal("update", callback)
+
+    def _update_state(self):
+        for attr in [self._powered, self._position_name, self._message]:
+            if attr is None:
+                # some of the values are still unknown,
+                # wait until we get all values
+                return
+
+        #
+        # update 'power' commands
+        #
+        self._commands_state["powerOn"] = not self._powered
+        self._commands_state["powerOff"] = self._powered
+
+        #
+        # update 'positions' commands
+        #
+        if not self._powered or self._position_name == "undefined":
+            # when powered off or running a trajectory,
+            # disable position commands
+            self._commands_state["home"] = False
+            self._commands_state["dry"] = False
+            self._commands_state["soak"] = False
+        else:
+            self._commands_state["home"] = True
+            self._commands_state["dry"] = True
+            self._commands_state["soak"] = True
+
+            if self._position_name in ["home", "dry", "soak"]:
+                # can't move to same position
+                self._commands_state[self._position_name] = False
+
+        self._emit_global_state_changed()
+
+    def _powered_updated(self, powered):
+        self._powered = powered
+        self._update_state()
+
+    def _position_name_updated(self, position_name):
+        self._position_name = position_name.lower()
+        self._update_state()
+
+    def _message_updated(self, message):
+        self._message = message
+        self._update_state()
+
+    def _emit_global_state_changed(self):
+        self.emit(
+            "globalStateChanged",
+            (self._commands_state, self._message),
+        )
+
+    def _toggle_power(self, power_on: bool):
+        """
+        issue powerOn or powerOff commands, catching ISARA command error exception
+        """
+        command = self.isara_dev.PowerOn if power_on else self.isara_dev.PowerOff
+
+        try:
+            command()
+        except DevFailed as ex:
+            isara_err = _get_isara_command_error(ex)
+
+            # this is not an ISARA command error, pass on the exception
+            if isara_err is None:
+                raise ex
+
+            state = "on" if power_on else "off"
+            raise RuntimeError(f"Can't power {state} sample changer, {isara_err}.")
+
+    def send_command(self, cmd_name, _args=None):
+        if cmd_name == "powerOn":
+            self._toggle_power(True)
+        elif cmd_name == "powerOff":
+            self._toggle_power(False)
+        elif cmd_name == "openLid":
+            self.isara_dev.OpenLid()
+        elif cmd_name == "closeLid":
+            self.isara_dev.CloseLid()
+        elif cmd_name == "home":
+            self.isara_dev.Home()
+        elif cmd_name == "dry":
+            self.isara_dev.Dry()
+        elif cmd_name == "soak":
+            self.isara_dev.Soak()
+        elif cmd_name == "clearMemory":
+            self.isara_dev.ClearMemory()
+        elif cmd_name == "reset":
+            self.isara_dev.Reset()
+        elif cmd_name == "back":
+            self.isara_dev.Back()
+        elif cmd_name == "abort":
+            self.isara_dev.Abort()
+        else:
+            raise Exception(f"ISARA MAINT: unexpected command '{cmd_name}'")
+
+    def get_global_state(self):
+        return dict(glob_state="dummy"), self._commands_state, self._message
+
+    def get_cmd_info(self):
+        return [
+            [
+                "Power",
+                [
+                    ["powerOn", "PowerOn", "Switch Power On"],
+                    ["powerOff", "PowerOff", "Switch Power Off"],
+                ],
+            ],
+            [
+                "Lid",
+                [
+                    ["openLid", "Open Lid", "Open Lid"],
+                    ["closeLid", "Close Lid", "Close Lid"],
+                ],
+            ],
+            [
+                "Positions",
+                [
+                    ["home", "Home", "Actions", "Home (trajectory)"],
+                    ["dry", "Dry", "Actions", "Dry (trajectory)"],
+                    ["soak", "Soak", "Actions", "Soak (trajectory)"],
+                ],
+            ],
+            [
+                "Recovery",
+                [
+                    [
+                        "clearMemory",
+                        "Clear Memory",
+                        "Clear Info in Robot Memory "
+                        " (includes info about sample on Diffr)",
+                    ],
+                    [
+                        "reset",
+                        "Reset Fault",
+                        "Acknowledge security fault",
+                    ],
+                    [
+                        "back",
+                        "Back",
+                        "Return sample back to Dewar",
+                    ],
+                ],
+            ],
+            ["Abort", [["abort", "Abort", "Abort running trajectory"]]],
+        ]
---
+++ test/pytest/test_hwo_isara_maint.py
@@ -0,0 +1,305 @@
+import pytest
+from tango import DeviceProxy, Except
+from tango.server import Device, attribute, command
+from tango.test_context import DeviceTestContext
+from mxcubecore.HardwareObjects.ISARAMaint import ISARAMaint
+from gevent.event import Event
+
+"""
+Test the HardwareObjects.ISARAMaint sample changer maintenance hardware object.
+"""
+
+
+class _ISARA(Device):
+    """
+    A small tango device used for testing ISARAMaint hardware object.
+
+    It mimics a small subset of attributes and commands of the real
+    ISARA tango device, required to run the tests.
+    """
+
+    def __init__(self, *a, **k):
+        self._remote_mode = True  # toggle for 'remote' and 'manual' modes
+        self._is_powered = True
+        self._position = "HOME"
+        super().__init__(*a, **k)
+
+    def _check_remote_mode(self):
+        """
+        raise tango exception if the device is not in remote mode
+        """
+        if not self._remote_mode:
+            Except.throw_exception("ISARACommandError", "Remote mode requested", "")
+
+    @attribute(name="Powered", dtype=bool)
+    def _powered(self):
+        return self._is_powered
+
+    @attribute(name="PositionName", dtype=str)
+    def _position_name(self):
+        return self._position
+
+    @attribute(name="Message", dtype=str)
+    def _message(self):
+        return "test message"
+
+    @command(dtype_out=str)
+    def PowerOn(self):
+        self._check_remote_mode()
+        self._is_powered = True
+        return "on"
+
+    @command(dtype_out=str)
+    def PowerOff(self):
+        self._check_remote_mode()
+        self._is_powered = False
+        return "off"
+
+    @command(dtype_out=str)
+    def Home(self):
+        self._position = "HOME"
+        return "home"
+
+    @command(dtype_out=str)
+    def Soak(self):
+        self._position = "SOAK"
+        return "soak"
+
+    @command(dtype_out=str)
+    def Dry(self):
+        self._position = "DRY"
+        return "dry"
+
+    #
+    # special command, that is not present in a normal tango device,
+    # it is used by the tests to emulate the situation when the
+    # robot is in 'manual mode'
+    #
+    @command
+    def disable_remote_mode(self):
+        self._remote_mode = False
+
+
+def _disconnect_channels(maint: ISARAMaint):
+    """
+    disconnect signal callbacks from tango attribute pollers
+
+    We need to disconnect signals, otherwise we'll get exceptions
+    when shut down the tango device, while tearing down text fixture.
+    """
+
+    # the hard-coded list of attribute poller callbacks
+    callbacks = {
+        "_chnPowered": maint._powered_updated,
+        "_chnPositionName": maint._position_name_updated,
+        "_chnMessage": maint._message_updated,
+    }
+
+    for ch in maint.get_channels():
+        ch.disconnect_signal("update", callbacks[ch.name()])
+
+
+@pytest.fixture
+def isara_maint():
+    #
+    # start our test ISARA tango device
+    #
+    dev_ctx = DeviceTestContext(_ISARA, host="127.0.0.1", process=True)
+    dev_ctx.start()
+
+    #
+    # create ISARAMaint hardware object
+    #
+    maint = ISARAMaint("/sc_maint")
+    maint.tangoname = dev_ctx.get_device_access()
+    # make polling faster, to speed up the tests
+    maint.polling = 10
+
+    yield maint
+
+    #
+    # clean-up fixture
+    #
+    _disconnect_channels(maint)
+    dev_ctx.stop()
+    dev_ctx.join()
+
+
+class CallbackTracker:
+    """
+    allows tests to wait until signal callback is invoked
+    """
+
+    def __init__(self):
+        self._cb_received = Event()
+
+    def callback(self, *a, **k):
+        self._cb_received.set()
+
+    def wait_for_callback(self):
+        self._cb_received.wait()
+        self._cb_received.clear()
+
+
+def test_power_on_home(isara_maint: ISARAMaint):
+    """
+    test the state of ISARAMain when the robot is:
+
+      * powered on
+      * the arm is in home position
+    """
+    cb_tracker = CallbackTracker()
+
+    isara_maint.connect("globalStateChanged", cb_tracker.callback)
+    isara_maint.init()
+
+    # wait until all tango attributes have been read from tango device
+    cb_tracker.wait_for_callback()
+
+    #
+    # check that get_cmd_info() seems to give us something reasonable
+    #
+    cmd_info = isara_maint.get_cmd_info()
+
+    # check that we got the expected command section names
+    cmd_section_names = set()
+    for cmd_section in cmd_info:
+        cmd_section_names.add(cmd_section[0])
+    assert cmd_section_names == {"Power", "Abort", "Lid", "Positions", "Recovery"}
+
+    #
+    # check that commands state is correct
+    #
+    _, commands_state, message = isara_maint.get_global_state()
+    assert commands_state == dict(
+        powerOn=False,
+        powerOff=True,
+        openLid=True,
+        closeLid=True,
+        home=False,
+        dry=True,
+        soak=True,
+        clearMemory=True,
+        reset=True,
+        back=True,
+        abort=True,
+    )
+    assert message == "test message"
+
+
+def test_power_off(isara_maint: ISARAMaint):
+    """
+    test running 'power off' command
+    """
+    cb_tracker = CallbackTracker()
+
+    isara_maint.connect("globalStateChanged", cb_tracker.callback)
+    isara_maint.init()
+
+    # wait until all tango attributes have been read from tango device
+    cb_tracker.wait_for_callback()
+
+    # issue 'power off' command
+    isara_maint.send_command("powerOff")
+    cb_tracker.wait_for_callback()
+
+    #
+    # check that commands state is correct
+    #
+    _, commands_state, _ = isara_maint.get_global_state()
+    assert commands_state == dict(
+        powerOn=True,
+        powerOff=False,
+        openLid=True,
+        closeLid=True,
+        home=False,
+        dry=False,
+        soak=False,
+        clearMemory=True,
+        reset=True,
+        back=True,
+        abort=True,
+    )
+
+
+def test_change_positions(isara_maint: ISARAMaint):
+    """
+    test moving robot between different positions,
+    using commands
+    """
+
+    cb_tracker = CallbackTracker()
+
+    isara_maint.connect("globalStateChanged", cb_tracker.callback)
+    isara_maint.init()
+
+    # wait until all tango attributes have been read from tango device
+    cb_tracker.wait_for_callback()
+
+    # check that the state of commands is correct 'home' position
+    _, commands_state, _ = isara_maint.get_global_state()
+    assert not commands_state["home"]
+    assert commands_state["dry"]
+    assert commands_state["soak"]
+
+    #
+    # move to 'soak' position
+    #
+    isara_maint.send_command("soak")
+    cb_tracker.wait_for_callback()
+
+    # check that the state of commands is correct 'soak' position
+    _, commands_state, _ = isara_maint.get_global_state()
+    assert commands_state["home"]
+    assert commands_state["dry"]
+    assert not commands_state["soak"]
+
+    #
+    # move to 'dry' position
+    #
+    isara_maint.send_command("dry")
+    cb_tracker.wait_for_callback()
+
+    # check that the state of commands is correct 'dry' position
+    _, commands_state, _ = isara_maint.get_global_state()
+    assert commands_state["home"]
+    assert not commands_state["dry"]
+    assert commands_state["soak"]
+
+    #
+    # move to 'home' position
+    #
+    isara_maint.send_command("home")
+    cb_tracker.wait_for_callback()
+
+    # check that the state of commands is correct 'home' position
+    _, commands_state, _ = isara_maint.get_global_state()
+    assert not commands_state["home"]
+    assert commands_state["dry"]
+    assert commands_state["soak"]
+
+
+def test_manual_mode_error(isara_maint: ISARAMaint):
+    """
+    test that when robot is in 'manual mode', issuing power on/off command
+    raises an 'not in remote mode' error
+    """
+    cb_tracker = CallbackTracker()
+
+    isara_maint.connect("globalStateChanged", cb_tracker.callback)
+    isara_maint.init()
+
+    # wait until all tango attributes have been read from tango device
+    cb_tracker.wait_for_callback()
+
+    # put robot into 'manual mode'
+    DeviceProxy(isara_maint.tangoname).disable_remote_mode()
+
+    #
+    # trying to power on or off the robot, should generate an appropriate error message
+    #
+    with pytest.raises(RuntimeError, match="power off .*not in remote mode"):
+        isara_maint.send_command("powerOff")
+
+    with pytest.raises(RuntimeError, match="power on .*not in remote mode"):
+        isara_maint.send_command("powerOn")
