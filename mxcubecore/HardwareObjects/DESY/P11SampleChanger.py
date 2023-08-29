b887c18b211f268309355942572ee529dddf748f
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-08-29 09:53:45 +0200

    Bumped the year in the copyright note

--- mxcubecore/HardwareObjects/DESY/P11SampleChanger.py
+++ mxcubecore/HardwareObjects/DESY/P11SampleChanger.py
@@ -18,7 +18,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with MXCuBE. If not, see <http://www.gnu.org/licenses/>.
 
-__copyright__ = """ Copyright © 2010 - 2020 by MXCuBE Collaboration """
+__copyright__ = """ Copyright © 2010 - 2023 by MXCuBE Collaboration """
 __license__ = "LGPLv3+"
 
 
