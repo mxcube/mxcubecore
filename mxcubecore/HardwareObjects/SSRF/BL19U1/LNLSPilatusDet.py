b2202e3db571e37759c8b17cfb6d29f7d558d9b0
ssrfmx <qinwenming@sari.ac.cn>
2023-09-07 20:25:24 +0800

    dev

--- mxcubecore/HardwareObjects/SSRF/BL19U1/LNLSPilatusDet.py
+++ mxcubecore/HardwareObjects/SSRF/BL19U1/LNLSPilatusDet.py
@@ -706,7 +706,8 @@ class LNLSPilatusDet(AbstractDetector):
 
     def get_beamy(self):
         distance = self.get_detector_distance()
-        value = 1331 - 0.0465*distance/1000
+        #value = 1331 - 0.0465*distance/1000
+        value = 1344.3 - 0.0465 * distance/1000
         return round(value, 2)
 
     def updateJobStatus(self,frame_number, uuid, path, status):
