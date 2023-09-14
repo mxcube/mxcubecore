62fb322ea680f7310380f320b67f4f0ab881cb9b
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-09-14 17:25:46 +0200

    Update after upgrade P11Zoom

--- mxcubecore/HardwareObjects/DESY/P11Zoom.py
+++ mxcubecore/HardwareObjects/DESY/P11Zoom.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
-#
+# 
 #  Project: MXCuBE
-#  https://github.com/mxcube
+#  https://github.com/mxcube.
 #
 #  This file is part of MXCuBE software.
 #
@@ -16,11 +16,7 @@
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with MXCuBE. If not, see <http://www.gnu.org/licenses/>.
-
-__copyright__ = """ Copyright © 2010 - 2023 by MXCuBE Collaboration """
-__license__ = "LGPLv3+"
-
+#  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
 
 from enum import Enum
 import ast
@@ -28,36 +24,36 @@ import ast
 from mxcubecore.HardwareObjects.abstract.AbstractNState import AbstractNState
 from mxcubecore.HardwareObjects.abstract.AbstractNState import BaseValueEnum
 
-
 class P11Zoom(AbstractNState):
+
     def __init__(self, name):
         self.camera_hwobj = None
         self.pixels_per_mm = None
         self.closest_zoom = None
 
-        AbstractNState.__init__(self, name)
+        AbstractNState.__init__(self,name)
 
     def init(self):
 
-        self.pixels_per_mm = self.get_property("pixels_per_mm")
+        self._pixels_per_mm = self.get_property("pixels_per_mm")
+        self._overview_pixels_per_mm = self.get_property("overview_pixels_per_mm")
         self.camera_hwobj = self.get_object_by_role("camera")
 
         self.connect(self.camera_hwobj, "zoomChanged", self.zoom_value_changed)
 
         if self.camera_hwobj is None:
             self.log.debug("P11Zoom.py - cannot connect to camera hardware object")
-        else:
-            self.log.debug(
-                "P11Zoom.py / current zoom is %s" % self.camera_hwobj.get_zoom()
-            )
 
         AbstractNState.init(self)
 
     def initialise_values(self):
-        self.VALUES = Enum("ZoomEnum", ast.literal_eval(self.get_property("values")))
+        self.VALUES = Enum("ZoomEnum", ast.literal_eval(self.get_property('values')))
 
     def get_state(self):
         return self.STATES.READY
+
+    def is_moving(self):
+        return False
 
     def get_value(self):
         return self.get_zoom()
@@ -67,18 +63,27 @@ class P11Zoom(AbstractNState):
 
     def get_pixels_per_mm(self):
         current_zoom = self.get_zoom()
-        px_per_mm = self.pixels_per_mm * current_zoom
-        return [px_per_mm, px_per_mm]
+
+        if current_zoom == 0:
+            px_per_mm = self._overview_pixels_per_mm
+        else:
+            self.log.debug("getting pixels per mm: %s - %s" % (str(self._pixels_per_mm), str(current_zoom)))
+            px_per_mm = self._pixels_per_mm * current_zoom
+
+        scale = self.camera_hwobj.get_scale()
+
+        return [int(px_per_mm*scale), int(px_per_mm*scale)]
 
     def get_zoom(self):
-        self.log.debug("ZOOM: current zoom is : %s" % self.camera_hwobj.get_zoom())
-        self.current_value = self.camera_hwobj.get_zoom()
+        zoom = self.camera_hwobj.get_zoom()
+        if zoom is not None:
+            self.current_value =  zoom
         return self.current_value
 
     def set_zoom_value(self, value):
         self.camera_hwobj.set_zoom(value)
 
-    def set_zoom(self, zoom):
+    def set_zoom(self,zoom):
         self.camera_hwobj.set_zoom(zoom.value)
 
     def get_current_zoom(self):
@@ -86,23 +91,29 @@ class P11Zoom(AbstractNState):
         self.update_zoom()
         return self.closest_zoom, self.get_zoom()
 
-    def zoom_value_changed(self, value):
-        self.log.debug("ZOOM - value changed: %s" % value)
+    def zoom_value_changed(self,value):
         self.current_value = value
         self.update_value(value)
         self.update_zoom()
 
     def update_zoom(self):
+
         dist = None
         value = self.get_value()
 
         for zoom in self.VALUES:
-            _dist = abs(value - zoom.value)
+
+            if value == 0:
+                self.closest_zoom = zoom
+                break
+
+            _dist = abs(value - zoom.value) 
             if dist is None or _dist < dist:
-                dist = _dist
+                dist = _dist 
                 self.closest_zoom = zoom
 
         if self.closest_zoom is not None:
             self.emit("predefinedPositionChanged", (self.closest_zoom, value))
         else:
             self.emit("predefinedPositionChanged", (None, None))
+          
