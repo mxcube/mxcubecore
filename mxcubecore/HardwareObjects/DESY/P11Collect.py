dbbe6d470de5086f63da252c34fd8b3b9c6d3f38
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-09-27 11:20:37 +0200

    Enabled back explicit check on if beamtimeid is open

--- mxcubecore/HardwareObjects/DESY/P11Collect.py
+++ mxcubecore/HardwareObjects/DESY/P11Collect.py
@@ -62,28 +62,17 @@ class P11Collect(AbstractCollect):
 
     def init(self):
 
-<<<<<<< HEAD
-        super(P11Collect, self).init()
-=======
         super(P11Collect,self).init()
->>>>>>> P11Collect additional debug after upgrade
 
         # os.system("/opt/xray/bin/adxv -socket -colors Gray -rings &")
 
         # os.system("/bin/bash /gpfs/local/shared/MXCuBE/STRELA/start_viewer_zmq.sh")
 
-<<<<<<< HEAD
-        self.default_speed = self.get_property("omega_default_speed", 130)
-        self.turnback_time = self.get_property("turnback_time", 0.3)
-        self.filter_server_name = self.get_property("filterserver")
-        self.mono_server_name = self.get_property("monoserver")
-=======
         
         self.default_speed = self.get_property("omega_default_speed", 130)
         self.turnback_time = self.get_property("turnback_time", 0.3)
         self.filter_server_name = self.get_property('filterserver')
         self.mono_server_name = self.get_property('monoserver')
->>>>>>> P11Collect additional debug after upgrade
         self.filter_server = DeviceProxy(self.filter_server_name)
         self.mono_server = DeviceProxy(self.mono_server_name)
 
@@ -95,7 +84,6 @@ class P11Collect(AbstractCollect):
         self.acq_off_cmd = self.get_command_object("acq_off")
         self.acq_window_off_cmd = self.get_command_object("acq_window_off")
 
-<<<<<<< HEAD
         if None in [
             self.lower_bound_ch,
             self.upper_bound_ch,
@@ -104,11 +92,6 @@ class P11Collect(AbstractCollect):
             self.acq_off_cmd,
             self.acq_window_off_cmd,
         ]:
-=======
-        if None in [self.lower_bound_ch, self.upper_bound_ch,
-                    self.acq_arm_cmd, self.acq_on_cmd, self.acq_off_cmd,
-                    self.acq_window_off_cmd]:
->>>>>>> P11Collect additional debug after upgrade
             self.init_ok = False
             self.log.debug("lower_bound_ch: %s" % self.lower_bound_ch)
             self.log.debug("upper_bound_ch: %s" % self.upper_bound_ch)
@@ -369,20 +352,6 @@ class P11Collect(AbstractCollect):
         """
         HWR.beamline.diffractometer.wait_omega()
 
-<<<<<<< HEAD
-        start_pos = start_angle - self.turnback_time * self.acq_speed
-        stop_pos = stop_angle + self.turnback_time * self.acq_speed
-
-        self.log.debug("#COLLECT# Running OMEGA through the std acquisition")
-        if start_angle <= stop_angle:
-            self.lower_bound_ch.set_value(start_angle)
-            self.upper_bound_ch.set_value(stop_angle)
-
-        else:
-            self.lower_bound_ch.set_value(stop_angle)
-            self.upper_bound_ch.set_value(start_angle)
-
-=======
         start_pos = start_angle - self.turnback_time*self.acq_speed
         stop_pos = stop_angle + self.turnback_time*self.acq_speed
         
@@ -398,7 +367,6 @@ class P11Collect(AbstractCollect):
                 self.upper_bound_ch.set_value(start_angle)
 
 
->>>>>>> P11Collect additional debug after upgrade
         self.omega_mv(start_pos, self.default_speed)
         self.acq_arm_cmd()
         self.omega_mv(stop_pos, self.acq_speed)
@@ -407,10 +375,6 @@ class P11Collect(AbstractCollect):
         self.acq_window_off_cmd()
         self.omega_mv(stop_angle, self.acq_speed)
         HWR.beamline.diffractometer.wait_omega()
-<<<<<<< HEAD
-=======
-
->>>>>>> P11Collect additional debug after upgrade
 
     def collect_characterisation(
         self, start_angle, img_range, nimages, angle_inc, exp_time
@@ -431,13 +395,9 @@ class P11Collect(AbstractCollect):
 
         diffr = HWR.beamline.diffractometer
 
-<<<<<<< HEAD
         self.log.debug(
             "#COLLECT# Running OMEGA through the characteristation acquisition"
         )
-=======
-        self.log.debug("#COLLECT# Running OMEGA through the characteristation acquisition")
->>>>>>> P11Collect additional debug after upgrade
 
         self.omega_mv(start_angle, self.default_speed)
 
@@ -445,15 +405,6 @@ class P11Collect(AbstractCollect):
             print("collecting image %s" % img_no)
             start_at = start_angle + angle_inc * img_no
             stop_angle = start_at + img_range * 1.0
-<<<<<<< HEAD
-            # print("======= CHARACTERISATION Adding angle and range to the header...")
-            # Add start angle to the header
-            # detector = HWR.beamline.detector
-            # detector.set_eiger_start_angle(start_at)
-
-            # Add angle increment to the header
-            # detector.set_eiger_angle_increment(angle_inc)
-=======
             #print("======= CHARACTERISATION Adding angle and range to the header...")
             #Add start angle to the header
             #detector = HWR.beamline.detector
@@ -461,42 +412,24 @@ class P11Collect(AbstractCollect):
 
             # Add angle increment to the header
             #detector.set_eiger_angle_increment(angle_inc)
->>>>>>> P11Collect additional debug after upgrade
 
             print("collecting image %s, angle %f" % (img_no, start_at))
 
             if start_at >= stop_angle:
-<<<<<<< HEAD
-                init_pos = start_at  # - self.acq_speed * self.turnback_time
-                # init_pos = start_at - 1.5
-            else:
-                init_pos = start_at  # + self.acq_speed * self.turnback_time
-=======
                 init_pos = start_at #- self.acq_speed * self.turnback_time
                 # init_pos = start_at - 1.5
             else:
                 init_pos = start_at #+ self.acq_speed * self.turnback_time
->>>>>>> P11Collect additional debug after upgrade
                 # init_pos = start_at + 1.5
 
             # self.omega_mv(init_pos, self.default_speed)
             self.collect_std_collection(start_at, stop_angle)
 
-<<<<<<< HEAD
-            # This part goes to standard collection. Otherwise it produces phantom openings.
-            # diffr.set_omega_velocity(self.default_speed)
-            # self.acq_window_off_cmd()
-            # self.acq_off_cmd()
-            self.log.debug(
-                "======= collect_characterisation  Waiting ======================================="
-            )
-=======
             #This part goes to standard collection. Otherwise it produces phantom openings.
             # diffr.set_omega_velocity(self.default_speed)
             # self.acq_window_off_cmd()
             # self.acq_off_cmd()
             self.log.debug("======= collect_characterisation  Waiting =======================================")
->>>>>>> P11Collect additional debug after upgrade
 
             #Let adxv know whether it is 
             # self.adxv_notify(self.latest_h5_filename,img_no+1)
@@ -544,22 +477,14 @@ class P11Collect(AbstractCollect):
             detector.stop_acquisition()
             diffr.wait_omega()
             # =================
-<<<<<<< HEAD
-            # It is probably already finished in a standard collection.
-=======
             #It is probably already finished in a standard collection.
->>>>>>> P11Collect additional debug after upgrade
             self.acq_off_cmd()
             self.acq_window_off_cmd()
             # ==================
             diffr.set_omega_velocity(self.default_speed)
             self.log.debug("#COLLECT# Closing detector cover")
             diffr.detector_cover_close(wait=True)
-<<<<<<< HEAD
-
-=======
             
->>>>>>> P11Collect additional debug after upgrade
         except RuntimeError:
             self.log.error(traceback.format_exc())
 
