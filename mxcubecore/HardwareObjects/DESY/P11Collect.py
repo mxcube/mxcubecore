1aeb1f7a10267406af6f4d15d3c0045d416c31d7
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-10-06 19:18:03 +0200

    Upgraded the characterisation part for P11 using EDNA

--- mxcubecore/HardwareObjects/DESY/P11Collect.py
+++ mxcubecore/HardwareObjects/DESY/P11Collect.py
@@ -131,6 +131,7 @@ class P11Collect(AbstractCollect):
 
         self.diffr = HWR.beamline.diffractometer
         detector = HWR.beamline.detector
+        
 
         dc_pars = self.current_dc_parameters
         collection_type = dc_pars["experiment_type"]
@@ -204,7 +205,7 @@ class P11Collect(AbstractCollect):
                 )
 
                 # Filepath to the EDNA processing
-                # filepath = os.path.join(basepath,"%s_%d" % (prefix, runno))
+                #filepath = os.path.join(basepath,"%s_%d" % (prefix, runno))
 
                 # setting up xds_dir for characterisation (used there internally to create dirs)
                 self.current_dc_parameters["xds_dir"] = os.path.join(
