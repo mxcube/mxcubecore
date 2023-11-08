e3fe51320202118c1d5952cc193700ea52c8cabd
Andrey Gruzinov <andrey.gruzinov@desy.de>
2023-11-08 18:03:37 +0100

    Renamed get_properties to get_properties_ as before and made appropriate changes in qt part-multi_state_brick.py (PR800)

--- mxcubecore/HardwareObjects/MotorsNPosition.py
+++ mxcubecore/HardwareObjects/MotorsNPosition.py
@@ -121,7 +121,7 @@ class MotorsNPosition(AbstractActuator):
     def get_position_list(self):
         return list(self._positions.keys())
 
-    def get_properties(self, position_index, property_name):
+    def get_properties_(self, position_index, property_name):
         """
              returns property with name property_name for position_index 
              if position_index is None returns OrderedDict with property_name for all positions
@@ -161,7 +161,7 @@ class MotorsNPosition(AbstractActuator):
         posidx = -1
         for name in self._positions:
             posidx += 1
-            if posname == self.get_properties(posidx, "posname"):
+            if posname == self.get_properties_(posidx, "posname"):
                 self._set_value(posidx)
                 return
 
