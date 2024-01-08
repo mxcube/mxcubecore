5b02af9abed56318ccfd8cd2ad50aa9848832227
FLorial Jean Baptiste <jflorial@embl.fr>
2024-01-08 15:45:05 +0100

    add MXCuBE license message

--- mxcubecore/HardwareObjects/HarvesterMaintenance.py
+++ mxcubecore/HardwareObjects/HarvesterMaintenance.py
@@ -1,5 +1,25 @@
+# encoding: utf-8
+#
+#  Project: MXCuBE
+#  https://github.com/mxcube
+#
+#  This file is part of MXCuBE software.
+#
+#  MXCuBE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MXCuBE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU General Lesser Public License
+#  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
+
 """
-Harvester Maintenance maintenance .
+Harvester Maintenance.
 """
 from mxcubecore.BaseHardwareObjects import HardwareObject
 
