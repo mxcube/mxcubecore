922723255367794b905f7b40d242062ca4f1f69d
Annie Heroux <annie.heroux@gmail.com>
2024-03-19 22:43:31 -0400

    some text but mostly formatting for the Command package. Some Warning in Sardana module about site specific software

--- mxcubecore/Command/__init__.py
+++ mxcubecore/Command/__init__.py
@@ -3,35 +3,32 @@
 The Command package is the place to put Command launchers
 and Channel readers/writers modules.
 
-The modules are organised by Control System Software used at different facility or specific to the hardware targeted.
+The modules are organised by Control System Software used at different facilities 
+or specific to the hardware targeted.
+Not all the facilities or even different beamlines from a facility have the same dependencies installed.
+**One should modify the** ``pyproject.toml`` **to address their needs.**
 
-Command launchers and channels should derive from the
-CObject class of the HardwareRepository.CommandContainer module.
-They should emit the following Qt signals :
+CommandObject class and ChannelObject class derive from the mxcubecore.CommandContainer module.
+They should both emit the following signals :
     - ``connected``, when connected to the control software
-    - ``disconnected``, when disconnected from control software and implement the
-      ``is_connected()`` method.
-Every command launcher class in the module should emit
-the following signals :
-    - ``commandBeginWaitReply``, when the command has been sent
-      and we are waiting for the reply. The only argument
-      is the command 'user' name. Useful for setting up a
-      message to the user. *SHOULD NOT BLOCK EXECUTION*
-    - ``commandReplyArrived``, when the reply for the command
-      is arrived. The arguments are: reply value and command
-      'user' name.
-    - ``commandFailed``, when the command failed to execute.
-The arguments are: ``error message`` and command 'user' name.
-    - ``commandAborted``, when the command has been aborted.
-The arguments are: command 'user' name.
+    - ``disconnected``, when disconnected from control software
+They should both implement the ``is_connected()`` method.
 
-Every command launcher should be a callable object ;
+    - Command launcher class in the module should emit the following signals :
+        - ``commandBeginWaitReply``, when the command has been sent and we are waiting for the reply.
+     
+        - ``commandReplyArrived``, when the reply for the commandis arrived.
+
+        - ``commandFailed``, when the command failed to execute.
+
+        - ``commandAborted``, when the command has been aborted.
+ 
+Every Command launcher should be a callable object ;
 the arguments provided are those for the remote command to
 be executed.
 
-A Channel object should emit the following signals :
-    - ``update``, when the value of the channel changes
-
-A channel object should have a ``get_value`` method at least,
-and an optional ``set_value``
+    - A Channel object should:
+        - have a ``get_value`` method
+        - have a ``set_value`` method (optional)
+        - emit an ``update`` signal when the value of the channel changes
 """
