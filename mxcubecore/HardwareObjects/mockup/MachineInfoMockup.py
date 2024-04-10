dc4265f881953f1f825ee2ccd74dd6c72c445f51
Antonia Beteva <beteva@debian8-devel.esrf.fr>
2024-04-10 20:07:51 +0200

    Refactor AbstractMachineInfo.

--- mxcubecore/HardwareObjects/mockup/MachineInfoMockup.py
+++ mxcubecore/HardwareObjects/mockup/MachineInfoMockup.py
@@ -2,36 +2,38 @@
 [Name] MachineInfoMockup
 
 [Description]
-MachineInfo hardware objects are used to obtain information from the accelerator
-control system.
+MachineInfo hardware objects are used to obtain information from the
+accelerator control system.
 
 This is a mockup hardware object, it simulates the behaviour of an accelerator
 information by :
 
     - produces a current value that varies with time
     - simulates a control room message that changes with some condition
+      ()
+    - simulates
 
 [Emitted signals]
-machInfoChanged
+valueChanged
    pars:  values (dict)
 
    mandatory fields:
      values['current']  type: str; desc: synchrotron radiation current in milli-amps
      values['message']  type: str; desc: message from control room
-     values['attention'] type: boolean; desc: False (if no special attention is required)
-                                            True (if attention should be raised to the user)
+     values['attention'] type: boolean; desc: False (no attention required)
+                                            True (attention raised to the user)
 
    optional fields:
-      any number of optional fields can be sent over with this signal by adding them in the
-      values dictionary
+      any number of optional fields can be sent over with this signal by
+      adding them in the values dictionary
 
       for example:
          values['lifetime']
          values['topup_remaining']
 """
+import time
 
 import gevent
-import time
 
 from mxcubecore import HardwareRepository as HWR
 from mxcubecore.HardwareObjects.abstract.AbstractMachineInfo import (
@@ -40,16 +42,16 @@ from mxcubecore.HardwareObjects.abstract.AbstractMachineInfo import (
 
 
 class MachineInfoMockup(AbstractMachineInfo):
+    """Simulates the behaviour of an accelerator information"""
+
     default_current = 200  # milliamps
     default_message = "Beam Delivered"
     default_lifetime = 45  # hours Lifetime
     default_topup_remaining = 70  # seconds
 
-    def __init__(self, *args):
-        AbstractMachineInfo.__init__(self, *args)
-
     def init(self):
         """Initialise some parameters and update routine."""
+        super().init()
         self._current = self.default_current
         self._message = self.default_message
         self._lifetime = self.default_lifetime
@@ -61,6 +63,7 @@ class MachineInfoMockup(AbstractMachineInfo):
         gevent.spawn(self._update_me)
 
     def _update_me(self):
+        """Simulate change of different parameters"""
         self.t0 = time.time()
 
         while True:
@@ -68,27 +71,22 @@ class MachineInfoMockup(AbstractMachineInfo):
             elapsed = time.time() - self.t0
             self._topup_remaining = abs((self.default_topup_remaining - elapsed) % 300)
             if self._topup_remaining < 60:
-                self._message = "ATTENTION: topup in %3d secs" % int(
-                    self._topup_remaining
-                )
+                self._message = f"ATTENTION: topup in {self._topup_remaining} s"
                 self.attention = True
             else:
                 self._message = self.default_message
                 self.attention = False
 
-            self._current = "%3.2f" % (
-                self.default_current - (3 - self._topup_remaining / 100.0) * 5
-            )
+            self._current = f"{(self.default_current - (3 - self._topup_remaining / 100.0) * 5):3.2f}"
 
-            values = dict()
-            values["current"] = self._current
+            values = {}
             values["message"] = self._message
-            values["lifetime"] = self._lifetime
             values["topup_remaining"] = self._topup_remaining
             values["attention"] = self.attention
-            self._mach_info_dict = values
 
-            self.emit("valueChanged", values)
+            # current and lifetime should be configured in the xml file
+            values.update(self.get_value())
+            self.update_value(values)
 
     def get_current(self) -> float:
         """Override method."""
@@ -108,6 +106,7 @@ class MachineInfoMockup(AbstractMachineInfo):
 
 
 def test():
+    """Test routine"""
     import sys
 
     hwr = HWR.get_hardware_repository()
