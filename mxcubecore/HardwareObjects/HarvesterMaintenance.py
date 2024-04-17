760b12154815bd256a0205c7413f433e9844630c
FLorial Jean Baptiste <jflorial@embl.fr>
2024-04-17 16:07:47 +0200

    Move Function/logic from mxcubeweb to mxcubecore level

--- mxcubecore/HardwareObjects/HarvesterMaintenance.py
+++ mxcubecore/HardwareObjects/HarvesterMaintenance.py
@@ -21,7 +21,10 @@
 """
 Harvester Maintenance.
 """
+import gevent
+import logging
 from mxcubecore.BaseHardwareObjects import HardwareObject
+from mxcubecore import HardwareRepository as HWR
 
 
 class HarvesterMaintenance(HardwareObject):
@@ -29,7 +32,7 @@ class HarvesterMaintenance(HardwareObject):
 
     """
     Actual implementation of the Harvester MAINTENANCE,
-    COMMANDS ONLY
+    COMMANDS, Actions and Calibration procedure, and Centering.
     """
 
     def __init__(self, *args, **kwargs):
@@ -156,4 +159,129 @@ class HarvesterMaintenance(HardwareObject):
             self._load_plate(args)
         if cmd_name == "set_room_temperature_mode":
             self._set_room_temperature_mode(args)
+        return True
+
+    def calibrate_pin(self) -> bool:
+        """
+            Pin Calibration Procedure
+            In other for the Centring procedure to work on a Harvested Pin
+            a Calibration Procedure need to be execute
+
+        Return (bool): whether the calibration procedure goes to end (True)
+        or had and exception (False)
+        """
+
+        self._harvester.load_calibrated_pin()
+        self._harvester._wait_sample_transfer_ready(None)
+        print("waiting 40 seconds before mount")
+        # For some reason the Harvester return READY too soon
+        # approximately 40 Second sooner
+        gevent.sleep(40)
+        sample_mount_device = HWR.beamline.sample_changer
+        mount_current_sample = sample_mount_device.load_a_pin_for_calibration()
+
+        if mount_current_sample:
+            try:
+                md = HWR.beamline.diffractometer
+                md._wait_ready()
+
+                sample_drift_x = float(self._harvester.get_last_sample_drift_offset_x())
+                sample_drift_y = float(self._harvester.get_last_sample_drift_offset_y())
+                sample_drift_z = float(
+                    -self._harvester.get_last_sample_drift_offset_z()
+                )
+
+                motor_pos_dict = {
+                    "kappa": float(
+                        md["HacentringReferencePosition"].get_property("kappa_ref")
+                    ),
+                    "kappa_phi": float(
+                        md["HacentringReferencePosition"].get_property("phi_ref")
+                    ),
+                    "phi": float(
+                        md["HacentringReferencePosition"].get_property("omega_ref")
+                    ),
+                    "phiy": md.phiyMotor.get_value() + sample_drift_x,
+                }
+
+                md.move_motors(motor_pos_dict)
+                md._wait_ready()
+                md.centringFocus.set_value_relative(sample_drift_z, None)
+                md.centringVertical.set_value_relative(sample_drift_y, None)
+
+                md.save_current_motor_position()
+                self._harvester.calibration_state(True)
+
+                logging.getLogger("user_level_log").info(
+                    "Pin Calibration Step 1 Succeed"
+                )
+                logging.getLogger("user_level_log").info(
+                    "User Need to Perform an  3 click centring"
+                )
+                return True
+            except Exception:
+                logging.getLogger("user_level_log").exception("Pin Calibration Failed")
+                return False
+        else:
+            logging.getLogger("user_level_log").error("Pin Calibration Failed")
+            logging.getLogger("user_level_log").error(
+                "Sample Changer could not mount Pin"
+            )
+            return False
+
+    def validate_calibration(self) -> bool:
+        """
+        finish Calibration Procedure step 2
+        after user ran a 3 click centering
+        Return (bool): whether the step 2 of calibration procedure
+        goes to end (True) or had and exception (False)
+        """
+        try:
+            md = HWR.beamline.diffractometer
+
+            motor_pos_dict = {
+                "focus": md.focusMotor.get_value(),
+                "phiy": md.phiyMotor.get_value(),
+                "phiz": md.phizMotor.get_value(),
+                "centring_focus": md.centringFocus.get_value(),
+                "centring_vertical": md.centringVertical.get_value(),
+            }
+
+            saved_position = md.saved_motor_position
+            # find offset position based on old and new motor position
+            new_motor_offset = {
+                "focus": motor_pos_dict["focus"] - saved_position["focus"],
+                "phiy": motor_pos_dict["phiy"] - saved_position["phiy"],
+                "phiz": motor_pos_dict["phiz"] - saved_position["phiz"],
+                "centring_focus": (
+                    motor_pos_dict["centring_focus"] - saved_position["centring_focus"]
+                ),
+                "centring_vertical": (
+                    motor_pos_dict["centring_vertical"]
+                    - saved_position["centring_vertical"]
+                ),
+            }
+
+            calibrated_motor_offset = {
+                "focus": new_motor_offset["focus"] + new_motor_offset["centring_focus"],
+                "phiy": new_motor_offset["phiy"],
+                "phiz": (
+                    new_motor_offset["phiz"] + new_motor_offset["centring_vertical"]
+                ),
+            }
+
+            # we store the motor offset in the Harvester, to be used for sample centering
+            self._harvester.store_calibrated_pin(
+                calibrated_motor_offset["focus"],
+                calibrated_motor_offset["phiy"],
+                calibrated_motor_offset["phiz"],
+            )
+
+            self._harvester.calibration_state(False)
+        except Exception:
+            logging.getLogger("user_level_log").exception(
+                "Pin Calibration / validation Failed"
+            )
+            return False
+
         return True
