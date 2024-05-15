9e4ae3c8a369597cc84250472b501bda84ae4cbc
ssrfmx <qinwenming@sari.ac.cn>
2024-05-15 19:58:53 +0800

    20240515,change to pilatus1m and comment the detector cover

--- mxcubecore/HardwareObjects/SSRF/BL19U1/BL19U1DetCover.py
+++ mxcubecore/HardwareObjects/SSRF/BL19U1/BL19U1DetCover.py
@@ -9,32 +9,32 @@ class DetCover(object):
 
     # DO1:0064
     def closeDetCover(self):
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = "10.30.61.75"
-        port = 50000
-        server.connect((host, port))
-        modbus_data = 'CCDDA10100000001A346'
-        # binascii Send hexadecimal
-        send_data = binascii.a2b_hex(modbus_data)
-        server.sendall(send_data)
-        time.sleep(0.5)
-        server.close()
-        #pass
+        # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # host = "10.30.61.75"
+        # port = 50000
+        # server.connect((host, port))
+        # modbus_data = 'CCDDA10100000001A346'
+        # # binascii Send hexadecimal
+        # send_data = binascii.a2b_hex(modbus_data)
+        # server.sendall(send_data)
+        # time.sleep(0.5)
+        # server.close()
+        pass
 
 
     def openDetCover(self):
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = "10.30.61.75"
-        port = 50000
-        server.connect((host, port))
-        # modbus_data = '000100000006FF050064FF00'
-        modbus_data = 'CCDDA10100010001A448'
-        # binascii Send hexadecimal
-        send_data = binascii.a2b_hex(modbus_data)
-        server.sendall(send_data)
-        time.sleep(0.5)
-        server.close()
-        #pass
+        # server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # host = "10.30.61.75"
+        # port = 50000
+        # server.connect((host, port))
+        # # modbus_data = '000100000006FF050064FF00'
+        # modbus_data = 'CCDDA10100010001A448'
+        # # binascii Send hexadecimal
+        # send_data = binascii.a2b_hex(modbus_data)
+        # server.sendall(send_data)
+        # time.sleep(0.5)
+        # server.close()
+        pass
 
 if __name__ == "__main__":
    detCover = DetCover()
