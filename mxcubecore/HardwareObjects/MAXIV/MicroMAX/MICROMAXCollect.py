14581115bc5c1d5c221aede4d17ab43a94c525c2
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-05-20 10:14:59 +0200

    MicroMAX: don't send unit cell params to Eiger
    The unit cell parameters should only be configured on Jungfrau
    detector.

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
@@ -1240,17 +1240,18 @@ class MICROMAXCollect(DataCollect):
         config["FilenamePattern"] = name_pattern
 
         # for Jungfrau
-        sample_info = self.current_dc_parameters["sample_reference"]
-        cell = sample_info.get("cell", "0,0,0,0,0,0")
-        if cell == ",,,,,":
-            cell = "0,0,0,0,0,0"
-        cell_float = [float(x) for x in cell.split(",")]
-        config["UnitCellA"] = cell_float[0]
-        config["UnitCellB"] = cell_float[1]
-        config["UnitCellC"] = cell_float[2]
-        config["UnitCellAlpha"] = cell_float[3]
-        config["UnitCellBeta"] = cell_float[4]
-        config["UnitCellGamma"] = cell_float[5]
+        if self.detector_hwobj.get_property("model") == "JUNGFRAU":
+            sample_info = self.current_dc_parameters["sample_reference"]
+            cell = sample_info.get("cell", "0,0,0,0,0,0")
+            if cell == ",,,,,":
+                cell = "0,0,0,0,0,0"
+            cell_float = [float(x) for x in cell.split(",")]
+            config["UnitCellA"] = cell_float[0]
+            config["UnitCellB"] = cell_float[1]
+            config["UnitCellC"] = cell_float[2]
+            config["UnitCellAlpha"] = cell_float[3]
+            config["UnitCellBeta"] = cell_float[4]
+            config["UnitCellGamma"] = cell_float[5]
 
         # make sure the filewriter is enabled
         self.detector_hwobj.enable_filewriter()
