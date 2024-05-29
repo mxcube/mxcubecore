ade687bc25b2cd8d0c96344af9d864fb409182f0
ssrfmx <qinwenming@sari.ac.cn>
2024-05-29 18:18:21 +0800

    20240529 改回探测器6m，恢复探测器盖板，但距离还是固定值

--- mxcubecore/HardwareObjects/SSRF/BL19U1/LNLSPilatusDet.py
+++ mxcubecore/HardwareObjects/SSRF/BL19U1/LNLSPilatusDet.py
@@ -706,7 +706,7 @@ class LNLSPilatusDet(AbstractDetector):
         # self.set_channel_value("det_phi_omega_incr", file_template)
         self.set_channel_value("det_phi_oscill_axis", self.header["Oscillation_axis"])
         self.set_channel_value("det_num_oscill", self.header["N_oscillations"])
-        self.set_channel_value("det_beam_x", 470)
+        self.set_channel_value("det_beam_x", 460)
         # self.set_channel_value("det_beam_y", 1331.00)
     #    beamy = self.get_beamy()
         beamy = 480
