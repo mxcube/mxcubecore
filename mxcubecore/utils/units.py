ca4967898870c9fb1b647da11727bcd1a4405875
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-06-17 08:05:58 +0200

    add utility module for unit conversions
    Adds mxcubecore.utils.units module, so there is a common place
    for unit converson utility functions.
    
    Adds some initial function to convert between time, energy, length
    and current units.

---
+++ mxcubecore/utils/units.py
@@ -0,0 +1,71 @@
+"""
+Utility functions for converting between different units.
+"""
+
+#
+# time units
+#
+
+
+def us_to_sec(us: float) -> float:
+    """
+    convert microseconds (μs) to seconds
+    """
+    return us / 1_000_000.0
+
+
+def sec_to_us(sec: float) -> float:
+    """
+    convert seconds to microseconds (μs)
+    """
+    return sec * 1_000_000.0
+
+
+def sec_to_hour(sec: float) -> float:
+    """
+    convert seconds to hours
+    """
+    return sec / (60 * 60)
+
+
+#
+# energy units
+#
+
+
+def ev_to_kev(ev: float) -> float:
+    """
+    convert eV value to KeV value
+    """
+    return ev / 1000.0
+
+
+#
+# length units
+#
+
+
+def meter_to_mm(meters: float) -> float:
+    """
+    convert meters to millimeters (mm)
+    """
+    return meters * 1000.0
+
+
+def mm_to_meter(millimeters: float) -> float:
+    """
+    convert millimeters (mm) to meters
+    """
+    return millimeters / 1000.0
+
+
+#
+# current units
+#
+
+
+def A_to_mA(amp: float) -> float:
+    """
+    convert Ampere (A) to milli Ampere (mA)
+    """
+    return amp * 1000
