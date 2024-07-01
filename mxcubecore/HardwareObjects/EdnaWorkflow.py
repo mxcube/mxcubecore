a252af7e2a5817ed6be3843db5d8ed209e1b21be
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-07-01 11:25:12 +0200

    yaml: update edna workflow to new API
    The method self.name() is now a property self.name.
    
    Also, use the f-string for generatng log messages, for a more
    modern look and feel of the code.

--- mxcubecore/HardwareObjects/EdnaWorkflow.py
+++ mxcubecore/HardwareObjects/EdnaWorkflow.py
@@ -106,9 +106,7 @@ class EdnaWorkflow(HardwareObject):
 
     def state_changed(self, new_value):
         new_value = str(new_value)
-        logging.getLogger("HWR").debug(
-            "%s: state changed to %r", str(self.name()), new_value
-        )
+        logging.getLogger("HWR").debug(f"{self.name}: state changed to {new_value}")
         self.emit("stateChanged", (new_value,))
 
     def workflow_end(self):
