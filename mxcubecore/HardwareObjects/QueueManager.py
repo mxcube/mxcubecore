9ffedc5e337cfab0434a08a0f7b8510ef60daf07
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-07-26 14:16:19 +0200

    remove duplicate import

--- mxcubecore/HardwareObjects/QueueManager.py
+++ mxcubecore/HardwareObjects/QueueManager.py
@@ -8,7 +8,6 @@ container of the queue, note the inheritance from QueueEntryContainer. See the
 documentation for the queue_entry module for more information.
 """
 import logging
-import traceback
 import gevent
 import traceback
 
