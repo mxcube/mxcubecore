f73598012172d5114325a9ead6feffc79e45d40e
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-08-12 16:42:54 +0200

    fix issue with skipped calls to init() for YAML HWOBJs
    Only set 'msg0' variable when loading the 'beamline_config.yml'
    file. Otherwise the call to HWOBJ.init() will be skipped on
    line 225, if it have any sub-HWOBJs configured.

--- mxcubecore/HardwareRepository.py
+++ mxcubecore/HardwareRepository.py
@@ -174,7 +174,7 @@ def load_from_yaml(
 
         result._config = result.HOConfig(**config)
 
-        if objects:
+        if _container is None:
             load_time = 1000 * (time.time() - start_time)
             msg1 = "Start loading contents:"
             _table.append(
