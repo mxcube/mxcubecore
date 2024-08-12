dc28f4dc4eeef3fa5cce6e207280f6b20338a094
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-08-12 18:22:47 +0200

    port AbstractDetector to use get_property()
    Update AbstractDetector class to access it's 'beam' setting with
    get_property().
    
    Replaces self["beam"] expression, as it does not work when YAML
    config file is used.

--- mxcubecore/HardwareObjects/abstract/AbstractDetector.py
+++ mxcubecore/HardwareObjects/abstract/AbstractDetector.py
@@ -84,13 +84,10 @@ class AbstractDetector(HardwareObject):
         self._metadata = {}
 
     def init(self):
-        """Initialise some common paramerters"""
+        """Initialise some common parameters"""
         super().init()
 
-        try:
-            self._metadata = dict(self["beam"].get_properties())
-        except KeyError:
-            pass
+        self._metadata = self.get_property("beam", {})
 
         self._images_per_file = self.get_property("images_per_file", 100)
 
