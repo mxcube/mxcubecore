d40f2a3441d04d66d31bad8326fd81471c8e4d52
Antonia Beteva <beteva@debian8-devel.esrf.fr>
2024-08-20 17:53:08 +0200

    ESRF Implementation of new beam size and definer handling

--- mxcubecore/HardwareObjects/ESRF/ID232BeamDefiner.py
+++ mxcubecore/HardwareObjects/ESRF/ID232BeamDefiner.py
@@ -1,54 +1,89 @@
-import ast
+# encoding: utf-8
+#
+#  Project: MXCuBE
+#  https://github.com/mxcube
+#
+#  This file is part of MXCuBE software.
+#
+#  MXCuBE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MXCuBE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU General Lesser Public License
+#  along with MXCuBE. If not, see <http://www.gnu.org/licenses/>.
+"""
+ID23-2 Beam Definer.
+
+Example xml configuration:
+
+.. code-block:: xml
+
+ <object class="ESRF.ESRFBeamDefiner"
+   <username>Beam Definer</username>
+   <object href="/bliss" role="controller"/>
+   # bliss tranfocator object names
+   <tf1>tf</tf1>
+   <tf2>tf2</tf2>
+   <beam_config>
+      <name>4x4 um</name>
+      <beam_size>0.004, 0.004</beam_size>
+      <tf1>0 0 0 0 0 0 0 0 0</tf1>
+      <tf2>0 0 1 1 0 0 0 0 0</tf2>
+   </beam_config>
+   <beam_config>
+      <name>4x8 um</name>
+      <beam_size>0.004, 0.008</beam_size>
+      <tf1>0 0 0 0 0 0 0 0 0</tf1>
+      <tf2>0 0 1 0 1 0 0 0 0</tf2>
+   </beam_config>
+   <default_size_name>4x4 um</default_size_name>
+ </object>
+"""
+
+__copyright__ = """ Copyright © by the MXCuBE collaboration """
+__license__ = "LGPLv3+"
+
 
 from enum import Enum
 from gevent import Timeout, sleep
-
-from mxcubecore.BaseHardwareObjects import HardwareObject
-from mxcubecore.HardwareObjects.abstract.AbstractNState import AbstractNState
-from bliss.common import event
+from mxcubecore.HardwareObjects.ESRF.ESRFBeamDefiner import ESRFBeamDefiner
 
 
-class ID232BeamDefiner(AbstractNState):
+class ID232BeamDefiner(ESRFBeamDefiner):
+    """ID23-2 beam definer implementattion"""
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.tf_cfg_by_name = {}
-        self.coef_by_name = {}
-        self.size_by_name = {}
-        self.pos_names = []
+        self.tf_cfg = {}
         self.controller = None
+        self.tf1 = None
+        self.tf2 = None
 
     def init(self):
+        """Initialisation"""
         super().init()
 
         self.controller = self.get_object_by_role("controller")
-        event.connect(self.controller.tf, "state", self._tf_state_updated)
-        event.connect(self.controller.tf2, "state", self._tf_state_updated)
+        self.tf1 = self.controller.config.get(self.get_property("tf1"))
+        self.tf2 = self.controller.config.get(self.get_property("tf2"))
+        for beam_cfg in self.config:
+            name = beam_cfg.get_property("name")
+            tf1 = [int(x) for x in beam_cfg.get_property("tf1").split()]
+            tf2 = [int(x) for x in beam_cfg.get_property("tf2").split()]
+            self.tf_cfg[name] = {"tf1": tf1, "tf2": tf2}
 
-        cfg = self["lenses_config"]
-        if not isinstance(cfg, list):
-            cfg = [cfg]
+        self.connect(self.tf1, "state", self._tf_update_state)
+        self.connect(self.tf2, "state", self._tf_update_state)
 
-        for lens_cfg in cfg:
-            name = lens_cfg.get_property("name")
-            tf1 = lens_cfg.get_property("tf1").split()
-            tf2 = lens_cfg.get_property("tf2").split()
-            size = lens_cfg.get_property("size")
-            coef = lens_cfg.get_property("coef")
-            self.pos_names.append(lens_cfg.get_property("name"))
-            self.tf_cfg_by_name[name] = {
-                "tf1": ["IN" if x else "OUT" for x in map(int, tf1)],
-                "tf2": ["IN" if x else "OUT" for x in map(int, tf2)],
-            }
-            self.size_by_name[name] = tuple([float(x) for x in ast.literal_eval(size)])
-            self.coef_by_name[name] = float(coef)
-
-        # check if we have values other that UKNOWN
-        if len(self.VALUES) == 1:
-            self._initialise_values()
-
-    def is_ready(self):
-        return self.controller is not None
+        if self.get_value() == self.VALUES.UNKNOWN and self._default_name:
+            # set default beam value
+            self.set_value(self._default_name)
 
     def get_state(self):
         """Get the device state.
@@ -57,89 +92,65 @@ class ID232BeamDefiner(AbstractNState):
         """
         return self.STATES.READY
 
-    def get_limits(self):
-        return (1, len(self.pos_names))
-
-    def get_value(self):
-        """Get the device value
-        Returns:
-            (int): The position index.
-        """
-        try:
-            return self.VALUES[self.get_current_position_name()]
-        except (ValueError, KeyError):
-            return self.VALUES.UNKNOWN
-
-    def _tf_state_updated(self, new_state=None):
+    def _tf_update_state(self, state=None):
+        """Update the value"""
         name = self.get_current_position_name()
         self.emit("valueChanged", name)
-        self.emit(
-            "diameterIndexChanged", (name, (1e6, self.size_by_name.get(name, 1e6)))
-        )
-
-    def connect_notify(self, signal):
-        return self._tf_state_updated()
-
-    def get_predefined_positions_list(self):
-        return self.pos_names
 
     def get_current_status(self):
-        tf1_status = self.controller.tf.wago.get("stat")[::2]
-        tf2_status = self.controller.tf2.wago.get("stat")[::2]
+        """Get the status of the transfocators.
+        Returns:
+            (tuple): Tuple of two lists, giving the state for each lense
+        """
+        tf1_status = self.tf1.wago.get("stat")[::2]
+        tf2_status = self.tf2.wago.get("stat")[::2]
         return tf1_status, tf2_status
 
-    def get_current_position_name(self, *args):
+    def get_current_position_name(self):
+        """Get the current beam size name.
+        Returns:
+            (str): Current beam size name.
+        """
         try:
-            tf1_state = self.controller.tf.status_read()[1].split()
-            tf2_state = self.controller.tf2.status_read()[1].split()
-        except:
+            tf1_state, tf2_state = self.get_current_status()
+        except ValueError:
             return "UNKNOWN"
-        for name in self.pos_names:
-            tf1_cfg = self.tf_cfg_by_name[name]["tf1"]
-            tf2_cfg = self.tf_cfg_by_name[name]["tf2"]
 
-            for i, tf_pos in enumerate(zip(tf1_state, tf2_state)):
-                if tf_pos[0] in (tf1_cfg[i], "---") and tf_pos[1] in (
-                    tf2_cfg[i],
-                    "---",
-                ):
-                    continue
-                else:
-                    break
-            else:
+        for name in self.beam_config:
+            if (
+                self.tf_cfg[name]["tf1"] == tf1_state
+                and self.tf_cfg[name]["tf2"] == tf2_state
+            ):
                 return name
+        return "UNKNOWN"
 
-    def set_value(self, name, timeout=None):
+    def set_value(self, value, timeout=None):
         """Set the beam size.
         Args:
-            name (str): position name
+            value(str): name of the beam size to set.
+            timeout(float): Timeout to wait for the execution to finish [s].
+        Raises:
+            RuntimeError: Cannot change beam size.
         """
-        tf1_cfg = [1 if x == "IN" else 0 for x in self.tf_cfg_by_name[name]["tf1"]]
-        tf2_cfg = [1 if x == "IN" else 0 for x in self.tf_cfg_by_name[name]["tf2"]]
+        if isinstance(value, Enum):
+            value = value.name
 
-        self.controller.tf.set(*tf1_cfg)
-        self.controller.tf2.set(*tf2_cfg)
-        self.wait_ready((tf1_cfg,tf2_cfg), timeout)
+        tf1_cfg  = self.tf_cfg[value]["tf1"]
+        tf2_cfg = self.tf_cfg[value]["tf2"]
+        self.tf1.set(*tf1_cfg)
+        self.tf2.set(*tf2_cfg)
 
-    def wait_ready(self, status, timeout=None):
+        try:
+            self.wait_status((tf1_cfg, tf2_cfg), timeout)
+        except RuntimeError as err:
+            raise RuntimeError("Cannot change beam size") from err
+
+    def wait_status(self, status, timeout=None):
         """Wait timeout seconds until status reached
         Args:
+            status (tuple): Transfocator status to be reached.
             timeout (float): Timeout [s]. Defaults to None.
         """
         with Timeout(timeout, RuntimeError("Execution timeout")):
             while status != self.get_current_status():
                 sleep(0.5)
-
-    def _initialise_values(self):
-        """Initialise the ValueEnum from the hardware
-        Raises:
-            RuntimeError: No aperture diameters defined.
-        """
-        values = {}
-        for val in self.pos_names:
-            values[val] = [self.pos_names.index(val), self.size_by_name[val]]
-
-        self.VALUES = Enum(
-            "ValueEnum",
-            dict(values, **{item.name: item.value for item in self.VALUES}),
-        )
---
+++ mxcubecore/HardwareObjects/ESRF/ID30A3BeamDefiner.py
@@ -0,0 +1,128 @@
+# encoding: utf-8
+#
+#  Project: MXCuBE
+#  https://github.com/mxcube
+#
+#  This file is part of MXCuBE software.
+#
+#  MXCuBE is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MXCuBE is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU General Lesser Public License
+#  along with MXCuBE. If not, see <http://www.gnu.org/licenses/>.
+"""
+ID30-A3 Beam Definer.
+
+Example xml configuration:
+
+.. code-block:: xml
+
+ <object class="ESRF.ID30A3BeamDefiner"
+   <object href="/udiff_aperture" role="controller"/>
+   <beam_config>
+      <name>15um, A15 um</name>
+      <beam_size>0.015, 0.015</beam_size>
+      <aperture_size>15</aperture_size>
+   </beam_config>
+   <beam_config>
+      <name>15um, A15 um</name>
+      <beam_size>0.015, 0.015</beam_size>
+      <aperture_size>30</aperture_size>
+   </beam_config>
+   <beam_config>
+      <name>7um, A7 um</name>
+      <beam_size>0.007, 0.007</beam_size>
+      <aperture_size>7</aperture_size>
+   </beam_config>
+ </object>
+"""
+
+__copyright__ = """ Copyright © by the MXCuBE collaboration """
+__license__ = "LGPLv3+"
+
+from enum import Enum
+from mxcubecore.HardwareObjects.ESRF.ESRFBeamDefiner import ESRFBeamDefiner
+
+
+class ID30A3BeamDefiner(ESRFBeamDefiner):
+    """ID30-A3 beam definer implementattion"""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.controller = None
+
+    def init(self):
+        """Initialisation"""
+        super().init()
+
+        self.controller = self.get_object_by_role("controller")
+        _dict = {}
+        for beam_cfg in self.config:
+            name = beam_cfg.get_property("name")
+            _ap_size = beam_cfg.get_property("aperture_size")
+            _aperture = self.controller.value_to_enum(_ap_size, idx=1)
+            _dict[name] = [self.beam_config[name], _aperture]
+
+        self.beam_config = _dict
+        self._initialise_values()
+        self.connect(self.controller, "valueChanged", self._update_name)
+
+    def get_state(self):
+        """Get the device state.
+        Returns:
+            (enum 'HardwareObjectState'): Device state.
+        """
+        return self.controller.get_state()
+
+    def _update_name(self, value=None):
+        """
+        Emits:
+            valueChanged (str): Current beam size name.
+        """
+        name = self.get_current_position_name()
+        self.emit("valueChanged", name)
+
+    def get_current_position_name(self):
+        """Get the current beam size name.
+        Returns:
+            (str): Current beam size name.
+        """
+        _aperture = self.controller.get_value()
+        for name in self.beam_config:
+            if self.beam_config[name][1] == _aperture:
+                return name
+        return "UNKNOWN"
+
+    def get_value(self):
+        """Get the device value
+        Returns:
+            (Enum): The current position Enum.
+        """
+        try:
+            value = self.VALUES[self.get_current_position_name()]
+            if isinstance(value.value, tuple):
+                return value
+            return Enum("Dummy", {value.name: value.value[0]})[value.name]
+        except (ValueError, KeyError):
+            return self.VALUES.UNKNOWN
+
+    def set_value(self, value, timeout=None):
+        """Set the beam size.
+        Args:
+            value(str): name of the beam size to set.
+            timeout(float): Timeout to wait for the execution to finish [s].
+        Raises:
+            RuntimeError: Cannot change beam size.
+        """
+        if isinstance(value, Enum):
+            value = value.name
+
+        #self.controller.set_value(self.ctrl_cfg[value], timeout=timeout)
+        self.controller.set_value(self.beam_config[value][1], timeout=timeout)
