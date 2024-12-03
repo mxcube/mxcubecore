3e1b938d6f5505815fa098a25a6f2c6dbb320d54
agruzinov <78498907+agruzinov@users.noreply.github.com>
2024-12-03 14:49:04 +0100

    Update P11SampleChanger.py

--- mxcubecore/HardwareObjects/DESY/P11SampleChanger.py
+++ mxcubecore/HardwareObjects/DESY/P11SampleChanger.py
@@ -366,11 +366,10 @@ class P11SampleChanger(SampleChanger):
         try:
             sample_no = self.chan_current_sample.get_value()
             self.log.debug("P11SampleChanger - Loaded sample no is: %s" % sample_no)
-            return self.sample_no_to_address(sample_no)
         except Exception as e:
-            self.log.error("Failed to read or convert the current sample: %s. Returning 0 - no sample loaded" % e)
+            self.log.error("Failed to read or convert the current sample: %s" % e)
             sample_no = 0
-            return self.sample_no_to_address(sample_no)
+        return self.sample_no_to_address(sample_no)
             
     def _do_abort(self):
         return
