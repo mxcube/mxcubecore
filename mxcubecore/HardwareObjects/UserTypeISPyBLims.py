61f97dbfa8cea6c85151d233e7b036631d8493a8
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-01-16 14:57:43 +0100

    ISPyB LIMS: fix import path
    remove the extra 'mxcubecore' from LimsSessionManager import

--- mxcubecore/HardwareObjects/UserTypeISPyBLims.py
+++ mxcubecore/HardwareObjects/UserTypeISPyBLims.py
@@ -8,7 +8,7 @@ from suds.client import Client
 from suds.sudsobject import asdict
 
 from mxcubecore.HardwareObjects.abstract.ISPyBAbstractLims import ISPyBAbstractLIMS
-from mxcubecore.mxcubecore.model.lims_session import LimsSessionManager
+from mxcubecore.model.lims_session import LimsSessionManager
 
 try:
     from urllib2 import URLError
