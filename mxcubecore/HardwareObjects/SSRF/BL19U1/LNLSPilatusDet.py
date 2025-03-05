2d942985eb69e814b76de2283ec1165619222aa9
ssrfmx <qinwenming@sari.ac.cn>
2025-03-05 21:06:31 +0800

    更改光心

--- mxcubecore/HardwareObjects/SSRF/BL19U1/LNLSPilatusDet.py
+++ mxcubecore/HardwareObjects/SSRF/BL19U1/LNLSPilatusDet.py
@@ -717,7 +717,7 @@ class LNLSPilatusDet(AbstractDetector):
         # self.set_channel_value("det_phi_omega_incr", file_template)
         self.set_channel_value("det_phi_oscill_axis", self.header["Oscillation_axis"])
         self.set_channel_value("det_num_oscill", self.header["N_oscillations"])
-        self.set_channel_value("det_beam_x", )
+        self.set_channel_value("det_beam_x", 1229)
         # self.set_channel_value("det_beam_y", 1331.00)
         beamy = self.get_beamy()
         self.set_channel_value("det_beam_y", beamy)
