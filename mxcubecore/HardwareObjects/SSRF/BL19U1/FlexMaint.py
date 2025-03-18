dba32ed3a44a282b8cbe2f525795824c2109272f
ssrfmx <qinwenming@sari.ac.cn>
2025-03-18 14:03:03 +0800

    更改上次提交错误，SC.py 的status，前后端联动，sc_maint.py的poweroff，会设置SC.py的state和status为disable…

--- mxcubecore/HardwareObjects/SSRF/BL19U1/FlexMaint.py
+++ mxcubecore/HardwareObjects/SSRF/BL19U1/FlexMaint.py
@@ -904,14 +904,21 @@ class FlexMaint(Equipment):
 
 
         if cmd_name == "powerOn":
-            # logging.getLogger("HWR").debug(
-            #     "powerOn命令 %d"
-            # )
-            # self._do_power_state(True)
-            pass
+            SC = HWR.beamline.sample_changer
+            SC._set_state(AbstractSampleChanger.SampleChangerState.Ready)
+            SC._set_status('Ready')
+            print('powerOn cmd')
+            logging.getLogger("HWR").debug(
+                "powerOn命令 %d"
+            )
+            self._do_power_state(True)
+
         if cmd_name == "powerOff":
-            # self._do_power_state(False)
-            pass
+            SC = HWR.beamline.sample_changer
+            SC._set_state(AbstractSampleChanger.SampleChangerState.Disabled)
+            SC._set_status('DISABLED')
+            print('powerOff cmd')
+            self._do_power_state(False)
 
         # if cmd_name == "regulon":
         #     self._do_enable_regulation()
