f197339631d3105be9b13000663f41552be4d5c4
Alejandro De Maria Antolinos <demariaa@esrf.fr>
2025-03-27 14:44:34 +0100

    Added inline documentation

--- mxcubecore/model/lims_session.py
+++ mxcubecore/model/lims_session.py
@@ -16,28 +16,43 @@ from pydantic.v1 import (
 
 
 class Proposal(BaseModel):
-    proposal_id: str = ""
-    person_id: str = ""
-    type: str = ""
-    code: str = ""
+    """Represents a proposal with key identifying details."""
+
+    proposal_id: str = ""  # Unique identifier for the proposal
+    person_id: str = ""  # Identifier of the person associated
+    type: str = ""  # Type/category of the proposal (e.g., "MX")
+    code: str = ""  # Specific code associated with the proposal e.g., "IH-LS"
+    # Proposal number; may be sequential or unique (uniqueness may depend on
+    # the combination of `code` and `number`)
     number: str = ""
-    title: str = ""
+    title: str = ""  # Title of the proposal
 
 
 class Lims(BaseModel):
-    name: str = ""
-    description: str = ""
+    """Represents a LIMS system"""
+
+    name: str = ""  # Unique identifier for the LIMS (e.g: ispyb, icat, ...)
+    description: str = ""  # Free text with a short description
 
 
 class Session(BaseModel):
-    session_id: str = ""
-    beamline_name: str = ""
-    start_date: str = ""  # YYYYMDD
+    """Represents a experiment in a specific time slot in a beamline
+    for a proposal
+    This class has been kept more or less as-it-is for legacy purposes
+    because ISPyB rely on it
+    """
+
+    session_id: str = (
+        ""  # Unique identifier for the experiment (it's dependent of the LIMS)
+    )
+    beamline_name: str = ""  # Beamline where the experiment is scheduled
+    start_date: str = ""  # The official start date. Format: YYYYMDD
     start_time: str = ""
-    end_date: str = ""  # YYYYMDD
+    end_date: str = ""  # The official end date. Format: YYYYMDD
     end_time: str = ""
 
-    # Proposal information
+    # Proposal information. It is kept for legacy purpose but it
+    # should be replaced by the Proposal object
     title: str = ""
     code: str = ""
     number: str = ""
@@ -51,37 +66,57 @@ class Session(BaseModel):
         default_factory=lambda: datetime.now() + timedelta(days=1)
     )
 
-    # If rescheduled the actual dates are used instead
-    actual_start_date: str = ""
+    # Actual start and end date is used when a session is "moved" in time.
+    # Example: data is collected before or after it was originally scheduled
+    actual_start_date: str = (
+        ""  # Start date of a session that has been rescheduled time-wise
+    )
     actual_start_time: str = ""
     actual_end_date: str = ""
     actual_end_time: str = ""
 
-    nb_shifts: str = ""
-    scheduled: str = ""
+    nb_shifts: str = (
+        ""  # Number of shifts allocated to a session.
+        # A shift is typically 8 hours, though this may vary.
+    )
+    scheduled: str = (
+        ""  # True if the session has been officialiy scheduled and approved
+    )
 
-    # status of the session depending on wether it has been rescheduled or moved
-    is_rescheduled: bool = False
-    is_scheduled_time: bool = False
-    is_scheduled_beamline: bool = False
+    is_rescheduled: bool = (
+        False  # if the session has been rescheduled in terms of time/beamline.
+    )
+    is_scheduled_beamline: bool = (
+        False  # if the session is scheduled in the current beamline
+    )
+    is_scheduled_time: bool = False  # True if the session is currently active
 
     # direct links to different services
-    user_portal_URL: Optional[str] = None
-    data_portal_URL: Optional[str] = None
-    logbook_URL: Optional[str] = None
+    user_portal_URL: Optional[str] = None  # Link to the session page in the UP
+    data_portal_URL: Optional[str] = (
+        None  # Link to the session page in the data portal or lims
+    )
+    logbook_URL: Optional[str] = (
+        None  # Link to the session page in the electronic logbook
+    )
 
-    volume: Optional[str] = None
-    dataset_count: Optional[str] = None
-    sample_count: Optional[str] = None
+    volume: Optional[str] = None  # Volume (bytes) of data produced
+    dataset_count: Optional[str] = None  # Number of datasets collected
+    sample_count: Optional[str] = None  # Number of samples collected
 
 
 class Instrument(BaseModel):
+    """This class represents a beamline"""
+
     name: str
     id: int
     instrumentScientists: List[Any]
 
 
 class Investigation(BaseModel):
+    """This class represents a investigation and is a proposal
+    to replace the session class"""
+
     name: str
     startDate: datetime
     endDate: datetime
@@ -95,10 +130,13 @@ class Investigation(BaseModel):
 
 
 class Parameter(BaseModel):
-    name: str
-    value: str
-    id: int
-    units: str
+    """Generic reprentation of a parameter atached to any entity like
+    investigation, sample, dataset."""
+
+    name: str  # name of the parameter
+    value: str  # its value
+    id: int  # identifier of the parameter
+    units: str  # units if any
 
 
 class MetaPage(BaseModel):
@@ -113,21 +151,34 @@ class Meta(BaseModel):
 
 
 class LimsUser(BaseModel):
-    user_name: str = ""
-    sessions: Optional[List[Session]] = []
+    """Represents and users that is connected to MXCuBE"""
+
+    user_name: str = ""  # identifier of the users, most likely login name
+    sessions: Optional[
+        List[Session]
+    ] = []  # The sessions that the user is allowed to collect data from.
 
 
 class LimsSessionManager(BaseModel):
-    active_session: Optional[Session] = None
-    sessions: Optional[List[Session]] = []
-    users: Optional[Dict[str, LimsUser]] = {}
+    active_session: Optional[Session] = None  # the current active sessions
+    sessions: Optional[
+        List[Session]
+    ] = []  # Selectable sessions that are calculated based on the connected users
+    users: Optional[
+        Dict[str, LimsUser]
+    ] = {}  # the list of users that are currently connected
 
 
 class SampleSheet(BaseModel):
+    """Represents a description of the sample sheet as defined on
+    some user portals"""
+
     id: int
-    name: str
+    name: str  # Name of the samplesheet that use to correspond to the protein's name
     investigation: Investigation
     modTime: datetime
-    parameters: List[Parameter]
-    datasets: List[Any]
-    meta: Meta
+    parameters: List[
+        Parameter
+    ]  # Generic list of parameters that will depend on the user portal
+    datasets: List[Any]  # datasets collected for this sample
+    meta: Meta  # pagination parameters
