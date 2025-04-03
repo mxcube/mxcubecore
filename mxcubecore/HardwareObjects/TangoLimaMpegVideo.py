fe617abc3c23e38c1684f51701c3d798bfaa39a4
walesch-yan <yan.walesch@esrf.fr>
2025-04-03 16:14:49 +0200

    [TMP] HUtchCamerasTest

--- mxcubecore/HardwareObjects/TangoLimaMpegVideo.py
+++ mxcubecore/HardwareObjects/TangoLimaMpegVideo.py
@@ -25,6 +25,7 @@ from typing import (
 
 import psutil
 
+from mxcubecore import HardwareRepository as HWR
 from mxcubecore.HardwareObjects.TangoLimaVideo import TangoLimaVideo
 
 
@@ -104,6 +105,16 @@ class TangoLimaMpegVideo(TangoLimaVideo):
                 close_fds=True,
                 stdout=subprocess.DEVNULL,
             )
+
+            argus = HWR.beamline.argus
+            if argus:
+                argus.add_camera_stream(
+                    {
+                        "name": "OnAxisViewer",
+                        "port": str(self._port),
+                        "id": self.stream_hash,
+                    }
+                )
 
             atexit.register(self.clean_up)
 
