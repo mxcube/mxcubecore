fa1d537edf8e731689106ca81a60c8b53cac8ea6
walesch-yan <yan.walesch@esrf.fr>
2025-05-13 17:48:51 +0200

    Adding Argus object

--- mxcubecore/HardwareObjects/Beamline.py
+++ mxcubecore/HardwareObjects/Beamline.py
@@ -253,6 +253,10 @@ class Beamline(HardwareObject):
         return self.get_object_by_role("image_tracking")
 
     @property
+    def argus(self) -> HardwareObject | None:
+        return self.get_object_by_role("argus")
+
+    @property
     def procedure(self) -> HardwareObject | None:
         return self.get_object_by_role("procedure")
 
