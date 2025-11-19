51dd97cdd2c976f980869f7116a018e4002d191b
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-11-19 09:13:05 +0100

    MAXIV MicroMAX: update 'prepare open hutch' beamline action
    Expand steps done when running 'prepare open hutch' beamline action.
    
    * 'turn off' laser
    * close fast shutter
    * put MD3 into 'transfer' phase in HVE mode

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/beamline_actions.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/beamline_actions.py
@@ -12,25 +12,38 @@ class PrepareOpenHutch:
 
     - close safety shutter
     - close detector cover
+    - close fast shutter
     - move detector to a safe position
-    - put MD3 into 'Transfer' phase
+    - put MD3 into 'Transfer' phase in case of OSC delivery mode
+    - move MD3's `BeamstopPosition` and `CapillaryPosition` to `PARK` in case of HVE
     - if jungfrau is used, take pedestal
     """
 
     def __call__(self):
         try:
+            # Ensure laser is stopped before opening the hutch
+            laser = HWR.beamline.get_object_by_role("laser")
+            laser.disarm()
+
             collect = HWR.beamline.collect
             diffractometer = HWR.beamline.diffractometer
             detector = HWR.beamline.detector
 
             log.info("Preparing experimental hutch for door opening.")
 
+            collect.close_fast_shutter()
             collect.close_safety_shutter()
             collect.close_detector_cover()
 
-            log.info("Setting diffractometer to transfer phase.")
             diffractometer.wait_device_ready()
-            diffractometer.set_phase("Transfer")
+            if HWR.beamline.is_hve_sample_delivery():
+                # This is 'equivalent' of Transfer phase for HVE experiments
+                log.info("Setting diffractometer to 'equivalent' of Transfer phase.")
+                diffractometer.channel_dict["BeamstopPosition"].set_value("PARK")
+                diffractometer.channel_dict["CapillaryPosition"].set_value("PARK")
+            else:
+                log.info("Setting diffractometer to Transfer phase.")
+                diffractometer.set_phase("Transfer")
 
             log.info("Moving detector to safe position.")
             collect.move_detector_to_safe_position()
