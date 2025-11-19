ece1f4c2e5c1f7ae36e021eba80219526c3f0e4c
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-11-19 09:39:57 +0100

    MAXIV MicroMAX: add initial 'check beam' beamline action
    Add simple 'check beam' beamline action, for checking beam current
    and flux at sample position.

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/beamline_actions.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/beamline_actions.py
@@ -1,6 +1,9 @@
 import logging
 
+from tango import DeviceProxy
+
 from mxcubecore import HardwareRepository as HWR
+from mxcubecore.utils.units import kev_to_ev
 
 log = logging.getLogger("user_level_log")
 
@@ -55,6 +58,41 @@ class PrepareOpenHutch:
             # Explicitly add raised exception into the log message,
             # so that it is shown to the user in the beamline action UI log.
             log.exception("Error preparing to open hutch.\nError was: '%s'", str(ex))  # noqa: TRY401
+
+
+class CheckBeam:
+    def __call__(self):
+        """
+        Check beam stability
+        """
+        xbpms = {
+            "DM3": DeviceProxy("b312a-o06/dia/xbpm-01"),
+            "DM4": DeviceProxy("b312a-e01/dia/xbpm-01"),
+            "BCU XBPM1": DeviceProxy("b312a-e04/dia/xbpm-01"),
+            "BCU XBPM2": DeviceProxy("b312a-e04/dia/xbpm-02"),
+        }
+        energy = kev_to_ev(HWR.beamline.energy.get_value())
+        transmission = HWR.beamline.transmission.get_value()
+        for name, xbpm in xbpms.items():
+            total_current = xbpm.S
+            flux = total_current * (
+                -0.534515 * energy**4
+                - 43197.6 * energy**3
+                + 5.13449e09 * energy**2
+                - 4.39169e13 * energy
+                + 1.14591e17
+            )
+            log.info(
+                f"XBPM: {name}, total current: {total_current * 1e6:.2f} uA, "
+                f"estimated flux at sample position: {flux:.2e} ph/s"
+            )
+
+            if "BCU" in name:
+                full_flux = flux * 100.0 / transmission
+                log.info(
+                    f"Current transmission: {transmission:.2f}%, "
+                    f"estimated full flux at sample position: {full_flux:.2e} ph/s"
+                )
 
 
 class MeasureFlux:
