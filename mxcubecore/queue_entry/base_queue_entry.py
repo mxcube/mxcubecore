a491de1c31d81a710c07a9ad3261f4fe7c09c20f
Generic Bliss Account For Control Software <blissadm@lid30a1control2.esrf.fr>
2025-11-24 09:52:48 +0100

    [Queue] - Added missing keyword argument `origin`

--- mxcubecore/queue_entry/base_queue_entry.py
+++ mxcubecore/queue_entry/base_queue_entry.py
@@ -95,7 +95,7 @@ class TaskPrerequisite(str, Enum):
 
 
 class QueueExecutionException(Exception):
-    def __init__(self, message, origin):
+    def __init__(self, message, origin=None):
         Exception.__init__(self, message, origin)
         self.message = message
         self.origin = origin
