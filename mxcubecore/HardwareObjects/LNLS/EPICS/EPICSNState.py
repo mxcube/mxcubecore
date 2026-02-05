cfe76c12b8cb68a7e07af752f7393350e9ce444f
Pedro Benetton <pedro.benetton@lnls.br>
2026-02-05 08:21:21 -0300

    Implement EPICSNStateInterval
    This class is a workaround for our frontlight device. The device
    allows intensity adjustment but does not have an on/off switch.
    This class allows it to have a virtual on/off switch so that it
    can be displayed in the lightbulb icon at the GUI. This class
    can also be used by other devices that have the same issue,
    provided that their values are also integers.

--- mxcubecore/HardwareObjects/LNLS/EPICS/EPICSNState.py
+++ mxcubecore/HardwareObjects/LNLS/EPICS/EPICSNState.py
@@ -73,3 +73,54 @@ class EPICSNState(EPICSActuator, AbstractNState):
         if not readback:
             return False
         return setpoint != readback
+
+
+class EPICSNStateInterval(EPICSNState):
+    """
+    This class is a workaround for devices that exist in a discrete number of states
+    and do NOT have an on/off switch, but NEED to have a virtual on/off state in the
+    GUI. Example: our frontlight device only allows for intensity setting (between 0
+    and 20000), but does not have an on/off switch.
+
+    This class implements the following logic: if the state of the device is higher
+    than LEVEL1, it will be shown as on in the GUI. Otherwise, it will be shown as
+    off in the GUI.
+
+    Clicking on the icon when it is off will bring it to the LEVEL0 intensity value.
+    Clicking on the icon when it is on will bring it to the LEVEL1 intensity value.
+
+    YAML Example
+    ------------
+
+    %YAML 1.2
+    ---
+    class: LNLS.EPICS.EPICSNState.EPICSNStateInterval
+    epics:
+    "MNC:B:LUCIOLE01:LIGHT_CH2":
+      channels:
+        rbv:
+          suffix: ''
+          polling_period: 200
+        val:
+          suffix: ''
+    configuration:
+      low_limit: 0
+      high_limit: 1
+      values: {'LEVEL0': 15000, 'LEVEL1': 0}
+    """
+
+    def init(self):
+        super().init()
+
+    def update_value(self, value=None) -> None:
+        if value is None:
+            return
+        if isinstance(value, Enum):
+            value = value.value
+        if not isinstance(value, int):
+            return
+        values_list = list(self.VALUES)
+        if not (len(values_list) > 1):
+            return
+        enum_value = values_list[0] if value > values_list[1].value else values_list[1]
+        super().update_value(enum_value)
