91b1ab6e91c8d66bdaa89108d4e7cb5a878c6a3d
Marcus Oskarsson <oscarsso@esrf.fr>
2026-02-18 13:20:43 +0100

    [Manual Centring] - Fix

--- mxcubecore/HardwareObjects/sample_centring.py
+++ mxcubecore/HardwareObjects/sample_centring.py
@@ -73,7 +73,7 @@ def prepare(centring_motors_dict):
 
     if CURRENT_CENTRING and not CURRENT_CENTRING.ready():
         logging.getLogger("HWR").debug("DEBUG: ENDING CURRENT CENTRING")
-        end()
+        CURRENT_CENTRING.kill()
 
     if USER_CLICKED_EVENT and not USER_CLICKED_EVENT.ready():
         logging.getLogger("HWR").debug("DEBUG: USER_CLICKED_EVENT: false")
