b2f9cf54a589d11338f7c08e8b664cfc12ff639f
Dominika <dominika.trojanowska@s2innovation.com>
2026-03-23 10:40:25 +0100

    Refactor ISPyB integration: replace SOAP client with PyISPyB REST client
    Replace the legacy SOAP-based ISPyB client with a new PyISPyB REST implementation
    backed by Keycloak token authentication. Introduce ``PyISPyBDataAdapter`` and
    ``PyISPyBRestClient``as abstract, reusable components, removing the old ``ISPyBAdapter``.
    Add adjustable timeouts, improved error handling, and legacy method compatibility.
    Update tests to reflect the new REST implementation and handle Max IV specific  ``duo``
    and ``sdm`` dependencies gracefully.

--- test/test_hwo_maxiv_ispyblims.py
+++ test/test_hwo_maxiv_ispyblims.py
@@ -6,23 +6,19 @@ from unittest.mock import (
 
 import pytest
 
-ISPYB_AUTH_ERROR_MESSSAGE = (
-    "JBAS011843: Failed instantiate InitialContextFactory com.sun.jndi.ldap.LdapCtxFactory "
-    'from classloader ModuleClassLoader for Module "deployment.ispyb.ear.ispyb-ws.war:main" '
-    "from Service Module Loader"
-)
+PYISPYB_AUTH_ERROR_MESSSAGE = "Could not verify"
 
 REST_ROOT = "http://example.com/rest/"
 USER = "testusr"
-PASS = "testpsd"  # noqa: S105
+TOKEN = "testtkn"  # noqa: S105
 
 
 @pytest.fixture
 @patch("mxcubecore.HardwareObjects.abstract.AbstractLims.HWR")
 @patch("mxcubecore.HardwareObjects.abstract.ISPyBAbstractLims.HWR")
-@patch("mxcubecore.HardwareObjects.abstract.ISPyBDataAdapter.Client")
+@patch("mxcubecore.HardwareObjects.abstract.PyISPyBRestClient.Session")
 @patch("mxcubecore.HardwareObjects.UserTypeISPyBLims.Client")
-def ispyb_lims(_suds_mock1, _suds_mock2, _hwr_mock, hwr_mock):
+def ispyb_lims(_suds_mock, session_mock, _hwr_mock, hwr_mock):
     # make sure mocked session HWOBJ have session set
     hwr_mock.beamline.session.beamline_name = "ID42"
 
@@ -31,12 +27,11 @@ def ispyb_lims(_suds_mock1, _suds_mock2, _hwr_mock, hwr_mock):
 
     lims = ISPyBLims(name="dummy")
     lims._config = lims.HOConfig(  # noqa: SLF001
-        ws_root="http://example.com/tstWS/",
-        rest_root=REST_ROOT,
+        pyispyb_rest_root=REST_ROOT
     )
     lims.init()
 
-    return lims
+    return lims, session_mock.return_value.post
 
 
 @pytest.fixture
@@ -47,14 +42,21 @@ def json_decode_error():
     return JSONDecodeError("mocked-err", doc, 0)
 
 
-@patch("requests.post")
-def test_login_ok(post_mock, ispyb_lims):
+# @patch("requests.Session")
+def test_login_ok(ispyb_lims):
     """test the case when user logs in successfully"""
+    ispyb_lims, post_mock = ispyb_lims
 
-    # mocks the ISPyB client to return valid token on POST request
-    post_mock.return_value.json.return_value = {"token": "dummy-token"}
+    # mock the ISPyB client to return valid token on POST request
+    response_mock = MagicMock(
+        url=f"{REST_ROOT}auth/login",
+        status_code=200,
+        text="dummy-text",
+    )
+    post_mock.return_value = response_mock
+    response_mock.json.return_value = {"token": "dummy-token"}
 
-    is_ok, err = ispyb_lims.ispyb_login(USER, PASS)
+    is_ok, err = ispyb_lims.ispyb_login(USER, TOKEN)
 
     # check that we got 'login ok' result
     assert is_ok
@@ -62,27 +64,44 @@ def test_login_ok(post_mock, ispyb_lims):
 
     # check that correct POST request was made
     post_mock.assert_called_once_with(
-        f"{REST_ROOT}authenticate?site=MAXIV",
-        data={"login": USER, "password": PASS},
+        f"{REST_ROOT}auth/login",
+        json={
+            "plugin": "keycloak",
+            "login": USER,
+            "token": TOKEN,
+        },
+        timeout=5,
     )
 
 
-@patch("requests.post")
-def test_login_invalid_credentials(post_mock, ispyb_lims, json_decode_error):
+def test_login_invalid_credentials(ispyb_lims, json_decode_error):
     """test the case when user fails to login due to wrong credentials"""
+    ispyb_lims, post_mock = ispyb_lims
 
-    #
     # mock ISPyB 'login failed' response,
-    # yes, this is how ISPyB replies at MAXIV
-    #
-    response_mock = MagicMock()
-    response_mock.text = ISPYB_AUTH_ERROR_MESSSAGE
+    response_mock = MagicMock(
+        url=f"{REST_ROOT}auth/login",
+        status_code=401,
+        text=PYISPYB_AUTH_ERROR_MESSSAGE,
+    )
+    post_mock.return_value = response_mock
     response_mock.json.side_effect = json_decode_error
 
-    post_mock.return_value = response_mock
-
-    is_ok, err = ispyb_lims.ispyb_login(USER, PASS)
+    is_ok, err = ispyb_lims.ispyb_login(USER, TOKEN)
 
     # check that we got 'login failed' result
     assert not is_ok
-    assert err == "invalid credentials"
+    assert (
+        err == f"Request to {REST_ROOT}auth/login failed with code: 401 "
+        f"Response: {PYISPYB_AUTH_ERROR_MESSSAGE}"
+    )
+
+    post_mock.assert_called_once_with(
+        f"{REST_ROOT}auth/login",
+        json={
+            "plugin": "keycloak",
+            "login": USER,
+            "token": TOKEN,
+        },
+        timeout=5,
+    )
