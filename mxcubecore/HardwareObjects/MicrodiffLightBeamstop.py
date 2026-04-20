92068e79246e0bf27f21e1d9173a53fa91153ca6
Antonia Beteva <beteva@vm-beteva.esrf.fr>
2026-04-20 19:15:51 +0200

    Make MicrodiffLightBeamstop to be used by both backight and beamstop. Remove obsolete HO.

--- mxcubecore/HardwareObjects/MicrodiffLightBeamstop.py
+++ mxcubecore/HardwareObjects/MicrodiffLightBeamstop.py
@@ -2,65 +2,73 @@
 Combine setting the back light IN with moving the beamstop out.
 This is used to set the back light in/out faster, than using
 the phase CENTRING - the beamstop motor moves only if needed.
-Example xml file:
-<object class="MicrodiffLightBeamstop">
-  <username>Back Light</username>
-  <exporter_address>wid30bmd2s:9001</exporter_address>
-  <actuator_name>BackLightIsOn</actuator_name>
-  <values>{"IN": True, "OUT": False}</values>
-  <use_hwstate>True</use_hwstate>
-  <object role="beamstop" href="/udiff_beamstop"/>
-</object>
+It also makes sure the beamstop is in when in COLLECT phase.
+Example xml configuration:
+
+.. code-block:: xml
+ <object class="MicrodiffLightBeamstop">
+   <username>Back Light</username>
+   <actuator_name>backlight</actuator_name>
+   <exporter_address>xxx:9001</exporter_address>
+   <value_channel_name>BackLightIsOn</value_channel_name>
+   <values>{"IN": True, "OUT": False}</values>
+   <state_channel_name>HardwareState</state_channel_name>
+ or
+   <username>Beamstop</username>
+   <actuator_name>beamstop</actuator_name>
+   <exporter_address>xxx:9001</exporter_address>
+   <value_channel_name>BeamstopPosition</value_channel_name>
+   <values>{"IN": "BEAM", "OUT": "OFF", "PARK": "PARK", "TRANSFER": "TRANSFER"}</values>
+   <state_channel_name>HardwareState</state_channel_name>
+ </object>
 """
 
+from mxcubecore import HardwareRepository as HWR
 from mxcubecore.HardwareObjects.ExporterNState import ExporterNState
 
-__copyright__ = """ Copyright © 2010-2020 by the MXCuBE collaboration """
+__copyright__ = """ Copyright © by the MXCuBE collaboration """
 __license__ = "LGPLv3+"
 
 
 class MicrodiffLightBeamstop(ExporterNState):
-    """Control backlight, move the beamstop to safety position"""
+    """MicrodiffLightBeamstop class"""
 
     def __init__(self, name):
         super().__init__(name)
-        self._beamstop_obj = None
-        self._saved_beamstop_value = None
+        self.coupled_ctrl = None
 
-    def init(self):
-        """Initialize the light and the beamstop object"""
-        super().init()
+    def _set_coupled_out(self):
+        """Move the coupled device out."""
+        if self.coupled_ctrl.get_value() == self.coupled_ctrl.VALUES.IN:
+            # move the extra controlle out
+            self.coupled_ctrl.set_value(self.coupled_ctrl.VALUES.OUT)
 
-        # get the beamstop object
-        self._beamstop_obj = self.get_object_by_role("beamstop")
+    def _set_coupled_in(self):
+        """Move sure the coupled device in if needed. Usually needed when
+        the phase is COLLECT and the beamstop is out.
+        """
+        diffr = HWR.beamline.diffractometer
+        phase = diffr.current_phase
+        if phase == diffr.get_phase_enum.COLLECT and "light" in self.actuator_name:
+            self.coupled_ctrl.set_value(self.coupled_ctrl.VALUES.IN)
 
     def _set_value(self, value):
-        """Set light to value. Move the beamstop, if needed.
+        """Set device to value.
         Args:
-            value (enum): Value to be set.
+            value (str, int, float or enum): Value to be set.
         """
-        if value == self.VALUES.IN:
-            # move the beamstop out before setting the back light in
-            if self._beamstop_obj:
-                self.handle_beamstop(value)
-            super()._set_value(value)
-        else:
-            super()._set_value(value)
-            # move the beamstop if needed after getting the light out
-            if self._beamstop_obj:
-                self.handle_beamstop(value)
+        self.coupled_ctrl = (
+            HWR.beamline.diffractometer.beamstop
+            if "light" in self.actuator_name
+            else HWR.beamline.diffractometer.backlightswitch
+        )
 
-    def handle_beamstop(self, value):
-        """Move the beamstop as function of the value of the back light.
-        Args:
-            (enum): light value.
-        """
+        # set the coupled device out first when the value is IN
         if value == self.VALUES.IN:
-            self._saved_beamstop_value = self._beamstop_obj.get_value()
-            # move the beamstop out to avoid collision with the back light
-            if self._saved_beamstop_value == self._beamstop_obj.VALUES.IN:
-                self._beamstop_obj.set_value(self._beamstop_obj.VALUES.OUT, timeout=60)
-        else:
-            if self._saved_beamstop_value:
-                self._beamstop_obj.set_value(self._saved_beamstop_value, timeout=60)
-                self._saved_beamstop_value = None
+            self._set_coupled_out()
+
+        super()._set_value(value)
+
+        # set the coupled device in, if needed, when the value is OUT
+        if value == self.VALUES.OUT:
+            self._set_coupled_in()
