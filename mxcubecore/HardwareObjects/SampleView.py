3f167d71039d8589faa9846d6cad498ee4c5f906
Marcus Oskarsson <oscarsso@esrf.fr>
2026-04-27 10:03:07 +0200

    Rejecting centring when autmoatic loop centring fails

--- mxcubecore/HardwareObjects/SampleView.py
+++ mxcubecore/HardwareObjects/SampleView.py
@@ -314,6 +314,7 @@ class SampleView(AbstractSampleView):
                     "Automatic loop centring failed"
                 )
                 self.centring_failed()
+                self.reject_centring()
             else:
                 self.centring_done()
                 self.accept_centring()
