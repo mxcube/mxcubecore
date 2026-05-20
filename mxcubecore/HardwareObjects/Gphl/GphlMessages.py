f5beb615b6412a62dfa7cdb38d0032ced6e671dd
Olof Svensson <svensson@esrf.fr>
2026-05-20 10:54:59 +0200

    Issue #1609 : Reverted the changes to GphlMessages.py

--- mxcubecore/HardwareObjects/Gphl/GphlMessages.py
+++ mxcubecore/HardwareObjects/Gphl/GphlMessages.py
@@ -227,12 +227,12 @@ class IdentifiedElement(MessageData):
 
     def __set_id(self, value):
         """Setter for uuid - accessible only within this class"""
-        if isinstance(value, uuid.UUID):
-            self._id = value
-        elif value is None:
+        if value is None:
             self._id = uuid.uuid1()
+        elif isinstance(value, uuid.UUID):
+            self._id = value
         else:
-            self._id = uuid.UUID(value)
+            raise TypeError("UUID input must be of type uuid.UUID")
 
 
 # Sync with Java 4/5/2017
