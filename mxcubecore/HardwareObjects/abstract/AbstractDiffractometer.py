3ae80f338162e9ec4eee773636bbcb93704bc159
Generic Bliss Account For Control Software <blissadm@lid30a1control2.esrf.fr>
2026-06-22 10:16:49 +0200

    Defaulting goniometer head orientation to horizontal

--- mxcubecore/HardwareObjects/abstract/AbstractDiffractometer.py
+++ mxcubecore/HardwareObjects/abstract/AbstractDiffractometer.py
@@ -230,7 +230,7 @@ class AbstractDiffractometer(HardwareObject):
         """
         super().init()
         self.username = self.get_property("username") or self.username
-        self.head_orientation = self.get_property("head_orientation", "vertical")
+        self.head_orientation = self.get_property("head_orientation", "horizontal")
 
         # motors
         for role in self.config.motors:
