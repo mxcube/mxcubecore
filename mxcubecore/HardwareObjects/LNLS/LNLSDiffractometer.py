89762bac4ae56818c28b95a7ffd67eeed9e8f93d
rafael.lyra <rafael.lyra@lnls.br>
2026-07-13 09:47:02 -0300

    Move some methods to LNLSSampleView

--- mxcubecore/HardwareObjects/LNLS/LNLSDiffractometer.py
+++ mxcubecore/HardwareObjects/LNLS/LNLSDiffractometer.py
@@ -1,48 +1,21 @@
-from gevent.event import AsyncResult
-
 from mxcubecore import HardwareRepository as HWR
-from mxcubecore.HardwareObjects.abstract.AbstractDiffractometer import AbstractDiffractometer, DiffractometerPhase
+from mxcubecore.HardwareObjects.abstract.AbstractDiffractometer import (
+    AbstractDiffractometer,
+    DiffractometerPhase,
+)
 
 
 class LNLSDiffractometer(AbstractDiffractometer):
-
     def init(self):
         AbstractDiffractometer.init(self)
         self._bluesky_api = HWR.beamline.get_object_by_role("bluesky")
-        self.current_motor_positions = {}
         self.current_phase = DiffractometerPhase.UNKNOWN
 
-    def manual_centring(self):
-        self.log.info("Initializing manual sample alignment...")
-        for step in range(3):
-            self.log.info(f"Step {step + 1} of 3...")
-            self.user_clicked_event = AsyncResult()
-            self.waiting_for_click = True
-            x, y = self.user_clicked_event.get()
-            self.log.info(f"{x}, {y}")
-            self._bluesky_api.execute_plan(
-                plan_name="manual_alignment",
-                kwargs={"x_px": x, "y_px": y, "step": step},
-            )
-        self.log.info("Manual sample alignment has finished...")
-        return {}
-
-    def automatic_centring(self):
-        self.log.info("Initializing automatic sample alignment...")
-        self._bluesky_api.execute_plan(plan_name="automatic_alignment")
-        self.log.info("Automatic sample alignment has finished...")
-
-    def move_to_beam(self, x, y, omega=None):
-        self.log.info("Moving to beam...")
-
-        self._bluesky_api.execute_plan(
-            plan_name="move_to_beam",
-            kwargs={
-                "x_px": x - self.beam_position[0],
-                "y_px": y - self.beam_position[1],
-            },
-        )
-        self.log.info("Move to beam has finished...")
-    
     def get_pixels_per_mm(self):
         return (1, 1)
+
+    def save_centring_positions(self):
+        return
+
+    def wait_status_ready(self, timeout=None):
+        return True
---
+++ mxcubecore/HardwareObjects/LNLS/LNLSSampleView.py
@@ -0,0 +1,45 @@
+import logging
+from gevent.event import AsyncResult
+
+from mxcubecore import HardwareRepository as HWR
+from mxcubecore.HardwareObjects.SampleView import SampleView
+
+
+class LNLSSampleView(SampleView):
+
+    def init(self):
+        SampleView.init(self)
+        self.user_level_log = logging.getLogger("user_level_log")
+        self._bluesky_api = HWR.beamline.get_object_by_role("bluesky")
+        self.update_state(self.STATES.READY)
+
+    def move_to_beam(self, x, y, omega=None):
+        self.user_level_log.info("Moving to beam...")
+
+        beam_pos = HWR.beamline.beam.get_beam_position_on_screen()
+        self._bluesky_api.execute_plan(
+            plan_name="move_to_beam",
+            kwargs={
+                "x_px": x - beam_pos[0],
+                "y_px": y - beam_pos[1],
+            },
+        )
+        self.user_level_log.info("Move to beam has finished...")
+
+    def start_manual_centring(self, nb_click: int = 3):
+        self.user_level_log.info("Initializing manual sample alignment...")
+        for step in range(nb_click):
+            self.user_level_log.info(f"Step {step + 1} of 3...")
+            self.user_clicked_event = AsyncResult()
+            self.waiting_for_click = True
+            x, y = self.user_clicked_event.get()
+            self._bluesky_api.execute_plan(
+                plan_name="manual_alignment",
+                kwargs={"x_px": x, "y_px": y, "step": step},
+            )
+        self.user_level_log.info("Manual sample alignment has finished...")
+
+    def start_auto_centring(self):
+        self.user_level_log.info("Initializing automatic sample alignment...")
+        self._bluesky_api.execute_plan(plan_name="automatic_alignment")
+        self.user_level_log.info("Automatic sample alignment has finished...")
