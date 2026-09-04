5b8cbf67321393445a43629082e4ab6c87341e90
Marcus Oskarsson <oscarsso@esrf.fr>
2026-09-04 14:39:02 +0200

    Removed unsed import

--- mxcubecore/HardwareObjects/ICATLIMS.py
+++ mxcubecore/HardwareObjects/ICATLIMS.py
@@ -19,7 +19,6 @@ from mxcubecore.model.lims_session import (
     Download,
     Lims,
     LimsSessionManager,
-    SampleSheet,
     Session,
 )
 from mxcubecore.model.tracking_model_objects import LoadedPuck
